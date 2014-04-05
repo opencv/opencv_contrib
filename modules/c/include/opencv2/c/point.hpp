3844f284cebc178507d962a1fda95c0c252618aa
Arjun Comar <nrujac@gmail.com>
2014-04-05 00:42:18 -0400

    Switched point.c/hpp to macros for wrapping
    Now using macros to automatically generate code for the various element types
    that points can accept. While I'm only committing code for int/float/double,
    support can now trivially be extended to any type you might like to store in
    a cv::Point_<>.

--- modules/c/include/opencv2/c/point.hpp
+++ modules/c/include/opencv2/c/point.hpp
@@ -17,36 +17,21 @@
 
 #include <opencv2/c/opencv_generated.hpp>
 
+#define ADD_POINT_FUNC_HEADERS(t, tn) \
+    Point2##t * cv_create_Point2##t ( tn x,  tn y); \
+    Point3##t * cv_create_Point3##t ( tn x,  tn y,  tn z); \
+    tn cv_Point2##t##_getX( Point2##t * self); \
+    tn cv_Point2##t##_getY( Point2##t * self); \
+    tn cv_Point3##t##_getX( Point3##t * self); \
+    tn cv_Point3##t##_getY( Point3##t * self); \
+    tn cv_Point3##t##_getZ( Point3##t * self); \
+    tn cv_Point2##t##_dot( Point2##t * self, Point2##t * other); \
+    tn cv_Point3##t##_dot( Point3##t * self, Point3##t * other); \
+    Point3##t * cv_Point3##t##_cross(Point3##t * self, Point3##t * other);
+
 extern "C" {
-    Point* cv_create_Point(int x, int y);
-    int cv_Point_getX(Point* self);
-    int cv_Point_getY(Point* self);
-    int cv_Point_dot(Point* self, Point* other);
-    Point2f* cv_create_Point2f(float x, float y);
-    float cv_Point2f_getX(Point2f* self);
-    float cv_Point2f_getY(Point2f* self);
-    float cv_Point2f_dot(Point2f* self, Point2f* other);
-    Point2d* cv_create_Point2d(double x, double y);
-    double cv_Point2d_getX(Point2d* self);
-    double cv_Point2d_getY(Point2d* self);
-    double cv_Point2d_dot(Point2d* self, Point2d* other);
-    Point3i* cv_create_Point3i(int x, int y, int z);
-    int cv_Point3i_getX(Point3i* self);
-    int cv_Point3i_getY(Point3i* self);
-    int cv_Point3i_getZ(Point3i* self);
-    int cv_Point3i_dot(Point3i* self, Point3i* other);
-    Point3i* cv_Point3i_cross(Point3i* self, Point3i* other);
-    Point3f* cv_create_Point3f(float x, float y, float z);
-    float cv_Point3f_getX(Point3f* self);
-    float cv_Point3f_getY(Point3f* self);
-    float cv_Point3f_getZ(Point3f* self);
-    float cv_Point3f_dot(Point3f* self, Point3f* other);
-    Point3f* cv_Point3f_cross(Point3f* self, Point3f* other);
-    Point3d* cv_create_Point3d(double x, double y, double z);
-    double cv_Point3d_getX(Point3d* self);
-    double cv_Point3d_getY(Point3d* self);
-    double cv_Point3d_getZ(Point3d* self);
-    double cv_Point3d_dot(Point3d* self, Point3d* other);
-    Point3d* cv_Point3d_cross(Point3d* self, Point3d* other);
+    ADD_POINT_FUNC_HEADERS(i, int);
+    ADD_POINT_FUNC_HEADERS(f, float);
+    ADD_POINT_FUNC_HEADERS(d, double);
 }
 
--- modules/c/src/point.cpp
+++ modules/c/src/point.cpp
@@ -1,94 +1,39 @@
 #include <opencv2/c/point.hpp>
 
+#define ADD_POINT_FUNC_IMPL(t, tn) \
+    Point2##t * cv_create_Point2##t ( tn x,  tn y) { \
+        return new Point2##t (x, y);\
+    } \
+    Point3##t * cv_create_Point3##t ( tn x,  tn y,  tn z) { \
+        return new Point3##t (x, y, z);\
+    } \
+    tn cv_Point2##t##_getX( Point2##t * self) { \
+        return self->x;\
+    }\
+    tn cv_Point2##t##_getY( Point2##t * self) { \
+        return self->y;\
+    }\
+    tn cv_Point3##t##_getX( Point3##t * self) { \
+        return self->x;\
+    }\
+    tn cv_Point3##t##_getY( Point3##t * self) { \
+        return self->y;\
+    }\
+    tn cv_Point3##t##_getZ( Point3##t * self) { \
+        return self->z;\
+    }\
+    tn cv_Point2##t##_dot( Point2##t * self, Point2##t * other) { \
+        return self->dot(*other);\
+    }\
+    tn cv_Point3##t##_dot( Point3##t * self, Point3##t * other) { \
+        return self->dot(*other);\
+    }\
+    Point3##t * cv_Point3##t##_cross(Point3##t * self, Point3##t * other) { \
+        return new Point3##t (self->cross(*other));\
+    }
+    
 extern "C" {
-    Point* cv_create_Point(int x, int y) {
-        return new Point(x, y);
-    }
-    int cv_Point_getX(Point* self) {
-        return self->x;
-    }
-    int cv_Point_getY(Point* self) {
-        return self->y;
-    }
-    int cv_Point_dot(Point* self, Point* other) {
-        return self->dot(*other);
-    }
-    Point2f* cv_create_Point2f(float x, float y) {
-        return new Point2f(x, y);
-    }
-    float cv_Point2f_getX(Point2f* self) {
-        return self->x;
-    }
-    float cv_Point2f_getY(Point2f* self) {
-        return self->y;
-    }
-    float cv_Point2f_dot(Point2f* self, Point2f* other) {
-        return (self->dot(*other));
-    }
-    Point2d* cv_create_Point2d(double x, double y) {
-        return new Point2d(x, y);
-    }
-    double cv_Point2d_getX(Point2d* self) {
-        return self->x;
-    }
-    double cv_Point2d_getY(Point2d* self) {
-        return self->y;
-    }
-    double cv_Point2d_dot(Point2d* self, Point2d* other) {
-        return (self->dot(*other));
-    }
-    Point3i* cv_create_Point3i(int x, int y, int z) {
-        return new Point3i(x, y, z);
-    }
-    int cv_Point3i_getX(Point3i* self) {
-        return self->x;
-    }
-    int cv_Point3i_getY(Point3i* self) {
-        return self->y;
-    }
-    int cv_Point3i_getZ(Point3i* self) {
-        return self->z;
-    }
-    int cv_Point3i_dot(Point3i* self, Point3i* other) {
-        return self->dot(*other);
-    }
-    Point3i* cv_Point3i_cross(Point3i* self, Point3i* other) {
-        return new Point3i(self->cross(*other));
-    }
-    Point3f* cv_create_Point3f(float x, float y, float z) {
-        return new Point3f(x, y, z);
-    }
-    float cv_Point3f_getX(Point3f* self) {
-        return self->x;
-    }
-    float cv_Point3f_getY(Point3f* self) {
-        return self->y;
-    }
-    float cv_Point3f_getZ(Point3f* self) {
-        return self->z;
-    }
-    float cv_Point3f_dot(Point3f* self, Point3f* other) {
-        return (self->dot(*other));
-    }
-    Point3f* cv_Point3f_cross(Point3f* self, Point3f* other) {
-        return new Point3f(self->cross(*other));
-    }
-    Point3d* cv_create_Point3d(double x, double y, double z) {
-        return new Point3d(x, y, z);
-    }
-    double cv_Point3d_getX(Point3d* self) {
-        return self->x;
-    }
-    double cv_Point3d_getY(Point3d* self) {
-        return self->y;
-    }
-    double cv_Point3d_getZ(Point3d* self) {
-        return self->z;
-    }
-    double cv_Point3d_dot(Point3d* self, Point3d* other) {
-        return (self->dot(*other));
-    }
-    Point3d* cv_Point3d_cross(Point3d* self, Point3d* other) {
-        return new Point3d(self->cross(*other));
-    }
+    ADD_POINT_FUNC_IMPL(i, int);
+    ADD_POINT_FUNC_IMPL(f, float);
+    ADD_POINT_FUNC_IMPL(d, double);
 }
