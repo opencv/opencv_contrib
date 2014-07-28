a52c0a2bb0d19f4d68a9eca82d2b6f098de704a0
Vadim Pisarevsky <vadim.pisarevsky@gmail.com>
2014-07-29 01:32:25 +0400

    probably fixed compile errors

--- modules/text/samples/textdetection.cpp
+++ modules/text/samples/textdetection.cpp
@@ -8,7 +8,6 @@
  *     Author: Lluis Gomez i Bigorda <lgomez AT cvc.uab.es>
  */
 
-#include  "opencv2/opencv.hpp"
 #include  "opencv2/text.hpp"
 #include  "opencv2/highgui.hpp"
 #include  "opencv2/imgproc.hpp"
