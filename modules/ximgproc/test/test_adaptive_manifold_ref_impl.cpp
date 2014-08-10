f45b5602640f3dcfd0b69dafa2efe8e507da3762
vludv <ludv1x@yandex.ru>
2014-08-10 22:28:00 +0400

    fixed include error.

--- modules/ximgproc/test/test_adaptive_manifold_ref_impl.cpp
+++ modules/ximgproc/test/test_adaptive_manifold_ref_impl.cpp
@@ -21,7 +21,7 @@
  * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
 
-#include "precomp.hpp"
+#include "test_precomp.hpp"
 #include <opencv2/core/private.hpp>
 #include <cmath>
 
