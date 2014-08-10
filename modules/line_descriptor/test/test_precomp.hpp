a43a29ca3a40ac63624999cfd0f563587f6d7acb
biagio montesano <ubisum@biagioPC.(none)>
2014-08-10 13:04:59 +0200

    Removed wrong header

--- modules/line_descriptor/test/test_precomp.hpp
+++ modules/line_descriptor/test/test_precomp.hpp
@@ -9,7 +9,6 @@
 #ifndef __OPENCV_TEST_PRECOMP_HPP__
 #define __OPENCV_TEST_PRECOMP_HPP__
 
-#include "opencv2/opencv.hpp"
 #include "opencv2/ts.hpp"
 #include "opencv2/imgproc.hpp"
 #include "opencv2/features2d.hpp"
