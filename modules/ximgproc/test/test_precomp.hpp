e622117f781edabce472654233d8e361ae258f98
vludv <ludv1x@yandex.ru>
2014-08-12 12:31:25 +0400

    Try to fix GCC warnings again

--- modules/ximgproc/test/test_precomp.hpp
+++ modules/ximgproc/test/test_precomp.hpp
@@ -1,6 +1,3 @@
-#ifndef __OPENCV_TEST_PRECOMP_HPP__
-#define __OPENCV_TEST_PRECOMP_HPP__
-
 #ifdef __GNUC__
 #  pragma GCC diagnostic ignored "-Wmissing-declarations"
 #  if defined __clang__ || defined __APPLE__
@@ -8,6 +5,9 @@
 #    pragma GCC diagnostic ignored "-Wextra"
 #  endif
 #endif
+
+#ifndef __OPENCV_TEST_PRECOMP_HPP__
+#define __OPENCV_TEST_PRECOMP_HPP__
 
 #include <opencv2/ts.hpp>
 #include <opencv2/ts/ts_perf.hpp>
