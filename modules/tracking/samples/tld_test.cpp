0c610a5e63a2749081f398b4ced5d9534469a5d5
Vladimir <Terran.ITX@gmail.com>
2015-06-08 23:45:52 +0900

    Fix #2

--- modules/tracking/samples/tld_test.cpp
+++ modules/tracking/samples/tld_test.cpp
@@ -39,9 +39,10 @@
 //
 //M*/
 
-#include <opencv2\tracking.hpp>
-#include <opencv2\core\ocl.hpp>
-
+#include <opencv2/core/utility.hpp>
+#include <opencv2/tracking.hpp>
+#include <opencv2/videoio.hpp>
+#include <opencv2/highgui.hpp>
 #include <iostream>
 
 using namespace std;
