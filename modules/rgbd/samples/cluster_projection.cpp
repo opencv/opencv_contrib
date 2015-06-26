0ef816d58c3ccaad17dfe8cefb59025c8d7069e8
micuat <micuat@gmail.com>
2015-06-25 21:29:53 -0400

    Fix include files of clustering example

--- modules/rgbd/samples/cluster_projection.cpp
+++ modules/rgbd/samples/cluster_projection.cpp
@@ -33,10 +33,13 @@
  *
  */
 
-#include <opencv2/core/core.hpp>
-#include <opencv2/imgproc.hpp>
 #include <opencv2/rgbd.hpp>
-#include <opencv2/highgui/highgui.hpp>
+
+#include <opencv2/highgui.hpp>
+#include <opencv2/calib3d.hpp>
+#include <opencv2/imgproc.hpp>
+#include <opencv2/core/utility.hpp>
+
 #include <iostream>
 #include <fstream>
 #include <cmath>
