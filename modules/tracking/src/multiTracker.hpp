e8a23dbb229c3842238ef586f2555ed296a88db3
Vladimir <Terran.ITX@gmail.com>
2015-08-10 19:50:04 +0900

    Fixed  spaces  in   >>

--- modules/tracking/src/multiTracker.hpp
+++ modules/tracking/src/multiTracker.hpp
@@ -7,10 +7,10 @@
 
 namespace cv
 {
-	void detect_all(const Mat& img, const Mat& imgBlurred, std::vector<Rect2d>& res, std::vector < std::vector < tld::TLDDetector::LabeledPatch >> &patches,
-		std::vector<bool>& detect_flgs,	std::vector<Ptr<Tracker>>& trackers);
-	void ocl_detect_all(const Mat& img, const Mat& imgBlurred, std::vector<Rect2d>& res, std::vector < std::vector < tld::TLDDetector::LabeledPatch >> &patches,
-		std::vector<bool>& detect_flgs, std::vector<Ptr<Tracker>>& trackers);
+	void detect_all(const Mat& img, const Mat& imgBlurred, std::vector<Rect2d>& res, std::vector < std::vector < tld::TLDDetector::LabeledPatch > > &patches,
+		std::vector<bool>& detect_flgs,	std::vector<Ptr<Tracker> >& trackers);
+	void ocl_detect_all(const Mat& img, const Mat& imgBlurred, std::vector<Rect2d>& res, std::vector < std::vector < tld::TLDDetector::LabeledPatch > > &patches,
+		std::vector<bool>& detect_flgs, std::vector<Ptr<Tracker> >& trackers);
 	std::vector <Rect2d> debugStack[10];
 }
 #endif
