d50527ca8e67d7da1b5378ea2e0b90a8bf936223
micuat <micuat@gmail.com>
2015-08-12 17:38:34 -0400

    Mesh generation for projector-(color)camera

--- modules/rgbd/samples/cluster_projection.cpp
+++ modules/rgbd/samples/cluster_projection.cpp
@@ -60,22 +60,59 @@ using namespace std;
 
 int main( int argc, char** argv )
 {
+    int devId;
+    int lightThreshold;
+    int lightIntensity;
+    Size camSize, projSize;
+    bool useOpenni;
+    FileStorage fs("capturer_parameters.yml", FileStorage::Mode::READ);
+    fs["deviceId"] >> devId;
+    fs["lightThreshold"] >> lightThreshold;
+    fs["lightIntensity"] >> lightIntensity;
+    fs["projectorWidth"] >> projSize.width;
+    fs["projectorHeight"] >> projSize.height;
+    fs["useOpenni"] >> useOpenni;
+
     Mat projectorPixels;
-    Mat correspondenceMapX, correspondenceMapY;
-    correspondenceMapX = imread("correspondenceX.png");
-    correspondenceMapY = imread("correspondenceY.png");
+    {
+        Mat correspondenceMapX, correspondenceMapY;
+        correspondenceMapX = imread("correspondenceX.png");
+        correspondenceMapY = imread("correspondenceY.png");
+        camSize = correspondenceMapX.size();
+
+        projectorPixels = Mat::zeros(correspondenceMapX.size(), CV_32SC2);
+        for (int i = 0; i < camSize.height; i++)
+        {
+            for (int j = 0; j < camSize.width; j++)
+            {
+//                if (frame->mask.at<uchar>(i, j) == 0)
+//                    continue;
+
+                Vec3b sx = correspondenceMapX.at<Vec3b>(i, j);
+                Vec3b sy = correspondenceMapY.at<Vec3b>(i, j);
+                int x = (int)sx[1] * 256 + (int)sx[2];
+                int y = (int)sy[1] * 256 + (int)sy[2];
+                if (/*sx[0] == 255
+                    && sx[1] == 255
+                    && sx[2] == 255*/
+                    x < 0 || projSize.width <= x || y < 0 || projSize.height <= y)
+                {
+                    projectorPixels.at<Point2i>(i, j) = Point2i(-1, -1);
+                }
+                else
+                {
+                    projectorPixels.at<Point2i>(i, j) = Point2i(x, y);
+                }
+            }
+        }
+
+    }
 
     Mat image, depth;
     float focalLength;
     Mat cameraMatrix = Mat::eye(3, 3, CV_32FC1);
 
-    if (argc != 2) {
-        // bad arguments
-        argv[1] = "OPENNI";
-        //exit(1);
-    }
-
-    if (string(argv[1]) == "OPENNI")
+    if (useOpenni)
     {
         VideoCapture capture(CAP_OPENNI2);
 
@@ -125,99 +162,158 @@ int main( int argc, char** argv )
         cameraMatrix.at<float>(1, 2) = cy;
 
         cout << cameraMatrix << endl;
+
+        Ptr<RgbdFrame> frame = makePtr<RgbdFrame>(image, depth);
+        frame->cameraMatrix = cameraMatrix;
+
+        Ptr<DepthCleaner> cleaner = makePtr<DepthCleaner>(CV_32F, 5);
+        Mat tmp;
+        (*cleaner)(frame->depth, tmp);
+        frame->depth = tmp;
+
+        depthTo3d(*frame);
+
+        // generate valid point mask for clusters
+        compare(frame->depth, 0, frame->mask, CMP_GT);
+
+        imshow("depth", frame->points3d);
+        waitKey(30);
+
+        RgbdClusterMesh mainCluster(frame);
+        vector<RgbdClusterMesh> clusters;
+        planarSegmentation(mainCluster, clusters, 2);
+        deleteEmptyClusters(clusters);
+
+        for (std::size_t i = 0; i < clusters.size(); i++) {
+            Mat labels;
+            Mat stats;
+            Mat centroids;
+
+            if (clusters.at(i).bPlane) {
+                stringstream ss;
+                ss << "cluster" << i;
+                // downsample by 2x
+                clusters.at(i).increment_step = 2;
+                clusters.at(i).projectorPixels = projectorPixels;
+                clusters.at(i).calculatePoints(true);
+                clusters.at(i).unwrapTexCoord();
+                clusters.at(i).save(ss.str() + ".obj");
+                imshow(ss.str(), clusters.at(i).silhouette * 255);
+                continue;
+            }
+
+            vector<RgbdClusterMesh> smallClusters;
+            euclideanClustering(clusters.at(i), smallClusters);
+            //deleteEmptyClusters(smallClusters);
+            for (std::size_t j = 0; j < smallClusters.size(); j++) {
+                stringstream ss;
+                ss << "mesh_" << i << "_" << j;
+                imshow(ss.str(), smallClusters.at(j).silhouette * 255);
+
+                // no downsample
+                smallClusters.at(j).increment_step = 1;
+                smallClusters.at(j).projectorPixels = projectorPixels;
+                smallClusters.at(j).calculatePoints(true);
+                if (smallClusters.at(j).getNumPoints() == 0)
+                {
+                    continue;
+                }
+                smallClusters.at(j).unwrapTexCoord();
+                smallClusters.at(j).save(ss.str() + ".obj");
+            }
+        }
+
     }
     else
     {
-        // read depth data from libfreenect
-        cv::FileStorage file(string(argv[1]), cv::FileStorage::READ);
+        Subdiv2D subdiv(Rect(0, 0, projSize.width, projSize.height));
+        Mat correspondenceMapPro = Mat(projSize, CV_32S, -1);
 
-        float pixelSize, refDistance;
-        file["depth"] >> depth;
-        file["cameraMatrix"] >> cameraMatrix;
-        depth.convertTo(depth, CV_32F);
-    }
-
-    Ptr<RgbdFrame> frame = makePtr<RgbdFrame>(image, depth);
-    frame->cameraMatrix = cameraMatrix;
-
-    Ptr<DepthCleaner> cleaner = makePtr<DepthCleaner>(CV_32F, 5);
-    Mat tmp;
-    (*cleaner)(frame->depth, tmp);
-    frame->depth = tmp;
-
-    depthTo3d(*frame);
-
-    // generate valid point mask for clusters
-    compare(frame->depth, 0, frame->mask, CMP_GT);
-    projectorPixels = Mat::zeros(correspondenceMapX.size(), CV_32SC2);
-    for (int i = 0; i < frame->mask.rows; i++)
-    {
-        for (int j = 0; j < frame->mask.cols; j++)
+        int xMin = 1e5, xMax = -1e5, yMin = 1e5, yMax = -1e5;
+        int count = 0;
+        vector<Point2i> points;
+        for (int i = 0; i < camSize.height; i++)
         {
-            if (frame->mask.at<uchar>(i, j) == 0)
+            for (int j = 0; j < camSize.width; j++)
+            {
+                Point2i & p = projectorPixels.at<Point2i>(i, j);
+                if (p.x < 0 || p.y < 0)
+                {
+                    continue;
+                }
+                points.push_back(p);
+                subdiv.insert(p);
+                correspondenceMapPro.at<int>(p) = count;
+                count++;
+
+                xMin = min(xMin, p.x);
+                xMax = max(xMax, p.x);
+                yMin = min(yMin, p.y);
+                yMax = max(yMax, p.y);
+            }
+        }
+        Rect projRoi(xMin, yMin, xMax - xMin + 1, yMax - yMin + 1);
+
+        std::vector<Vec6f> triangleList;
+        subdiv.getTriangleList(triangleList);
+
+        vector<int> indices;
+
+        for (std::size_t i = 0; i < triangleList.size(); i++)
+        {
+            Point2i p0(triangleList.at(i)[0], triangleList.at(i)[1]);
+            Point2i p1(triangleList.at(i)[2], triangleList.at(i)[3]);
+            Point2i p2(triangleList.at(i)[4], triangleList.at(i)[5]);
+            if (!projRoi.contains(p0) || !projRoi.contains(p1) || !projRoi.contains(p2))
                 continue;
 
-            Vec3b sx = correspondenceMapX.at<Vec3b>(i, j);
-            Vec3b sy = correspondenceMapY.at<Vec3b>(i, j);
-            if (sx[0] == 255
-                && sx[1] == 255
-                && sx[2] == 255)
-            {
-                projectorPixels.at<Point2i>(i, j) = Point2i(-1, -1);
-            }
-            else
-            {
-                projectorPixels.at<Point2i>(i, j) = Point2i((int)sx[1] * 256 + (int)sx[2], (int)sy[1] * 256 + (int)sy[2]);
-            }
-        }
-    }
+            int v0 = correspondenceMapPro.at<int>(cvRound(triangleList.at(i)[1]), cvRound(triangleList.at(i)[0]));
+            int v1 = correspondenceMapPro.at<int>(cvRound(triangleList.at(i)[3]), cvRound(triangleList.at(i)[2]));
+            int v2 = correspondenceMapPro.at<int>(cvRound(triangleList.at(i)[5]), cvRound(triangleList.at(i)[4]));
 
-    imshow("depth", frame->points3d);
-    waitKey(30);
-
-    RgbdClusterMesh mainCluster(frame);
-    vector<RgbdClusterMesh> clusters;
-    planarSegmentation(mainCluster, clusters, 2);
-    deleteEmptyClusters(clusters);
-
-    for (std::size_t i = 0; i < clusters.size(); i++) {
-        Mat labels;
-        Mat stats;
-        Mat centroids;
-
-        if (clusters.at(i).bPlane) {
-            stringstream ss;
-            ss << "cluster" << i;
-            // downsample by 2x
-            clusters.at(i).increment_step = 2;
-            clusters.at(i).projectorPixels = projectorPixels;
-            clusters.at(i).calculatePoints(true);
-            clusters.at(i).unwrapTexCoord();
-            clusters.at(i).save(ss.str() + ".obj");
-            imshow(ss.str(), clusters.at(i).silhouette * 255);
-            continue;
+            indices.push_back(v0);
+            indices.push_back(v1);
+            indices.push_back(v2);
         }
 
-        vector<RgbdClusterMesh> smallClusters;
-        euclideanClustering(clusters.at(i), smallClusters);
-        //deleteEmptyClusters(smallClusters);
-        for (std::size_t j = 0; j < smallClusters.size(); j++) {
-            stringstream ss;
-            ss << "mesh_" << i << "_" << j;
-            imshow(ss.str(), smallClusters.at(j).silhouette * 255);
+        string path = "mesh.obj";
+        std::ofstream ofs(path.c_str(), std::ofstream::out);
+        for (std::size_t i = 0; i < points.size(); i++)
+        {
+            Point2i & x = points.at(i);
+            // negate xy for Unity compatibility
+            ofs << "v " << -x.x << " " << -x.y << " " << 0 << std::endl;
+        }
 
-            // no downsample
-            smallClusters.at(j).increment_step = 1;
-            smallClusters.at(j).projectorPixels = projectorPixels;
-            smallClusters.at(j).calculatePoints(true);
-            if (smallClusters.at(j).getNumPoints() == 0)
+        for (std::size_t i = 0; i < points.size(); i++)
+        {
+            Point2i & vt = points.at(i);
+            ofs << "vt " << vt.x << " " << vt.y << std::endl;
+        }
+
+        for (std::size_t i = 0; i < indices.size(); i += 3)
+        {
+            int i0 = indices.at(i);
+            int i2 = indices.at(i + 1);
+            int i1 = indices.at(i + 2);
+
+            float distanceThreshold = 10; // [px]
+            if (norm(points.at(i0) - points.at(i1)) > distanceThreshold
+                || norm(points.at(i1) - points.at(i2)) > distanceThreshold
+                || norm(points.at(i2) - points.at(i0)) > distanceThreshold)
             {
                 continue;
             }
-            smallClusters.at(j).unwrapTexCoord();
-            smallClusters.at(j).save(ss.str() + ".obj");
+
+            ofs << "f " << i0 + 1 << "/" << i0 + 1
+                << "/ " << i1 + 1 << "/" << i1 + 1
+                << "/ " << i2 + 1 << "/" << i2 + 1
+                << "/" << std::endl;
         }
+        ofs.close();
+
     }
+
 
     waitKey(0);
     return 0;
