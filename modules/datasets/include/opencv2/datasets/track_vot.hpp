13870922eca20132d46b8fddb357ba2397b53c9d
Vladimir <Terran.ITX@gmail.com>
2015-08-22 04:27:05 +0900

    Untabify VOT dataset code

--- modules/datasets/include/opencv2/datasets/track_vot.hpp
+++ modules/datasets/include/opencv2/datasets/track_vot.hpp
@@ -68,30 +68,30 @@ struct TRACK_votObj : public Object
 {
     int id;
     std::string imagePath;
-	vector <Point2d> gtbb;
+    vector <Point2d> gtbb;
 };
 
 class CV_EXPORTS TRACK_vot : public Dataset
 {
 public:
-	static Ptr<TRACK_vot> create();
+    static Ptr<TRACK_vot> create();
 
     virtual void load(const std::string &path) = 0;
 
-	virtual int getDatasetsNum() = 0;
+    virtual int getDatasetsNum() = 0;
 
-	virtual int getDatasetLength(int id) = 0;
+    virtual int getDatasetLength(int id) = 0;
 
-	virtual bool initDataset(int id) = 0;
+    virtual bool initDataset(int id) = 0;
 
-	virtual bool getNextFrame(Mat &frame) = 0;
+    virtual bool getNextFrame(Mat &frame) = 0;
 
-	virtual vector <Point2d> getGT() = 0;
+    virtual vector <Point2d> getGT() = 0;
 
 protected:
-	vector <vector <Ptr<TRACK_votObj> > > data;
-	int activeDatasetID;
-	int frameCounter;
+    vector <vector <Ptr<TRACK_votObj> > > data;
+    int activeDatasetID;
+    int frameCounter;
 };
 
 //! @}
