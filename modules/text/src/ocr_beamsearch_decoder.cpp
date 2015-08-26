c3042c3f5187da6a96918d1d60816e485d699861
Lluis Gomez-Bigorda <lgomez@cvc.uab.es>
2015-08-26 21:47:36 +0200

    remove trailing space

--- modules/text/src/ocr_beamsearch_decoder.cpp
+++ modules/text/src/ocr_beamsearch_decoder.cpp
@@ -371,7 +371,7 @@ private:
         Mat interdist ((int)segmentation.size()-1, 1, CV_32F, 1);
         for (size_t i=0; i<segmentation.size()-1; i++)
         {
-          interdist.at<float>((int)i,0) = (float)oversegmentation[segmentation[(int)i+1]]*step_size 
+          interdist.at<float>((int)i,0) = (float)oversegmentation[segmentation[(int)i+1]]*step_size
                                           - (float)oversegmentation[segmentation[(int)i]]*step_size;
           if ((float)interdist.at<float>((int)i,0)/win_size > 2.25) // TODO explain how did you set this thrs
           {
