9c80c40c366e2a7527a211d1039df44cb8f61193
Maksim Shabunin <maksim.shabunin@itseez.com>
2015-12-17 12:18:00 +0300

    Fixed sfm module static build

--- modules/sfm/src/libmv_light/CMake/Installation.cmake
+++ modules/sfm/src/libmv_light/CMake/Installation.cmake
@@ -1,9 +1,9 @@
 #Install macro for libmv libraries
 MACRO (LIBMV_INSTALL_LIB name)
 
-  set_target_properties( ${name}
-    PROPERTIES
-    ARCHIVE_OUTPUT_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}/lib
-  )
+if(NOT BUILD_SHARED_LIBS)
+  ocv_install_target(${name} EXPORT OpenCVModules
+    ARCHIVE DESTINATION ${OPENCV_3P_LIB_INSTALL_PATH} COMPONENT dev)
+endif()
 
 ENDMACRO (LIBMV_INSTALL_LIB)
