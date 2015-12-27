430ad2fc20f3aea1ab7e245f840339e149095f5d
Zheng Xing <superxingzheng@gmail.com>
2015-12-26 21:04:26 -0500

    Try to restore formating in order to show differences from origin.

--- modules/tracking/include/opencv2/tracking/tracker.hpp
+++ modules/tracking/include/opencv2/tracking/tracker.hpp
@@ -43,6 +43,7 @@
 #define __OPENCV_TRACKER_HPP__
 
 #include "opencv2/core.hpp"
+#include "opencv2/videoio.hpp"
 #include "opencv2/imgproc/types_c.h"
 #include "feature.hpp"
 #include "onlineMIL.hpp"
@@ -1338,7 +1339,9 @@ class ROISelector {
 public:
 	Rect2d select(Mat img, bool fromCenter = true);
 	Rect2d select(const cv::String& windowName, Mat img, bool showCrossair = true, bool fromCenter = true);
+	Rect2d select(const cv::String &windowName, cv::VideoCapture &cap, bool showCrossair = true, bool fromCenter = true);
 	void select(const cv::String& windowName, Mat img, std::vector<Rect2d> & boundingBox, bool fromCenter = true);
+	void select(const cv::String &windowName, cv::VideoCapture &cap, std::vector<Rect2d> &boundingBox, bool fromCenter = true);
 
 	struct handlerT{
 		// basic parameters
