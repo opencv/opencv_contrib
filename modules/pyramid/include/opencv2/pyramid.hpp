55a48de763e1da7ebb7ca069df2da91c9ffe2110
LaurentBerger <laurent.berger@univ-lemans.fr>
2016-03-14 19:06:46 +0100

    Try doc 1

--- modules/pyramid/include/opencv2/pyramid.hpp
+++ modules/pyramid/include/opencv2/pyramid.hpp
@@ -1,14 +1,59 @@
-///////////// see LICENSE.txt in the OpenCV root directory //////////////
+/*
+By downloading, copying, installing or using the software you agree to this
+license. If you do not agree to this license, do not download, install,
+copy or use the software.
+
+
+                          License Agreement
+               For Open Source Computer Vision Library
+                       (3-clause BSD License)
+
+Copyright (C) 2013, OpenCV Foundation, all rights reserved.
+Third party copyrights are property of their respective owners.
+
+Redistribution and use in source and binary forms, with or without modification,
+are permitted provided that the following conditions are met:
+
+  * Redistributions of source code must retain the above copyright notice,
+    this list of conditions and the following disclaimer.
+
+  * Redistributions in binary form must reproduce the above copyright notice,
+    this list of conditions and the following disclaimer in the documentation
+    and/or other materials provided with the distribution.
+
+  * Neither the names of the copyright holders nor the names of the contributors
+    may be used to endorse or promote products derived from this software
+    without specific prior written permission.
+
+This software is provided by the copyright holders and contributors "as is" and
+any express or implied warranties, including, but not limited to, the implied
+warranties of merchantability and fitness for a particular purpose are
+disclaimed. In no event shall copyright holders or contributors be liable for
+any direct, indirect, incidental, special, exemplary, or consequential damages
+(including, but not limited to, procurement of substitute goods or services;
+loss of use, data, or profits; or business interruption) however caused
+and on any theory of liability, whether in contract, strict liability,
+or tort (including negligence or otherwise) arising in any way out of
+the use of this software, even if advised of the possibility of such damage.
+*/
 
 #ifndef __OPENCV_PYRAMID_HPP__
 #define __OPENCV_PYRAMID_HPP__
 #include "opencv2/core.hpp"
 #include <vector>
 
+/** @defgroup pyramid hierarchical pyramid @cite Wadhwa2014RieszPyramid @cite Burt:87
+*/
+
 namespace cv
 {
 namespace pyramid
 {
+//! @addtogroup pyramid
+//! @{
+
+/** @brief virtual class pyramid Pyramid  .
+ */
 
 
     enum {
@@ -42,7 +87,8 @@ public :
     ~Pyramid(){}
 };
 
-/* Not ready */
+/** @brief  class GaussianPyramid TO DO .
+ */
 class CV_EXPORTS_W GaussianPyramid:public Pyramid {
 public :
     GaussianPyramid(Mat m);
@@ -50,6 +96,8 @@ public :
 
 };
 
+/** @brief  class LaplacianPyramid  BURT ADELSON pyramid and Laplacian like pyramid.
+ */
 
 class CV_EXPORTS_W LaplacianPyramid :public Pyramid {
     Mat lowPassFilter;
@@ -67,6 +115,8 @@ public:
 };
 
 
+/** @brief  class PyramidRiesz  .
+ */
 
 class CV_EXPORTS_W PyramidRiesz:public Pyramid {
 
