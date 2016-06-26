6544bff403a3ba0a6e675f44a4f14a5fdf2b3229
stereomatchingkiss <stereomatchingkiss@gmail.com>
2016-06-27 02:02:29 +0800

    first commit

---
+++ modules/img_hash/include/opencv2/img_hash/color_moment_hash.hpp
@@ -0,0 +1,98 @@
+/*M///////////////////////////////////////////////////////////////////////////////////////
+//
+//  IMPORTANT: READ BEFORE DOWNLOADING, COPYING, INSTALLING OR USING.
+//
+//  By downloading, copying, installing or using the software you agree to this license.
+//  If you do not agree to this license, do not download, install,
+//  copy or use the software.
+//
+//
+//                           License Agreement
+//                For Open Source Computer Vision Library
+//
+// Copyright (C) 2015, University of Ostrava, Institute for Research and Applications of Fuzzy Modeling,
+// Pavel Vlasanek, all rights reserved. Third party copyrights are property of their respective owners.
+//
+// Redistribution and use in source and binary forms, with or without modification,
+// are permitted provided that the following conditions are met:
+//
+//   * Redistribution's of source code must retain the above copyright notice,
+//     this list of conditions and the following disclaimer.
+//
+//   * Redistribution's in binary form must reproduce the above copyright notice,
+//     this list of conditions and the following disclaimer in the documentation
+//     and/or other materials provided with the distribution.
+//
+//   * The name of the copyright holders may not be used to endorse or promote products
+//     derived from this software without specific prior written permission.
+//
+// This software is provided by the copyright holders and contributors "as is" and
+// any express or implied warranties, including, but not limited to, the implied
+// warranties of merchantability and fitness for a particular purpose are disclaimed.
+// In no event shall the Intel Corporation or contributors be liable for any direct,
+// indirect, incidental, special, exemplary, or consequential damages
+// (including, but not limited to, procurement of substitute goods or services;
+// loss of use, data, or profits; or business interruption) however caused
+// and on any theory of liability, whether in contract, strict liability,
+// or tort (including negligence or otherwise) arising in any way out of
+// the use of this software, even if advised of the possibility of such damage.
+//
+//M*/
+
+#ifndef __OPENCV_COLOR_MOMENT_HASH_HPP__
+#define __OPENCV_COLOR_MOMENT_HASH_HPP__
+
+#include "opencv2/core.hpp"
+
+namespace cv
+{
+
+namespace img_hash
+{
+    //! @addtogroup color_moment_hash
+    //! @{
+
+    /** @brief Computes color moment hash of the input, the algorithm
+     is come from the paper "Perceptual  Hashing  for  Color  Images
+     Using  Invariant Moments"
+    @param input Input CV_8UC3
+    @param hash 42 hash values with type CV_64F(double)
+     */
+    CV_EXPORTS void colorMomentHash(cv::Mat const &input, cv::Mat &hash);
+
+    class ColorMomentHash : public ImgHashBase
+    {
+    public:
+      /** @brief Computes color moment hash of the input, the algorithm
+       is come from the paper "Perceptual  Hashing  for  Color  Images
+       Using  Invariant Moments"
+      @param input Input CV_8UC3
+      @param hash 42 hash values with type CV_64F(double)
+       */
+      CV_EXPORTS virtual void compute(cv::Mat const &input, cv::Mat &hash);
+      CV_EXPORTS ~ColorMomentHash();
+
+      /** @brief Compare the hash value between inOne and inTwo
+      @param hashOne Hash value one
+      @param hashTwo Hash value two
+      @return The smaller the value, more similar the hash.
+      Recommend threshold is 10
+      */
+      CV_EXPORTS virtual double compare(cv::Mat const &hashOne, cv::Mat const &hashTwo) const;
+
+      CV_EXPORTS static Ptr<ColorMomentHash> create();
+
+    private:
+      void computeMoments(double *inout);
+
+      cv::Mat blurImg_;
+      std::vector<cv::Mat> channels_;
+      cv::Mat colorSpace_;
+      cv::Mat resizeImg_;
+    };
+
+    //! @}
+}
+}
+
+#endif // __OPENCV_PHASH_HPP__
---
+++ modules/img_hash/src/color_moment_hash.cpp
@@ -0,0 +1,101 @@
+/*M///////////////////////////////////////////////////////////////////////////////////////
+//
+//  IMPORTANT: READ BEFORE DOWNLOADING, COPYING, INSTALLING OR USING.
+//
+//  By downloading, copying, installing or using the software you agree to this license.
+//  If you do not agree to this license, do not download, install,
+//  copy or use the software.
+//
+//
+//                           License Agreement
+//                For Open Source Computer Vision Library
+//
+// Copyright (C) 2015, University of Ostrava, Institute for Research and Applications of Fuzzy Modeling,
+// Pavel Vlasanek, all rights reserved. Third party copyrights are property of their respective owners.
+//
+// Redistribution and use in source and binary forms, with or without modification,
+// are permitted provided that the following conditions are met:
+//
+//   * Redistribution's of source code must retain the above copyright notice,
+//     this list of conditions and the following disclaimer.
+//
+//   * Redistribution's in binary form must reproduce the above copyright notice,
+//     this list of conditions and the following disclaimer in the documentation
+//     and/or other materials provided with the distribution.
+//
+//   * The name of the copyright holders may not be used to endorse or promote products
+//     derived from this software without specific prior written permission.
+//
+// This software is provided by the copyright holders and contributors "as is" and
+// any express or implied warranties, including, but not limited to, the implied
+// warranties of merchantability and fitness for a particular purpose are disclaimed.
+// In no event shall the Intel Corporation or contributors be liable for any direct,
+// indirect, incidental, special, exemplary, or consequential damages
+// (including, but not limited to, procurement of substitute goods or services;
+// loss of use, data, or profits; or business interruption) however caused
+// and on any theory of liability, whether in contract, strict liability,
+// or tort (including negligence or otherwise) arising in any way out of
+// the use of this software, even if advised of the possibility of such damage.
+//
+//M*/
+
+#include "precomp.hpp"
+
+#include <iostream>
+
+namespace cv{
+
+namespace img_hash{
+
+ColorMomentHash::~ColorMomentHash()
+{
+
+}
+
+void ColorMomentHash::compute(const Mat &input, Mat &hash)
+{
+  CV_Assert(input.type() == CV_8UC3);
+
+  cv::resize(input, resizeImg_, cv::Size(512,512), 0, 0,
+             INTER_CUBIC);
+  cv::GaussianBlur(resizeImg_, blurImg_, cv::Size(3,3), 0, 0);
+
+  cv::cvtColor(blurImg_, colorSpace_, CV_BGR2HSV);
+  cv::split(colorSpace_, channels_);
+  hash.create(1, 42, CV_64F);
+  hash.setTo(0);
+  computeMoments(hash.ptr<double>(0));
+
+  cv::cvtColor(blurImg_, colorSpace_, CV_BGR2YCrCb);
+  cv::split(colorSpace_, channels_);
+  computeMoments(hash.ptr<double>(0) + 21);
+}
+
+double ColorMomentHash::compare(cv::Mat const &hashOne, cv::Mat const &hashTwo) const
+{
+  return norm(hashOne, hashTwo, NORM_L2) * 10000;
+}
+
+Ptr<ColorMomentHash> ColorMomentHash::create()
+{
+  return makePtr<ColorMomentHash>();
+}
+
+void ColorMomentHash::computeMoments(double *inout)
+{
+  for(size_t i = 0; i != channels_.size(); ++i)
+  {
+    auto mm = cv::moments(channels_[i]);
+    cv::HuMoments(cv::moments(channels_[i]), inout);
+    inout += 7;
+  }
+}
+
+void colorMomentHash(cv::Mat const &input, cv::Mat &hash)
+{
+  ColorMomentHash().compute(input, hash);
+}
+
+}
+
+}
