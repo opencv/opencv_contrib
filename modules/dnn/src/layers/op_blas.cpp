ddda13bc671e76b115a830b05fbd0157ec134831
Vadim Pisarevsky <vadim.pisarevsky@gmail.com>
2016-09-18 22:54:46 +0300

    fixed some compile-time and runtime problems in semantical segmentation sample (thanks to Vladimir Bystritsky for the "null bias" fix in convolution_layer.cpp); made gemmCPU() parallel (probably need to do that in core too)

--- modules/dnn/src/layers/op_blas.cpp
+++ modules/dnn/src/layers/op_blas.cpp
@@ -28,8 +28,67 @@ inline void SwapRowCols(const Mat &A, int &rows, int &cols, bool isTrans)
     cols = (isTrans) ? A.rows : A.cols;
 }
 
-void gemmCPU(const Mat &A, const Mat &B, double alpha, Mat &C, double beta, int flags /*= 0*/)
+
+class GEMMInvoker : public ParallelLoopBody
 {
+public:
+    GEMMInvoker(const Mat* _a, const Mat* _b, double _alpha, Mat* _c, double _beta)
+    {
+        a = _a;
+        b = _b;
+        c = _c;
+        alpha = _alpha;
+        beta = _beta;
+    }
+
+    void operator()(const Range& range) const
+    {
+        int mmax = a->rows;
+        int nmax = range.end - range.start;
+        int kmax = a->cols;
+        int m, n, k;
+
+        for( m = 0; m < mmax; m++ )
+        {
+            float* dst = c->ptr<float>(m) + range.start;
+            if( beta != 1 )
+            {
+                if( beta == 0 )
+                    for( n = 0; n < nmax; n++ )
+                        dst[n] = 0.f;
+                else
+                    for( n = 0; n < nmax; n++ )
+                        dst[n] *= (float)beta;
+            }
+            const float* aptr = a->ptr<float>(m);
+
+            for( k = 0; k < kmax; k++ )
+            {
+                float alpha1 = (float)(alpha*aptr[k]);
+                const float* bptr = b->ptr<float>(k) + range.start;
+
+                for( n = 0; n < nmax; n++ )
+                    dst[n] += alpha1*bptr[n];
+            }
+        }
+    }
+
+    const Mat *a, *b;
+    Mat* c;
+    double alpha, beta;
+};
+
+
+void gemmCPU(const Mat &A, const Mat &B, double alpha, Mat &C, double beta, int flags)
+{
+    if( C.type() == CV_32F && flags == 0 )
+    {
+        GEMMInvoker invoker(&A, &B, alpha, &C, beta);
+        double granularity = 10000000./((double)A.rows*A.cols);
+        parallel_for_(Range(0, B.cols), invoker, granularity);
+    }
+    else
+    {
     #if HAVE_CBLAS
     bool transA = static_cast<bool>(flags & GEMM_1_T);
     bool transB = static_cast<bool>(flags & GEMM_2_T);
@@ -70,6 +129,7 @@ void gemmCPU(const Mat &A, const Mat &B, double alpha, Mat &C, double beta, int 
     #else
     cv::gemm(A, B, alpha, C, beta, C, flags);
     #endif
+    }
 }
 
 int getBlasThreads()
