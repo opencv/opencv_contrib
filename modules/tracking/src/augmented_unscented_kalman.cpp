36f64dd17fc4d6830baa937c47c0124d4fd50d5f
Vladislav Sovrasov <sovrasov.vlad@gmail.com>
2016-11-22 14:26:01 +0300

    Remove duplicated code in UnscentedKalmanFilter

--- modules/tracking/src/augmented_unscented_kalman.cpp
+++ modules/tracking/src/augmented_unscented_kalman.cpp
@@ -47,50 +47,6 @@ namespace cv
 namespace tracking
 {
 
-/* Cholesky decomposition
- The function performs Cholesky decomposition <https://en.wikipedia.org/wiki/Cholesky_decomposition>.
- A - the Hermitian, positive-definite matrix,
- astep - size of row in A,
- asize - number of cols and rows in A,
- L - the lower triangular matrix, A = L*Lt.
-*/
-template<typename _Tp> bool
-inline choleskyDecomposition( const _Tp* A, size_t astep, const int asize, _Tp* L )
-{
-    int i, j, k;
-    double s;
-    astep /= sizeof(A[0]);
-    for( i = 0; i < asize; i++ )
-
-    {
-        for( j = 0; j < i; j++ )
-        {
-            s = A[i*astep + j];
-            for( k = 0; k < j; k++ )
-                s -= L[i*astep + k]*L[j*astep + k];
-            L[i*astep + j] = (_Tp)(s/L[j*astep + j]);
-        }
-        s = A[i*astep + i];
-        for( k = 0; k < i; k++ )
-        {
-            double t = L[i*astep + k];
-            s -= t*t;
-        }
-        if( s < std::numeric_limits<_Tp>::epsilon() )
-            return false;
-        L[i*astep + i] = (_Tp)(std::sqrt(s));
-    }
-
-   for( i = 0; i < asize; i++ )
-       for( j = i+1; j < asize; j++ )
-       {
-           L[i*astep + j] = 0.0;
-       }
-
-    return true;
-}
-
-
 void AugmentedUnscentedKalmanFilterParams::
     init( int dp, int mp, int cp, double processNoiseCovDiag, double measurementNoiseCovDiag,
                                 Ptr<UkfSystemModel> dynamicalSystem, int type )
@@ -179,10 +135,6 @@ class AugmentedUnscentedKalmanFilterImpl: public UnscentedKalmanFilter
     Mat r;                                      // zero vector of process noise for getting transitionSPFuncVals,
     Mat q;                                      // zero vector of measurement noise for getting measurementSPFuncVals
 
-
-    template <typename T>
-    Mat getSigmaPoints(const Mat& mean, const Mat& covMatrix, double coef);
-
     Mat getSigmaPoints(const Mat& mean, const Mat& covMatrix, double coef);
 
 public:
@@ -190,8 +142,8 @@ public:
     AugmentedUnscentedKalmanFilterImpl(const AugmentedUnscentedKalmanFilterParams& params);
     ~AugmentedUnscentedKalmanFilterImpl();
 
-    Mat predict(const Mat& control);
-    Mat correct(const Mat& measurement);
+    Mat predict(InputArray control);
+    Mat correct(InputArray measurement);
 
     Mat getProcessNoiseCov() const;
     Mat getMeasurementNoiseCov() const;
@@ -303,7 +255,6 @@ AugmentedUnscentedKalmanFilterImpl::~AugmentedUnscentedKalmanFilterImpl()
 
 }
 
-template <typename T>
 Mat AugmentedUnscentedKalmanFilterImpl::getSigmaPoints(const Mat &mean, const Mat &covMatrix, double coef)
 {
 // x_0 = mean
@@ -313,11 +264,18 @@ Mat AugmentedUnscentedKalmanFilterImpl::getSigmaPoints(const Mat &mean, const Ma
     int n = mean.rows;
     Mat points = repeat(mean, 1, 2*n+1);
 
-// covMatrixL = cholesky( covMatrix )
     Mat covMatrixL = covMatrix.clone();
-    covMatrixL.setTo(0);
 
-    choleskyDecomposition<T>( covMatrix.ptr<T>(), covMatrix.step, covMatrix.rows, covMatrixL.ptr<T>() );
+// covMatrixL = cholesky( covMatrix )
+    if ( dataType == CV_64F )
+        choleskyDecomposition<double>(
+                    covMatrix.ptr<double>(), covMatrix.step, covMatrix.rows,
+                    covMatrixL.ptr<double>(), covMatrixL.step );
+    else if ( dataType == CV_32F )
+        choleskyDecomposition<float>(
+                    covMatrix.ptr<float>(), covMatrix.step, covMatrix.rows,
+                    covMatrixL.ptr<float>(), covMatrixL.step );
+
     covMatrixL = coef * covMatrixL;
 
     Mat p_plus = points( Rect( 1, 0, n, n ) );
@@ -329,16 +287,9 @@ Mat AugmentedUnscentedKalmanFilterImpl::getSigmaPoints(const Mat &mean, const Ma
     return points;
 }
 
-Mat AugmentedUnscentedKalmanFilterImpl::getSigmaPoints(const Mat& mean, const Mat& covMatrix, double coef)
+Mat AugmentedUnscentedKalmanFilterImpl::predict(InputArray _control)
 {
-    if ( dataType == CV_64F ) return getSigmaPoints<double>(mean, covMatrix, coef);
-    if ( dataType == CV_32F ) return getSigmaPoints<float>(mean, covMatrix, coef);
-
-    return Mat();
-}
-
-Mat AugmentedUnscentedKalmanFilterImpl::predict(const Mat& control)
-{
+    Mat control = _control.getMat();
 // get sigma points from xa* and Pa
     sigmaPoints = getSigmaPoints( stateAug, errorCovAug, sqrt( tmpLambda ) );
 
@@ -368,8 +319,9 @@ Mat AugmentedUnscentedKalmanFilterImpl::predict(const Mat& control)
     return state.clone();
 }
 
-Mat AugmentedUnscentedKalmanFilterImpl::correct(const Mat& measurement)
+Mat AugmentedUnscentedKalmanFilterImpl::correct(InputArray _measurement)
 {
+    Mat measurement = _measurement.getMat();
 // get sigma points from xa* and Pa
     sigmaPoints = getSigmaPoints( stateAug, errorCovAug, sqrt( tmpLambda ) );
 
