478baf937fd221dc15a4d16608b48880d1555f60
goldstar616 <goldstar616@gmail.com>
2017-02-08 23:29:20 +0900

    Merge pull request #981 from goldstar616:master
    issue #980 resolves - Some module has a round error similar to #975.  (#981)
    
    * round function changes to cvRound
    
    Minor changes to fix lower visual studio version build errors.
    
    * round function changes to cvRound
    
    Minor changes to fix lower visual studio version build errors.
    
    * round function changes to cvRound
    
    Minor changes to fix lower visual studio version build errors.

--- modules/text/src/ocr_beamsearch_decoder.cpp
+++ modules/text/src/ocr_beamsearch_decoder.cpp
@@ -566,7 +566,7 @@ OCRBeamSearchClassifierCNN::OCRBeamSearchClassifierCNN (const string& filename)
 
     nr_feature = weights.rows;
     nr_class   = weights.cols;
-    patch_size  = (int)round(sqrt((float)kernels.cols));
+    patch_size  = cvRound(sqrt((float)kernels.cols));
     window_size = 4*patch_size;
     step_size   = 4;
     quad_size   = 12;
--- modules/text/src/ocr_hmm_decoder.cpp
+++ modules/text/src/ocr_hmm_decoder.cpp
@@ -982,7 +982,7 @@ OCRHMMClassifierCNN::OCRHMMClassifierCNN (const string& filename)
 
     nr_feature  = weights.rows;
     nr_class    = weights.cols;
-    patch_size  = (int)round(sqrt((float)kernels.cols));
+    patch_size  = cvRound(sqrt((float)kernels.cols));
     // algorithm internal parameters
     window_size = 32;
     num_quads   = 25;
