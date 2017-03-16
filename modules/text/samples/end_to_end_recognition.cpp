4e229e8c5247d7bf3bdf4b8a7b8852c6720dd6b7
Haaris <hsed@users.noreply.github.com>
2017-03-16 23:34:59 +0000

    fixed typo in comment
    Line 139: Fixed typo of word "length".

--- modules/text/samples/end_to_end_recognition.cpp
+++ modules/text/samples/end_to_end_recognition.cpp
@@ -136,7 +136,7 @@ int main(int argc, char* argv[])
         ocr->run(group_img, output, &boxes, &words, &confidences, OCR_LEVEL_WORD);
 
         output.erase(remove(output.begin(), output.end(), '\n'), output.end());
-        //cout << "OCR output = \"" << output << "\" lenght = " << output.size() << endl;
+        //cout << "OCR output = \"" << output << "\" length = " << output.size() << endl;
         if (output.size() < 3)
             continue;
 
