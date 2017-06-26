c6756c0e1c4a895dd072384bc9cb8dc27780569c
Shengxin Qian <qsxyh123@gmail.com>
2017-06-26 15:04:43 -0400

    redo first pull request

--- modules/saliency/src/DeepGaze1.cpp
+++ modules/saliency/src/DeepGaze1.cpp
@@ -267,6 +267,7 @@ double DeepGaze1::computeAUC( InputArray _saliencyMap, InputArray _fixtionMap )
     if ( saliency.empty() || fixtion.empty() || saliency.dims > 2 || fixtion.dims > 2 )
     {
         cout << "saliency map and fixtion map must be 1 channel and have same size" << endl;
+        CV_
         return -1;
     }
 
