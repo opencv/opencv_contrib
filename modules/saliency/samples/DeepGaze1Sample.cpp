7d03b3a246deaedd9a28a17af1c72b93bdebeab6
Shengxin Qian <qsxyh123@gmail.com>
2017-06-27 23:28:13 -0400

    fix datasets saliency sample bug

--- modules/saliency/samples/DeepGaze1Sample.cpp
+++ modules/saliency/samples/DeepGaze1Sample.cpp
@@ -6,7 +6,7 @@
 #include <opencv2/imgproc.hpp>
 #include <opencv2/highgui.hpp>
 #include <opencv2/saliency.hpp>
-#include "opencv2/datasets/saliency_mit1003.hpp"
+//#include <opencv2/datasets/saliency_mit1003.hpp>
 #include <vector>
 #include <string>
 #include <iostream>
@@ -16,7 +16,7 @@ using namespace std;
 using namespace cv;
 using namespace cv::dnn;
 using namespace cv::saliency;
-using namespace cv::datasets;
+//using namespace cv::datasets;
 /* Find best class for the blob (i. e. class with maximal probability) */
 
 int main()
@@ -29,20 +29,21 @@ int main()
 //ALLSTIMULI folder store images
 //ALLFIXATIONMAPS foler store training eye fixation
 //************ Code only work in linux platform ****
-    string dataset_path;
+
+/*    string dataset_path;
 
     cin >> dataset_path;
     Ptr<SALIENCY_mit1003> datasetConnector = SALIENCY_mit1003::create();
     datasetConnector->load( dataset_path );
     vector<vector<Mat> > dataset( datasetConnector->getDataset() );
 
-    //g.training( dataset[0], dataset[1] );
-
+    g.training( dataset[0], dataset[1] );
+*/
     ofstream file;
     Mat res2;
-    g.computeSaliency( dataset[0][0], res2 );
+    g.computeSaliency( imread( "ALLSTIMULI/i05june05_static_street_boston_p1010764.jpeg"), res2 );
     resize( res2, res2, Size( 1024, 768 ) );
-    cout << "AUC = " << g.computeAUC( res2, dataset[1][0] ) << endl;
+    cout << "AUC = " << g.computeAUC( res2, imread( "ALLFIXATIONMAPS/i05june05_static_street_boston_p1010764_fixMap.jpg", 0 ) ) << endl;;
     g.saliencyMapVisualize( res2 );
     file.open( "saliency.csv" );
     for ( int i = 0; i < res2.rows; i++)
