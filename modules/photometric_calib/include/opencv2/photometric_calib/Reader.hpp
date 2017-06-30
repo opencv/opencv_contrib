616c25ef6caa3b6250bd73729f7cb0024715661d
Nan Yang <yangnan719@gmail.com>
2017-06-30 13:21:48 +0200

    add Doxygen documentation

--- modules/photometric_calib/include/opencv2/photometric_calib/Reader.hpp
+++ modules/photometric_calib/include/opencv2/photometric_calib/Reader.hpp
@@ -15,15 +15,45 @@ namespace cv { namespace photometric_calib{
 //! @addtogroup photometric_calib
 //! @{
 
+/*!
+ * @brief Class for reading the sequence used for photometric calibration. Both the folder path of the sequence
+ * and the path of time file should be provided. The images of the sequence should be of format CV_8U. The time
+ * file should be .yaml or .yml. In the time file, the timestamps and exposure duration of the corresponding images
+ * of the sequence should be provided.
+ *
+ * The image paths are stored in std::vector<String> images, timestamps are stored in std::vector<double> timeStamps,
+ * exposure duration is stored in std::vector<float> exposureTimes
+ */
+
 class Reader
 {
 public:
+    /*!
+     * @brief Constructor
+     * @param folderPath the path of folder which contains the images
+     * @param timesPath the path of time file
+     */
     Reader(const std::string &folderPath, const std::string &timesPath);
 
+    /*!
+     * @return the amount of images loaded
+     */
     unsigned long getNumImages() const;
 
+
+    /*!
+     * @brief Given the id of the image and return its timestamp value. id is in fact just the index of the image in the
+     * vector contains all the images.
+     * @param id
+     * @return timestamp of the id^th image.
+     */
     double getTimestamp(unsigned long id) const;
 
+    /*!
+     * @brief Given the id of the image and return its exposure duration when is was taken.
+     * @param id
+     * @return exposure duration of the image.
+     */
     float getExposureDuration(unsigned long id) const;
 
     int getWidth() const;
@@ -31,6 +61,10 @@ public:
     const std::string &getFolderPath() const;
 
 private:
+    /*!
+     * @brief Load timestamps and exposure duration.
+     * @param timesFile
+     */
     inline void loadTimestamps(const std::string &timesFile);
 
     std::vector<String> images; //All the names/paths of images
