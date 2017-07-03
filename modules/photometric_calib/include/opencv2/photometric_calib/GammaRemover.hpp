5c04c0295d1fcbdc95e1b74fdcb3d8d0895c9ed6
Nan Yang <yangnan719@gmail.com>
2017-07-03 15:31:02 +0200

    Camera response function(Gamma function) remover.

---
+++ modules/photometric_calib/include/opencv2/photometric_calib/GammaRemover.hpp
@@ -0,0 +1,41 @@
+// This file is part of OpenCV project.
+// It is subject to the license terms in the LICENSE file found in the top-level directory
+// of this distribution and at http://opencv.org/license.html.
+
+#ifndef _OPENCV_GAMMAREMOVER_HPP
+#define _OPENCV_GAMMAREMOVER_HPP
+
+#include "opencv2/photometric_calib.hpp"
+
+namespace cv { namespace photometric_calib {
+
+class CV_EXPORTS GammaRemover
+{
+public:
+    GammaRemover(const std::string &gammaPath, int w_, int h_);
+
+    Mat getUnGammaImageMat(Mat inputIm);
+    void getUnGammaImageArr(Mat inputIm, std::vector<float> &outImVec);
+
+    inline float* getG()
+    {
+        if(!validGamma) return nullptr;
+        else return G;
+    };
+    inline float* getGInv()
+    {
+        if(!validGamma) return nullptr;
+        else return GInv;
+    };
+
+private:
+    float G[256];
+    float GInv[256];
+    int w,h;
+    bool validGamma;
+};
+
+
+}}
+
+#endif //_OPENCV__GAMMAREMOVER_HPP
