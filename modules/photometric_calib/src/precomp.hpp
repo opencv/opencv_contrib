2556e8cc868b898e768ae21d27c7e00f9d50f8c2
Nan Yang <yangnan719@gmail.com>
2017-07-30 18:17:46 +0200

    add support header files for camera undistortion and aruco marker detector for vignette calibration

--- modules/photometric_calib/src/precomp.hpp
+++ modules/photometric_calib/src/precomp.hpp
@@ -8,5 +8,7 @@
 #include "opencv2/core.hpp"
 #include "opencv2/imgproc.hpp"
 #include "opencv2/highgui.hpp"
+#include "opencv2/aruco.hpp"
+#include "opencv2/calib3d/calib3d.hpp"
 #include <vector>
 #endif
