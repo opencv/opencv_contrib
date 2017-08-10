56f1fdebba507c330350ef951774926e203b6f2d
THUKey <304600203@qq.com>
2017-08-10 04:54:50 +0200

    use boost unordered_map

--- modules/ximgproc/src/fbs_filter.cpp
+++ modules/ximgproc/src/fbs_filter.cpp
@@ -54,14 +54,14 @@
 
 
 #include <cmath>
-#include <chrono>
 #include <vector>
 #include <memory>
 #include <stdlib.h>
 #include <iostream>
 #include <iterator>
 #include <algorithm>
-#include <unordered_map>
+// #include <unordered_map>
+#include<boost/unordered_map.hpp>
 
 namespace cv
 {
@@ -83,7 +83,7 @@ namespace ximgproc
 
         // FastBilateralSolverFilterImpl(){}
 
-        void filter(InputArray& src, InputArray& confidence, OutputArray& dst)
+        void filter(InputArray src, InputArray confidence, OutputArray dst)
         {
 
             CV_Assert( src.type() == CV_8UC1 && confidence.type() == CV_8UC1 && src.size() == confidence.size() );
@@ -118,7 +118,7 @@ namespace ximgproc
         int rows;
         std::vector<Eigen::SparseMatrix<float, Eigen::ColMajor> > blurs;
         std::vector<int> splat_idx;
-        std::vector<std::pair<int, int>> blur_idx;
+        std::vector<std::pair<int, int> > blur_idx;
         Eigen::VectorXf m;
         Eigen::VectorXf n;
         Eigen::SparseMatrix<float, Eigen::ColMajor> blurs_test;
@@ -167,16 +167,14 @@ namespace ximgproc
 	      cv::Mat reference_yuv;
 	      cv::cvtColor(reference_bgr, reference_yuv, COLOR_BGR2YCrCb);
 
-	      std::chrono::steady_clock::time_point begin_grid_construction = std::chrono::steady_clock::now();
-
 	      cols = reference_yuv.cols;
 	      rows = reference_yuv.rows;
         npixels = cols*rows;
-	      std::int64_t hash_vec[5];
+	      long long hash_vec[5];
 	      for (int i = 0; i < 5; ++i)
-		        hash_vec[i] = static_cast<std::int64_t>(std::pow(255, i));
+		        hash_vec[i] = static_cast<long long>(std::pow(255, i));
 
-	      std::unordered_map<std::int64_t /* hash */, int /* vert id */> hashed_coords;
+	      boost::unordered_map<long long /* hash */, int /* vert id */> hashed_coords;
 	      hashed_coords.reserve(cols*rows);
 
 	      const unsigned char* pref = (const unsigned char*)reference_yuv.data;
@@ -190,7 +188,7 @@ namespace ximgproc
       	{
     	    for (int x = 0; x < cols; ++x)
       		{
-      			std::int64_t coord[5];
+      			long long coord[5];
       			coord[0] = int(x / sigma_spatial);
       			coord[1] = int(y / sigma_spatial);
       			coord[2] = int(pref[0] / sigma_luma);
@@ -198,17 +196,17 @@ namespace ximgproc
       			coord[4] = int(pref[2] / sigma_chroma);
 
       			// convert the coordinate to a hash value
-      			std::int64_t hash_coord = 0;
+      			long long hash_coord = 0;
       			for (int i = 0; i < 5; ++i)
       				  hash_coord += coord[i] * hash_vec[i];
 
       			// pixels whom are alike will have the same hash value.
       			// We only want to keep a unique list of hash values, therefore make sure we only insert
       			// unique hash values.
-      			auto it = hashed_coords.find(hash_coord);
+      			boost::unordered_map<long long,int>::iterator it = hashed_coords.find(hash_coord);
       			if (it == hashed_coords.end())
       			{
-      				hashed_coords.insert(std::pair<std::int64_t, int>(hash_coord, vert_idx));
+      				hashed_coords.insert(std::pair<long long, int>(hash_coord, vert_idx));
               splat_idx[pix_idx] = vert_idx;
       				++vert_idx;
       			}
@@ -225,7 +223,6 @@ namespace ximgproc
 
 
       	// construct Blur matrices
-      	std::chrono::steady_clock::time_point begin_blur_construction = std::chrono::steady_clock::now();
         Eigen::VectorXf ones_nvertices = Eigen::VectorXf::Ones(nvertices);
         Eigen::VectorXf ones_npixels = Eigen::VectorXf::Ones(npixels);
         blurs_test = ones_nvertices.asDiagonal();
@@ -237,11 +234,11 @@ namespace ximgproc
           	{
           	     Eigen::SparseMatrix<float, Eigen::ColMajor> blur_temp(hashed_coords.size(), hashed_coords.size());
                  blur_temp.reserve(Eigen::VectorXi::Constant(nvertices,6));
-          		   std::int64_t offset_hash_coord = offset * hash_vec[i];
-        		     for (auto it = hashed_coords.begin(); it != hashed_coords.end(); ++it)
+          		   long long offset_hash_coord = offset * hash_vec[i];
+        		     for (boost::unordered_map<long long,int>::iterator it = hashed_coords.begin(); it != hashed_coords.end(); ++it)
       		       {
-      			         std::int64_t neighb_coord = it->first + offset_hash_coord;
-      			         auto it_neighb = hashed_coords.find(neighb_coord);
+      			         long long neighb_coord = it->first + offset_hash_coord;
+      			         boost::unordered_map<long long,int>::iterator it_neighb = hashed_coords.find(neighb_coord);
       			         if (it_neighb != hashed_coords.end())
           			     {
                          blur_temp.insert(it->second,it_neighb->second) = 1.0f;
