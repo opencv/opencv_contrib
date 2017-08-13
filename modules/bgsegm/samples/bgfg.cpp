9ae3cedb9227788b1288fac2b2b36c56131475e1
atinfinity <dandelion1124@gmail.com>
2017-08-13 16:15:25 +0900

    fixed a sample code of bgsegm

--- modules/bgsegm/samples/bgfg.cpp
+++ modules/bgsegm/samples/bgfg.cpp
@@ -1,46 +1,55 @@
-#include "opencv2/bgsegm.hpp"
-#include "opencv2/videoio.hpp"
-#include "opencv2/highgui.hpp"
+#include <opencv2/bgsegm.hpp>
+#include <opencv2/video/background_segm.hpp>
+#include <opencv2/videoio.hpp>
+#include <opencv2/highgui.hpp>
 #include <opencv2/core/utility.hpp>
 #include <iostream>
+#include <string>
 
-using namespace cv;
-using namespace cv::bgsegm;
-
-const String about =
+const std::string about =
     "\nA program demonstrating the use and capabilities of different background subtraction algrorithms\n"
-    "Using OpenCV version " + String(CV_VERSION) +
+    "Using OpenCV version " + std::string(CV_VERSION) +
     "\nPress q or ESC to exit\n";
 
-const String keys =
+const std::string keys =
         "{help h usage ? |      | print this message   }"
         "{vid            |      | path to a video file }"
         "{algo           | GMG  | name of the algorithm (GMG, CNT, KNN, MOG, MOG2) }"
         ;
 
-static Ptr<BackgroundSubtractor> createBGSubtractorByName(const String& algoName)
+static cv::Ptr<cv::BackgroundSubtractor> createBGSubtractorByName(const std::string& algoName)
 {
-    Ptr<BackgroundSubtractor> algo;
-    if(algoName == String("GMG"))
-        algo = createBackgroundSubtractorGMG(20, 0.7);
-    else if(algoName == String("CNT"))
-        algo = createBackgroundSubtractorCNT();
-    else if(algoName == String("KNN"))
-        algo = createBackgroundSubtractorKNN();
-    else if(algoName == String("MOG"))
-        algo = createBackgroundSubtractorMOG();
-    else if(algoName == String("MOG2"))
-        algo = createBackgroundSubtractorMOG2();
+    cv::Ptr<cv::BackgroundSubtractor> algo;
+    if (algoName == std::string("GMG"))
+    {
+        algo = cv::bgsegm::createBackgroundSubtractorGMG(20, 0.7);
+    }
+    else if (algoName == std::string("CNT"))
+    {
+        algo = cv::bgsegm::createBackgroundSubtractorCNT();
+    }
+    else if (algoName == std::string("KNN"))
+    {
+        algo = cv::createBackgroundSubtractorKNN();
+    }
+    else if (algoName == std::string("MOG"))
+    {
+        algo = cv::bgsegm::createBackgroundSubtractorMOG();
+    }
+    else if (algoName == std::string("MOG2"))
+    {
+        algo = cv::createBackgroundSubtractorMOG2();
+    }
 
     return algo;
 }
 
 int main(int argc, char** argv)
 {
-    setUseOptimized(true);
-    setNumThreads(8);
+    cv::setUseOptimized(true);
+    cv::setNumThreads(8);
 
-    CommandLineParser parser(argc, argv, keys);
+    cv::CommandLineParser parser(argc, argv, keys);
     parser.about(about);
     parser.printMessage();
     if (parser.has("help"))
@@ -49,8 +58,8 @@ int main(int argc, char** argv)
         return 0;
     }
 
-    String videoPath = parser.get<String>("vid");
-    String algoName = parser.get<String>("algo");
+    std::string videoPath = parser.get<std::string>("vid");
+    std::string algoName = parser.get<std::string>("algo");
 
     if (!parser.check())
     {
@@ -58,18 +67,22 @@ int main(int argc, char** argv)
         return 0;
     }
 
-    Ptr<BackgroundSubtractor> bgfs = createBGSubtractorByName(algoName);
+    cv::Ptr<cv::BackgroundSubtractor> bgfs = createBGSubtractorByName(algoName);
     if (!bgfs)
     {
         std::cerr << "Failed to create " << algoName << " background subtractor" << std::endl;
         return -1;
     }
 
-    VideoCapture cap;
+    cv::VideoCapture cap;
     if (argc > 1)
+    {
         cap.open(videoPath);
+    }
     else
+    {
         cap.open(0);
+    }
 
     if (!cap.isOpened())
     {
@@ -77,27 +90,30 @@ int main(int argc, char** argv)
         return -1;
     }
 
-    Mat frame, fgmask, segm;
-
-    namedWindow("FG Segmentation", WINDOW_NORMAL);
+    cv::Mat frame, fgmask, segm;
+    cv::namedWindow("FG Segmentation", cv::WINDOW_NORMAL);
 
     for (;;)
     {
         cap >> frame;
 
         if (frame.empty())
+        {
             break;
+        }
 
         bgfs->apply(frame, fgmask);
 
         frame.convertTo(segm, CV_8U, 0.5);
-        add(frame, Scalar(100, 100, 0), segm, fgmask);
+        cv::add(frame, cv::Scalar(100, 100, 0), segm, fgmask);
 
-        imshow("FG Segmentation", segm);
+        cv::imshow("FG Segmentation", segm);
 
-        int c = waitKey(30);
+        int c = cv::waitKey(30);
         if (c == 'q' || c == 'Q' || (c & 255) == 27)
+        {
             break;
+        }
     }
 
     return 0;
