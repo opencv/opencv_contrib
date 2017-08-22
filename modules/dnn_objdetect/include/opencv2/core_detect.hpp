05896f566b2e871942b7c6b7b8e6e761f00c0bf3
kvmanohar22 <kvmanohar22@gmail.com>
2017-08-22 14:12:34 +0530

    fix bugs

--- modules/dnn_objdetect/include/opencv2/core_detect.hpp
+++ modules/dnn_objdetect/include/opencv2/core_detect.hpp
@@ -45,8 +45,8 @@
 #include <vector>
 #include <memory>
 
-#include "opencv2/highgui.hpp"
-#include "opencv2/imgproc.hpp"
+#include <opencv2/highgui.hpp>
+#include <opencv2/imgproc.hpp>
 
 namespace cv
 {
