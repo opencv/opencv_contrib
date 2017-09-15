dc53f69182d8701fed9801ae4e7cad2a40a0486f
Suleyman TURKMEN <sturkmen@hotmail.com>
2017-09-16 02:43:59 +0300

    remove_tabs

--- modules/plot/samples/plot_demo.cpp
+++ modules/plot/samples/plot_demo.cpp
@@ -6,30 +6,30 @@ using namespace cv;
 
 int main(int argc, char** argv)
 {
-	Mat data_x(1, 50, CV_64F);
-	Mat data_y(1, 50, CV_64F);
+    Mat data_x(1, 50, CV_64F);
+    Mat data_y(1, 50, CV_64F);
 
-	for (int i = 0; i < 50; i++)
-	{
-		data_x.at<double>(0, i) = (i - 25);
-		data_y.at<double>(0, i) = (i - 25)*(i - 25)*(i - 25);
-	}
+    for (int i = 0; i < 50; i++)
+    {
+        data_x.at<double>(0, i) = (i - 25);
+        data_y.at<double>(0, i) = (i - 25)*(i - 25)*(i - 25);
+    }
 
-	std::cout << "data_x : " << data_x << std::endl;
-	std::cout << "data_y : " << data_y << std::endl;
+    std::cout << "data_x : " << data_x << std::endl;
+    std::cout << "data_y : " << data_y << std::endl;
 
-	Mat plot_result;
+    Mat plot_result;
 
-	Ptr<plot::Plot2d> plot = plot::Plot2d::create(data_x, data_y);
-	plot->render(plot_result);
+    Ptr<plot::Plot2d> plot = plot::Plot2d::create(data_x, data_y);
+    plot->render(plot_result);
 
-	imshow("default orientation", plot_result);
+    imshow("default orientation", plot_result);
 
-	plot = plot::Plot2d::create(data_x, data_y,true);
-	plot->render(plot_result);
+    plot = plot::Plot2d::create(data_x, data_y,true);
+    plot->render(plot_result);
 
-	imshow("inverted orientation", plot_result);
-	waitKey();
+    imshow("inverted orientation", plot_result);
+    waitKey();
 
-	return 0;
+    return 0;
 }
