7e46ea166c295d6145d48c626b627ad39279085d
Vitaly Tuzov <terfendail@mediana.jetos.com>
2017-09-29 17:40:45 +0300

    Removed helper functions from header. Updated module license text.

--- modules/xfeatures2d/include/opencv2/xfeatures2d.hpp
+++ modules/xfeatures2d/include/opencv2/xfeatures2d.hpp
@@ -279,12 +279,6 @@ CV_EXPORTS void FASTForPointSet( InputArray image, CV_IN_OUT std::vector<KeyPoin
                       int threshold, bool nonmaxSuppression=true, int type=FastFeatureDetector::TYPE_9_16);
 
 
-void makeOffsets(int pixel[25], int row_stride, int patternSize);
-
-template<int patternSize>
-int cornerScore(const uchar* ptr, const int pixel[], int threshold);
-
-
 //! @}
 
 }
