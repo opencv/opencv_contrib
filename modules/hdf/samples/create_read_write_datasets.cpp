13508c7618ce1ff5d910fd6ca88f12df07c41c66
KUANG Fangjun <csukuangfj@gmail.com>
2017-12-22 18:55:23 +0100

    Remove warnings.

--- modules/hdf/samples/create_read_write_datasets.cpp
+++ modules/hdf/samples/create_read_write_datasets.cpp
@@ -13,6 +13,14 @@
  *
  */
 
+#ifdef __GNUC__
+#  pragma GCC diagnostic ignored "-Wmissing-declarations"
+#  if defined __clang__ || defined __APPLE__
+#    pragma GCC diagnostic ignored "-Wmissing-prototypes"
+#    pragma GCC diagnostic ignored "-Wextra"
+#  endif
+#endif
+
 //! [tutorial]
 #include <iostream>
 
