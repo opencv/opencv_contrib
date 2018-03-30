3857b757f0ca3f00d4d4703a4cc4598a1b2abf9d
Rostislav Vasilikhin <rostislav.vasilikhin@intel.com>
2018-03-30 20:00:03 +0300

    ICP::getAb: parallel_for

--- modules/kinect_fusion/src/icp.cpp
+++ modules/kinect_fusion/src/icp.cpp
@@ -102,14 +102,120 @@ static inline Point3f bilinear(const Points& m, cv::Point2f pt)
        fastCheck(v10) && fastCheck(v11))
     {
         float tx  = pt.x - xi, ty = pt.y - yi;
-        float tx1 = 1.f-tx, ty1 = 1.f-ty;
-        return v00*tx1*ty1 + v01*tx*ty1 + v10*tx1*ty + v11*tx*ty;
+
+        //TODO: check speed
+//        float tx1 = 1.f-tx, ty1 = 1.f-ty;
+//        return v00*tx1*ty1 + v01*tx*ty1 + v10*tx1*ty + v11*tx*ty;
+
+        float txty = tx*ty;
+        Point3f d001 = v00 - v01;
+        return v00 + tx*d001 + ty*(v10-v00) + txty*(d001 - v10 + v11);
     }
     else
     {
         return nan3;
     }
 }
+
+//TODO: optimize it to use only 27 elems
+typedef Matx<float, 6, 7> ABtype;
+
+struct GetAbInvoker : ParallelLoopBody
+{
+    GetAbInvoker(ABtype& _globalAb, Mutex& _mtx,
+                 const Points& _oldPts, const Normals& _oldNrm, const Points& _newPts, const Normals& _newNrm,
+                 Affine3f _pose, Intr::Projector _proj, float _sqDistanceThresh, float _minCos) :
+        ParallelLoopBody(),
+        globalSumAb(_globalAb), mtx(_mtx),
+        oldPts(_oldPts), oldNrm(_oldNrm), newPts(_newPts), newNrm(_newNrm), pose(_pose),
+        proj(_proj), sqDistanceThresh(_sqDistanceThresh), minCos(_minCos)
+    { }
+
+    virtual void operator ()(const Range& range) const
+    {
+        ABtype sumAB = ABtype::zeros();
+
+        for(int y = range.start; y < range.end; y++)
+        {
+            const Point3f* newPtsRow = newPts[y];
+            const Point3f* newNrmRow = newNrm[y];
+
+            for(int x = 0; x < newPts.cols; x++)
+            {
+                Point3f newP = newPtsRow[x];
+                Point3f newN = newNrmRow[x];
+
+                Point3f oldP(nan3), oldN(nan3);
+
+                //if(!(isNaN(newP) || isNaN(newN)))
+                if(fastCheck(newP) && fastCheck(newN))
+                {
+                    //transform to old coord system
+                    newP = pose * newP;
+                    newN = pose.rotation() * newN;
+
+                    //find correspondence
+                    Point2f oldCoords = proj(newP);
+                    oldP = bilinear(oldPts, oldCoords);
+                    oldN = bilinear(oldNrm, oldCoords);
+                }
+                else
+                {
+                    continue;
+                }
+
+                if(fastCheck(oldP) && fastCheck(oldN))
+                {
+                    //filter by distance
+                    if((newP - oldP).dot(newP - oldP) > sqDistanceThresh)
+                    {
+                        continue;
+                    }
+
+                    //filter by angle
+                    if(abs(newN.dot(oldN)) < minCos)
+                    {
+                        continue;
+                    }
+
+                    // build point-wise vector ab = [ A | b ]
+
+                    //try to optimize
+                    Point3f VxN = newP.cross(oldN);
+                    float ab[7] = {VxN.x, VxN.y, VxN.z, oldN.x, oldN.y, oldN.z, oldN.dot(oldP - newP)};
+
+                    // build point-wise upper-triangle matrix [ab^T * ab] w/o last row
+                    // which is [A^T*A | A^T*b]
+                    //TODO: optimize it to use only 27 elems
+                    ABtype aab = ABtype::zeros();
+                    for(int i = 0; i < 6; i++)
+                    {
+                        for(int j = i; j < 7; j++)
+                        {
+                            aab(i, j) = ab[i]*ab[j];
+                        }
+                    }
+                    //TODO: optimize it to use only 27 elems
+                    sumAB += aab;
+                }
+            }
+        }
+
+        AutoLock al(mtx);
+        globalSumAb += sumAB;
+    }
+
+    ABtype& globalSumAb;
+    Mutex& mtx;
+    const Points& oldPts;
+    const Normals& oldNrm;
+    const Points& newPts;
+    const Normals& newNrm;
+    Affine3f pose;
+    const Intr::Projector proj;
+    float sqDistanceThresh;
+    float minCos;
+};
 
 
 void ICPCPU::getAb(const Points& oldPts, const Normals& oldNrm, const Points& newPts, const Normals& newNrm,
@@ -121,79 +227,14 @@ void ICPCPU::getAb(const Points& oldPts, const Normals& oldNrm, const Points& ne
     CV_Assert(newPts.size() == newNrm.size());
 
     //TODO: optimize it to use only 27 elems
-    typedef Matx<float, 6, 7> ABtype;
     ABtype sumAB = ABtype::zeros();
 
-    Intr::Projector proj = intrinsics.scale(level).makeProjector();
-
-    float sqDistanceThresh = distanceThreshold*distanceThreshold;
-    float minCos = cos(angleThreshold);
-
-    for(int y = 0; y < newPts.rows; y++)
-    {
-        const Point3f* newPtsRow = newPts[y];
-        const Point3f* newNrmRow = newNrm[y];
-
-        for(int x = 0; x < newPts.cols; x++)
-        {
-            Point3f newP = newPtsRow[x];
-            Point3f newN = newNrmRow[x];
-
-            Point3f oldP(nan3), oldN(nan3);
-
-            //if(!(isNaN(newP) || isNaN(newN)))
-            if(fastCheck(newP) && fastCheck(newN))
-            {
-                //transform to old coord system
-                newP = pose * newP;
-                newN = pose.rotation() * newN;
-
-                //find correspondence
-                Point2f oldCoords = proj(newP);
-                oldP = bilinear(oldPts, oldCoords);
-                oldN = bilinear(oldNrm, oldCoords);
-            }
-            else
-            {
-                continue;
-            }
-
-            if(fastCheck(oldP) && fastCheck(oldN))
-            {
-                //filter by distance
-                if((newP - oldP).dot(newP - oldP) > sqDistanceThresh)
-                {
-                    continue;
-                }
-
-                //filter by angle
-                if(abs(newN.dot(oldN)) < minCos)
-                {
-                    continue;
-                }
-
-                // build point-wise vector ab = [ A | b ]
-
-                //try to optimize
-                Point3f VxN = newP.cross(oldN);
-                float ab[7] = {VxN.x, VxN.y, VxN.z, oldN.x, oldN.y, oldN.z, oldN.dot(oldP - newP)};
-
-                // build point-wise upper-triangle matrix [ab^T * ab] w/o last row
-                // which is [A^T*A | A^T*b]
-                //TODO: optimize it to use only 27 elems
-                ABtype aab = ABtype::zeros();
-                for(int i = 0; i < 6; i++)
-                {
-                    for(int j = i; j < 7; j++)
-                    {
-                        aab(i, j) = ab[i]*ab[j];
-                    }
-                }
-                //TODO: optimize it to use only 27 elems
-                sumAB += aab;
-            }
-        }
-    }
+    Mutex mutex;
+    GetAbInvoker invoker(sumAB, mutex, oldPts, oldNrm, newPts, newNrm, pose,
+                         intrinsics.scale(level).makeProjector(),
+                         distanceThreshold*distanceThreshold, cos(angleThreshold));
+    Range range(0, newPts.rows);
+    parallel_for_(range, invoker);
 
     // splitting AB matrix to A and b
     for(int i = 0; i < 6; i++)
