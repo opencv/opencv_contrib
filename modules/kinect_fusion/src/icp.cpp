505c2a0e9f0de3f5276b263bdcdfbb7232e33cf3
Rostislav Vasilikhin <rostislav.vasilikhin@intel.com>
2018-04-03 00:04:39 +0300

    ICP optimized (doesn't work)

--- modules/kinect_fusion/src/icp.cpp
+++ modules/kinect_fusion/src/icp.cpp
@@ -1,7 +1,9 @@
 //TODO: add license
 
+//TODO: organize includes properly
 #include "precomp.hpp"
 #include "icp.hpp"
+#include "opencv2/core/hal/intrin.hpp"
 
 using namespace cv;
 using namespace cv::kinfu;
@@ -81,6 +83,32 @@ static inline bool fastCheck(const Point3f& p)
     return !cvIsNaN(p.x);
 }
 
+static inline bool isNaN(const v_float32x4& p)
+{
+    return v_check_any(p != p);
+}
+
+static inline void getCrossPerm(const v_float32x4& a, v_float32x4& yzx, v_float32x4& zxy)
+{
+    v_uint32x4 aa = v_reinterpret_as_u32(a);
+    v_uint32x4 yz00 = v_extract<1>(aa, v_setzero_u32());
+    v_uint32x4 x0y0, tmp;
+    v_zip(aa, v_setzero_u32(), x0y0, tmp);
+    v_uint32x4 yzx0 = v_combine_low(yz00, x0y0);
+    v_uint32x4 y000 = v_extract<2>(x0y0, v_setzero_u32());
+    v_uint32x4 zx00 = v_extract<1>(yzx0, v_setzero_u32());
+    zxy = v_reinterpret_as_f32(v_combine_low(zx00, y000));
+    yzx = v_reinterpret_as_f32(yzx0);
+}
+
+static inline v_float32x4 crossProduct(const v_float32x4& a, const v_float32x4& b)
+{
+    v_float32x4 ayzx, azxy, byzx, bzxy;
+    getCrossPerm(a, ayzx, azxy);
+    getCrossPerm(b, byzx, bzxy);
+    return ayzx*bzxy - azxy*byzx;
+}
+
 typedef Matx<float, 6, 7> ABtype;
 
 struct GetAbInvoker : ParallelLoopBody
@@ -101,31 +129,44 @@ struct GetAbInvoker : ParallelLoopBody
 
     virtual void operator ()(const Range& range) const
     {
-        float upperTriangle[UTSIZE];
-        for(int i = 0; i < UTSIZE; i++)
+        CV_Assert(ptype::channels == 4);
+
+        float CV_DECL_ALIGNED(16) upperTriangle[40];
+        for(int i = 0; i < 40; i++)
             upperTriangle[i] = 0;
 
+        const float (&pm)[16] = pose.matrix.val;
+        v_float32x4 poseRot0(pm[0], pm[4], pm[ 8], 0);
+        v_float32x4 poseRot1(pm[1], pm[5], pm[ 9], 0);
+        v_float32x4 poseRot2(pm[2], pm[6], pm[10], 0);
+        v_float32x4 poseTrans(pm[3], pm[7], pm[10], 0);
+
+        v_float32x4 vfxy(proj.fx, proj.fy, 0, 0), vcxy(proj.cx, proj.cy, 0, 0);
         for(int y = range.start; y < range.end; y++)
         {
-            const ptype* newPtsRow = newPts[y];
-            const ptype* newNrmRow = newNrm[y];
+            const CV_DECL_ALIGNED(16) float* newPtsRow = (const float*)newPts[y];
+            const CV_DECL_ALIGNED(16) float* newNrmRow = (const float*)newNrm[y];
 
             for(int x = 0; x < newPts.cols; x++)
             {
-                Point3f newP = fromPtype(newPtsRow[x]);
-                Point3f newN = fromPtype(newNrmRow[x]);
+                v_float32x4 newP = v_load_aligned(newPtsRow + x*4);
+                v_float32x4 newN = v_load_aligned(newNrmRow + x*4);
 
-                Point3f oldP(nan3), oldN(nan3);
-
-                if(!(fastCheck(newP) && fastCheck(newN)))
+                //TODO: take number(s) from each vector to check
+                if(isNaN(newP) || isNaN(newN))
                     continue;
 
                 //transform to old coord system
-                newP = pose * newP;
-                newN = pose.rotation() * newN;
+                newP = v_matmuladd(newP, poseRot0, poseRot1, poseRot2, poseTrans);
+                newN = v_matmuladd(newP, poseRot0, poseRot1, poseRot2, v_setzero_f32());
 
                 //find correspondence
-                Point2f oldCoords = proj(newP);
+                Point2f oldCoords;
+                float z = 1.f/v_reinterpret_as_f32(v_extract<2>(v_reinterpret_as_u32(newP), v_setzero_u32())).get0();
+                v_float32x4 p = newP*v_setall_f32(z) + vcxy;
+                oldCoords.x = p.get0();
+                oldCoords.y = v_reinterpret_as_f32(v_extract<1>(v_reinterpret_as_u32(p), v_setzero_u32())).get0();
+
                 if(!(oldCoords.x >= 0 && oldCoords.x < oldPts.cols - 1 &&
                      oldCoords.y >= 0 && oldCoords.y < oldPts.rows - 1))
                     continue;
@@ -135,78 +176,117 @@ struct GetAbInvoker : ParallelLoopBody
                 float tx  = oldCoords.x - xi, ty = oldCoords.y - yi;
                 float tx1 = 1.f-tx, ty1 = 1.f-ty;
                 float t00 = tx1*ty1, t01 = tx*ty1, t10 = tx1*ty, t11 = tx*ty;
+                v_float32x4 tv(t00, t01, t10, t11);
 
-                const ptype* prow0 = oldPts[yi+0];
-                const ptype* prow1 = oldPts[yi+1];
+                const float* prow0 = (const float*)oldPts[yi+0];
+                const float* prow1 = (const float*)oldPts[yi+1];
 
-                Point3f p00 = fromPtype(prow0[xi+0]);
-                Point3f p01 = fromPtype(prow0[xi+1]);
-                Point3f p10 = fromPtype(prow1[xi+0]);
-                Point3f p11 = fromPtype(prow1[xi+1]);
+                v_float32x4 p00 = v_load_aligned(prow0 + (xi+0)*4);
+                v_float32x4 p01 = v_load_aligned(prow0 + (xi+1)*4);
+                v_float32x4 p10 = v_load_aligned(prow1 + (xi+0)*4);
+                v_float32x4 p11 = v_load_aligned(prow1 + (xi+1)*4);
 
                 //do not fix missing data
-                if(!(fastCheck(p00) && fastCheck(p01) &&
-                     fastCheck(p10) && fastCheck(p11)))
+                //TODO: take number(s) from each vector to check
+                if(isNaN(p00) || isNaN(p01) || isNaN(p10) || isNaN(p11))
                     continue;
 
-                const ptype* nrow0 = oldNrm[yi+0];
-                const ptype* nrow1 = oldNrm[yi+1];
+                const float* nrow0 = (const float*)oldNrm[yi+0];
+                const float* nrow1 = (const float*)oldNrm[yi+1];
 
-                Point3f n00 = fromPtype(nrow0[xi+0]);
-                Point3f n01 = fromPtype(nrow0[xi+1]);
-                Point3f n10 = fromPtype(nrow1[xi+0]);
-                Point3f n11 = fromPtype(nrow1[xi+1]);
+                v_float32x4 n00 = v_load_aligned(nrow0 + (xi+0)*4);
+                v_float32x4 n01 = v_load_aligned(nrow0 + (xi+1)*4);
+                v_float32x4 n10 = v_load_aligned(nrow1 + (xi+0)*4);
+                v_float32x4 n11 = v_load_aligned(nrow1 + (xi+1)*4);
 
-                if(!(fastCheck(n00) && fastCheck(n01) &&
-                     fastCheck(n10) && fastCheck(n11)))
+                //TODO: take number(s) from each vector to check
+                if(isNaN(n00) || isNaN(n01) || isNaN(n10) || isNaN(n11))
                     continue;
 
-                oldP = p00*t00 + p01*t01 + p10*t10 + p11*t11;
-                oldN = n00*t00 + n01*t01 + n10*t10 + n11*t11;
+                v_float32x4 oldP = v_matmul(tv, p00, p01, p10, p11);
+                v_float32x4 oldN = v_matmul(tv, n00, n01, n10, n11);
 
-                if(!(fastCheck(oldP) && fastCheck(oldN)))
+                //TODO: take number(s) from each vector to check
+                if(isNaN(oldP) || isNaN(oldN))
                     continue;
 
                 //filter by distance
-                Point3f diff = newP - oldP;
-                if(diff.dot(diff) > sqDistanceThresh)
-                {
+                v_float32x4 diff = newP - oldP;
+                if(v_reduce_sum(diff*diff) > sqDistanceThresh)
                     continue;
-                }
 
                 //filter by angle
-                if(abs(newN.dot(oldN)) < minCos)
-                {
+                if(abs(v_reduce_sum(newN*oldN)) < minCos)
                     continue;
-                }
 
                 // build point-wise vector ab = [ A | b ]
+                v_float32x4 VxN = crossProduct(newP, oldN);
 
-                //try to optimize
-                Point3f VxN = newP.cross(oldN);
-                float ab[7] = {VxN.x, VxN.y, VxN.z, oldN.x, oldN.y, oldN.z, oldN.dot(-diff)};
+                float dotp = -v_reduce_sum(oldN*diff);
+                //ab == {VxN.x, VxN.y, VxN.z, oldN.x, oldN.y, oldN.z, dotp, 0};
+                float CV_DECL_ALIGNED(16) ab[8];
+                //write elems from 0th to 3th
+                v_store_aligned(ab + 0, v_reinterpret_as_f32(v_reinterpret_as_u32(VxN) |
+                                                             v_extract<3>(v_setzero_u32(), v_reinterpret_as_u32(oldN))));
+                //write elems from 4th to 5th
+                v_store_aligned(ab + 4, v_reinterpret_as_f32(v_extract<1>(v_reinterpret_as_u32(oldN), v_setzero_u32())));
+                //write the rest
+                ab[6] = dotp; ab[7] = 0;
 
                 // build point-wise upper-triangle matrix [ab^T * ab] w/o last row
                 // which is [A^T*A | A^T*b]
                 // and gather sum
-                int pos = 0;
-                for(int i = 0; i < 6; i++)
-                {
-                    for(int j = i; j < 7; j++)
-                    {
-                        upperTriangle[pos++] += ab[i]*ab[j];
-                    }
-                }
+                v_float32x4 vab0 = v_load_aligned(ab);
+                v_float32x4 vab1 = v_load_aligned(ab+4);
+
+                v_float32x4 vutg0, vutg1;
+                v_float32x4 m0 = v_setall_f32(ab[0]);
+                vutg0 = v_load_aligned(upperTriangle + 0);
+                vutg1 = v_load_aligned(upperTriangle + 4);
+                v_store_aligned(upperTriangle + 0, v_muladd(m0, vab0, vutg0));
+                v_store_aligned(upperTriangle + 4, v_muladd(m0, vab1, vutg1));
+
+                v_float32x4 m1 = v_setall_f32(ab[1]);
+                vutg0 = v_load_aligned(upperTriangle +  8);
+                vutg1 = v_load_aligned(upperTriangle + 12);
+                v_store_aligned(upperTriangle +  8, v_muladd(m1, vab0, vutg0));
+                v_store_aligned(upperTriangle + 12, v_muladd(m1, vab1, vutg1));
+
+                v_float32x4 m2 = v_setall_f32(ab[2]);
+                vutg0 = v_load_aligned(upperTriangle + 16);
+                vutg1 = v_load_aligned(upperTriangle + 20);
+                v_store_aligned(upperTriangle + 16, v_muladd(m2, vab0, vutg0));
+                v_store_aligned(upperTriangle + 20, v_muladd(m2, vab1, vutg1));
+
+                v_float32x4 m3 = v_setall_f32(ab[3]);
+                vutg0 = v_load_aligned(upperTriangle + 24);
+                vutg1 = v_load_aligned(upperTriangle + 28);
+                v_store_aligned(upperTriangle + 24, v_muladd(m3, vab0, vutg0));
+                v_store_aligned(upperTriangle + 28, v_muladd(m3, vab1, vutg1));
+
+                v_float32x4 m4 = v_setall_f32(ab[4]);
+                vutg0 = v_load_aligned(upperTriangle + 32);
+                v_store_aligned(upperTriangle + 32, v_muladd(m4, vab1, vutg0));
+
+                v_float32x4 m5 = v_setall_f32(ab[5]);
+                vutg1 = v_load_aligned(upperTriangle + 36);
+                v_store_aligned(upperTriangle + 36, v_muladd(m5, vab1, vutg1));
             }
         }
 
         ABtype sumAB = ABtype::zeros();
-        int pos = 0;
-        for(int i = 0; i < 6; i++)
+        for(int i = 0; i < 4; i++)
         {
             for(int j = i; j < 7; j++)
             {
-                sumAB(i, j) = upperTriangle[pos++];
+                sumAB(i, j) = upperTriangle[i*8 + j];
+            }
+        }
+        for(int i = 4; i < 6; i++)
+        {
+            for(int j = i; j < 7; j++)
+            {
+                sumAB(i, j) = upperTriangle[32+(i-4)*4 + j - 4];
             }
         }
 
