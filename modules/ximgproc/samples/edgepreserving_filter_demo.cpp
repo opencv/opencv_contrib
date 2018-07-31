55766d986ee2ee8defaf0545bd14f7734688ae37
Simon Reich <sreich@physik3.gwdg.de>
2018-07-31 13:14:56 +0200

    Beautified code via clang-format and https://raw.githubusercontent.com/opencv/opencv_contrib/master/modules/cvv/.clang-format

--- modules/ximgproc/samples/edgepreserving_filter_demo.cpp
+++ modules/ximgproc/samples/edgepreserving_filter_demo.cpp
@@ -1,23 +1,21 @@
 #include <iostream>
-#include <string>
 #include <opencv2/highgui.hpp>
 #include <opencv2/ximgproc.hpp>
-
+#include <string>
 
 using namespace cv;
 
-
 int main(int argc, char **argv)
 {
-    cv::CommandLineParser parser(argc, argv,
-        "{help h ? |     | help message}"
-        "{@image   |     | Image filename to process }"
-    );
-    if (parser.has("help") || !parser.has("@image"))
-    {
-        parser.printMessage();
-        return 0;
-		}
+	cv::CommandLineParser parser(
+	    argc, argv,
+	    "{help h ? |     | help message}"
+	    "{@image   |     | Image filename to process }");
+	if (parser.has("help") || !parser.has("@image"))
+	{
+		parser.printMessage();
+		return 0;
+	}
 
 	// Load image from first parameter
 	std::string filename = parser.get<std::string>("@image");
