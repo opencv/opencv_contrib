c6ea43fe97b6a1ea14a17d4048dc189dd10d61cf
Simon Reich <sreich@physik3.gwdg.de>
2018-08-01 09:14:52 +0200

    Added name to header

--- modules/ximgproc/perf/perf_edgepreserving_filter.cpp
+++ modules/ximgproc/perf/perf_edgepreserving_filter.cpp
@@ -1,6 +1,9 @@
 // This file is part of OpenCV project.
 // It is subject to the license terms in the LICENSE file found in the top-level
 // directory of this distribution and at http://opencv.org/license.html.
+//
+//                    Created by Simon Reich
+//
 #include "perf_precomp.hpp"
 
 namespace opencv_test
