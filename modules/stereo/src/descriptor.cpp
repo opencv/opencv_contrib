4e10bc5dae6272a86c43840feb3d05989953e5c5
Jan Beich <jbeich@FreeBSD.org>
2018-08-08 17:40:10 +0000

    stereo: unbreak with clang 7
     modules/stereo/src/descriptor.cpp:229:34: error: ordered comparison between pointer and zero ('const int *' and 'int')
    	     CV_Assert(image.size > 0);
    		       ~~~~~~~~~~ ^ ~
     modules/core/include/opencv2/core/base.hpp:478:84: note: expanded from macro 'CV_Assert'
     #define CV_Assert(...) do { CVAUX_CONCAT(CV_Assert_, CV_VA_NUM_ARGS(__VA_ARGS__)) (__VA_ARGS__); } while(0)
    										    ^~~~~~~~~~~
     modules/core/include/opencv2/core/base.hpp:455:35: note: expanded from macro 'CV_Assert_1'
     #define CV_Assert_1( expr ) if(!!(expr)) ; else cv::error( cv::Error::StsAssert, #expr, CV_Func, __FILE__, __LINE__ )
    				   ^~~~
     modules/stereo/src/descriptor.cpp:230:33: error: ordered comparison between pointer and zero ('const int *' and 'int')
    	     CV_Assert(cost.size > 0);
    		       ~~~~~~~~~ ^ ~
     modules/core/include/opencv2/core/base.hpp:478:84: note: expanded from macro 'CV_Assert'
     #define CV_Assert(...) do { CVAUX_CONCAT(CV_Assert_, CV_VA_NUM_ARGS(__VA_ARGS__)) (__VA_ARGS__); } while(0)
    										    ^~~~~~~~~~~
     modules/core/include/opencv2/core/base.hpp:455:35: note: expanded from macro 'CV_Assert_1'
     #define CV_Assert_1( expr ) if(!!(expr)) ; else cv::error( cv::Error::StsAssert, #expr, CV_Func, __FILE__, __LINE__ )
    				   ^~~~

--- modules/stereo/src/descriptor.cpp
+++ modules/stereo/src/descriptor.cpp
@@ -226,8 +226,8 @@ namespace cv
         //integral image computation used in the Mean Variation Census Transform
         void imageMeanKernelSize(const Mat &image, int windowSize, Mat &cost)
         {
-            CV_Assert(image.size > 0);
-            CV_Assert(cost.size > 0);
+            CV_Assert(!image.empty());
+            CV_Assert(!cost.empty());
             CV_Assert(windowSize % 2 != 0);
             int win = windowSize / 2;
             float scalling = ((float) 1) / (windowSize * windowSize);
