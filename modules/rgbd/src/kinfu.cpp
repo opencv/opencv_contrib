417b48581f30ff009f49541833498b472b269785
Rostislav Vasilikhin <rostislav.vasilikhin@intel.com>
2018-08-15 20:58:27 +0300

    KinFu: no Frame or FrameGenerator, parametrized for Mat or UMat based on OCL availability

--- modules/rgbd/src/kinfu.cpp
+++ modules/rgbd/src/kinfu.cpp
@@ -101,7 +101,9 @@ Ptr<Params> Params::coarseParams()
     return p;
 }
 
-class KinFu::KinFuImpl : public KinFu
+// T should be Mat or UMat
+template< typename T >
+class KinFuImpl : public KinFu
 {
 public:
     KinFuImpl(const Params& _params);
@@ -122,84 +124,130 @@ public:
 
     bool update(InputArray depth) CV_OVERRIDE;
 
+    bool updateT(T depth);
+
 private:
     Params params;
 
-    cv::Ptr<FrameGenerator> frameGenerator;
     cv::Ptr<ICP> icp;
     cv::Ptr<TSDFVolume> volume;
 
     int frameCounter;
     Affine3f pose;
-    cv::Ptr<Frame> frame;
+    std::vector<T> pyrPoints;
+    std::vector<T> pyrNormals;
 };
 
 
-KinFu::KinFuImpl::KinFuImpl(const Params &_params) :
+template< typename T >
+KinFuImpl<T>::KinFuImpl(const Params &_params) :
     params(_params),
-    frameGenerator(makeFrameGenerator(params.platform)),
     icp(makeICP(params.intr, params.icpIterations, params.icpAngleThresh, params.icpDistThresh)),
     volume(makeTSDFVolume(params.volumeDims, params.volumeSize, params.volumePose,
                           params.tsdf_trunc_dist, params.tsdf_max_weight,
                           params.raycast_step_factor)),
-    frame()
+    pyrPoints(), pyrNormals()
 {
     reset();
 }
 
-void KinFu::KinFuImpl::reset()
+template< typename T >
+void KinFuImpl<T>::reset()
 {
     frameCounter = 0;
     pose = Affine3f::Identity();
     volume->reset();
 }
 
-KinFu::KinFuImpl::~KinFuImpl()
-{
+template< typename T >
+KinFuImpl<T>::~KinFuImpl()
+{ }
 
-}
-
-const Params& KinFu::KinFuImpl::getParams() const
+template< typename T >
+const Params& KinFuImpl<T>::getParams() const
 {
     return params;
 }
 
-void KinFu::KinFuImpl::setParams(const Params& p)
+template< typename T >
+void KinFuImpl<T>::setParams(const Params& p)
 {
     params = p;
 }
 
-const Affine3f KinFu::KinFuImpl::getPose() const
+template< typename T >
+const Affine3f KinFuImpl<T>::getPose() const
 {
     return pose;
 }
 
-bool KinFu::KinFuImpl::update(InputArray _depth)
-{
-    ScopeTime st("kinfu update");
 
+template<>
+bool KinFuImpl<Mat>::update(InputArray _depth)
+{
     CV_Assert(!_depth.empty() && _depth.size() == params.frameSize);
 
-    cv::Ptr<Frame> newFrame = (*frameGenerator)();
-    (*frameGenerator)(newFrame, _depth,
-                      params.intr,
-                      params.pyramidLevels,
-                      params.depthFactor,
-                      params.bilateral_sigma_depth,
-                      params.bilateral_sigma_spatial,
-                      params.bilateral_kernel_size);
+    Mat depth;
+    if(_depth.isUMat())
+    {
+        _depth.copyTo(depth);
+        return updateT(depth);
+    }
+    else
+    {
+        return updateT(_depth.getMat());
+    }
+}
+
+
+template<>
+bool KinFuImpl<UMat>::update(InputArray _depth)
+{
+    CV_Assert(!_depth.empty() && _depth.size() == params.frameSize);
+
+    UMat depth;
+    if(!_depth.isUMat())
+    {
+        _depth.copyTo(depth);
+        return updateT(depth);
+    }
+    else
+    {
+        return updateT(_depth.getUMat());
+    }
+}
+
+
+template< typename T >
+bool KinFuImpl<T>::updateT(T depth)
+{
+    CV_TRACE_FUNCTION();
+
+    ScopeTime st("kinfu update");
+
+    if(depth.type() != DEPTH_TYPE)
+        depth.convertTo(depth, DEPTH_TYPE);
+
+    std::vector<T> newPoints, newNormals;
+    makeFrameFromDepth(depth, newPoints, newNormals, params.intr,
+                       params.pyramidLevels,
+                       params.depthFactor,
+                       params.bilateral_sigma_depth,
+                       params.bilateral_sigma_spatial,
+                       params.bilateral_kernel_size);
 
     if(frameCounter == 0)
     {
         // use depth instead of distance
-        volume->integrate(newFrame, params.depthFactor, pose, params.intr);
+        volume->integrate(depth, params.depthFactor, pose, params.intr);
 
-        frame = newFrame;
+        pyrPoints  = newPoints;
+        pyrNormals = newNormals;
     }
     else
     {
         Affine3f affine;
-        bool success = icp->estimateTransform(affine, frame, newFrame);
+        bool success = icp->estimateTransform(affine, pyrPoints, pyrNormals, newPoints, newNormals);
         if(!success)
             return false;
 
@@ -211,12 +259,15 @@ bool KinFu::KinFuImpl::update(InputArray _depth)
         if((rnorm + tnorm)/2 >= params.tsdf_min_camera_movement)
         {
             // use depth instead of distance
-            volume->integrate(newFrame, params.depthFactor, pose, params.intr);
+            volume->integrate(depth, params.depthFactor, pose, params.intr);
         }
 
-        // raycast and build a pyramid of points and normals
-        volume->raycast(pose, params.intr, params.frameSize,
-                        params.pyramidLevels, frameGenerator, frame);
+        T& points  = pyrPoints [0];
+        T& normals = pyrNormals[0];
+        volume->raycast(pose, params.intr, params.frameSize, points, normals);
+        // build a pyramid of points and normals
+        buildPyramidPointsNormals(points, normals, pyrPoints, pyrNormals,
+                                  params.pyramidLevels);
     }
 
     frameCounter++;
@@ -224,8 +275,11 @@ bool KinFu::KinFuImpl::update(InputArray _depth)
 }
 
 
-void KinFu::KinFuImpl::render(OutputArray image, const Matx44f& _cameraPose) const
+template< typename T >
+void KinFuImpl<T>::render(OutputArray image, const Matx44f& _cameraPose) const
 {
+    CV_TRACE_FUNCTION();
+
     ScopeTime st("kinfu render");
 
     Affine3f cameraPose(_cameraPose);
@@ -234,39 +288,46 @@ void KinFu::KinFuImpl::render(OutputArray image, const Matx44f& _cameraPose) con
     if((cameraPose.rotation() == pose.rotation() && cameraPose.translation() == pose.translation()) ||
        (cameraPose.rotation() == id.rotation()   && cameraPose.translation() == id.translation()))
     {
-        frame->render(image, 0, params.lightPose);
+        renderPointsNormals(pyrPoints[0], pyrNormals[0], image, params.lightPose);
     }
     else
     {
-        // raycast and build a pyramid of points and normals
-        cv::Ptr<Frame> f = (*frameGenerator)();
-        volume->raycast(cameraPose, params.intr, params.frameSize,
-                        params.pyramidLevels, frameGenerator, f);
-        f->render(image, 0, params.lightPose);
+        T points, normals;
+        volume->raycast(cameraPose, params.intr, params.frameSize, points, normals);
+        renderPointsNormals(points, normals, image, params.lightPose);
     }
 }
 
 
-void KinFu::KinFuImpl::getCloud(OutputArray p, OutputArray n) const
+template< typename T >
+void KinFuImpl<T>::getCloud(OutputArray p, OutputArray n) const
 {
     volume->fetchPointsNormals(p, n);
 }
 
-void KinFu::KinFuImpl::getPoints(OutputArray points) const
+
+template< typename T >
+void KinFuImpl<T>::getPoints(OutputArray points) const
 {
     volume->fetchPointsNormals(points, noArray());
 }
 
-void KinFu::KinFuImpl::getNormals(InputArray points, OutputArray normals) const
+
+template< typename T >
+void KinFuImpl<T>::getNormals(InputArray points, OutputArray normals) const
 {
     volume->fetchNormals(points, normals);
 }
 
 // importing class
 
-Ptr<KinFu> KinFu::create(const Ptr<Params>& _params)
+Ptr<KinFu> KinFu::create(const Ptr<Params>& params)
 {
-    return makePtr<KinFu::KinFuImpl>(*_params);
+#ifdef HAVE_OPENCL
+    if(cv::ocl::isOpenCLActivated())
+        return makePtr< KinFuImpl<UMat> >(*params);
+#endif
+    return makePtr< KinFuImpl<Mat> >(*params);
 }
 
 KinFu::~KinFu() {}
