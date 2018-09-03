ec534fff210a9a8ca00b02eded8c19dc532f6137
Dai Fujita <fujita.dai@gmail.com>
2018-09-03 21:59:23 +0900

    Remove license header and add progress window

--- modules/ximgproc/samples/rollingGuidanceFilter_demo.cpp
+++ modules/ximgproc/samples/rollingGuidanceFilter_demo.cpp
@@ -1,42 +1,7 @@
-/*
- *  By downloading, copying, installing or using the software you agree to this license.
- *  If you do not agree to this license, do not download, install,
- *  copy or use the software.
- *
- *
- *  License Agreement
- *  For Open Source Computer Vision Library
- *  (3 - clause BSD License)
- *
- *  Redistribution and use in source and binary forms, with or without modification,
- *  are permitted provided that the following conditions are met :
- *
- *  * Redistributions of source code must retain the above copyright notice,
- *  this list of conditions and the following disclaimer.
- *
- *  * Redistributions in binary form must reproduce the above copyright notice,
- *  this list of conditions and the following disclaimer in the documentation
- *  and / or other materials provided with the distribution.
- *
- *  * Neither the names of the copyright holders nor the names of the contributors
- *  may be used to endorse or promote products derived from this software
- *  without specific prior written permission.
- *
- *  This software is provided by the copyright holders and contributors "as is" and
- *  any express or implied warranties, including, but not limited to, the implied
- *  warranties of merchantability and fitness for a particular purpose are disclaimed.
- *  In no event shall copyright holders or contributors be liable for any direct,
- *  indirect, incidental, special, exemplary, or consequential damages
- *  (including, but not limited to, procurement of substitute goods or services;
- *  loss of use, data, or profits; or business interruption) however caused
- *  and on any theory of liability, whether in contract, strict liability,
- *  or tort(including negligence or otherwise) arising in any way out of
- *  the use of this software, even if advised of the possibility of such damage.
- */
-
 #include <opencv2/core.hpp>
 #include <opencv2/core/utility.hpp>
 #include <opencv2/highgui.hpp>
+#include <opencv2/imgproc.hpp>
 #include <opencv2/ximgproc.hpp>
 
 using namespace cv;
@@ -49,7 +14,13 @@ int sColor = 10, sSpace = 3,nbIter=4;
 
 const char* window_name = "Rolling guidance filter";
 
-
+void progressWindow(String windowName, Mat x)
+{
+    Mat y = Mat::zeros(x.size(), CV_8UC1);
+    putText(y, "rollingGuidanceFilter in progress!", Point(10, x.rows / 2), FONT_HERSHEY_SIMPLEX, 1, Scalar(255));
+    imshow(windowName, y);
+    waitKey(1);
+}
 
 /**
  * @function paillouFilter
@@ -60,6 +31,7 @@ static void rollingFilter(int, void *pm)
     Mat img = *((Mat*)pm);
     double sigmaColor(sColor/10.0), sigmaSpace(sSpace);
     Mat dst;
+    progressWindow("rollingGuidanceFilter", img);
     rollingGuidanceFilter(img, dst, -1, sigmaColor, sigmaSpace, nbIter);
     imshow("rollingGuidanceFilter",dst );
 }
