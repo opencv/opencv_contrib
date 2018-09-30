542b9acf5b3b38298e0cb271305f32afabba1672
Hamdi Sahloul <hamdisahloul@hotmail.com>
2018-09-30 21:29:02 +0900

    CUDA/Python: Cleanup some manual enum wrappers

--- modules/cudacodec/misc/python/pyopencv_cudacodec.hpp
+++ modules/cudacodec/misc/python/pyopencv_cudacodec.hpp
@@ -5,10 +5,6 @@
 typedef cudacodec::EncoderCallBack::PicType EncoderCallBack_PicType;
 
 CV_PY_TO_CLASS(cudacodec::EncoderParams);
-
-CV_PY_TO_ENUM(cudacodec::EncoderCallBack::PicType);
-CV_PY_TO_ENUM(cudacodec::SurfaceFormat);
-
 CV_PY_FROM_CLASS(cudacodec::EncoderParams);
 
 #endif
