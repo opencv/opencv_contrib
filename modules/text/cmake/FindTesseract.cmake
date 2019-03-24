b7156ec3fc781d086065b483bfe4b6f760cb56b3
Ivan Pozdeev <vano@mail.mipt.ru>
2019-03-24 20:39:52 +0300

    Make manual FindTesseract.cmake cache entries consistent with pkg_check_modules's to prevent further confusion

--- modules/text/cmake/FindTesseract.cmake
+++ modules/text/cmake/FindTesseract.cmake
@@ -3,24 +3,24 @@ if(COMMAND pkg_check_modules)
   pkg_check_modules(Tesseract tesseract lept)
 endif()
 if(NOT Tesseract_FOUND)
-  find_path(Tesseract_INCLUDE_DIR tesseract/baseapi.h
+  find_path(Tesseract_INCLUDE_DIRS tesseract/baseapi.h
     HINTS
     /usr/include
     /usr/local/include)
 
-  find_library(Tesseract_LIBRARY NAMES tesseract
+  find_library(Tesseract_LIBRARIES NAMES tesseract
     HINTS
     /usr/lib
     /usr/local/lib)
 
-  find_library(Lept_LIBRARY NAMES lept
+  find_library(Tesseract_Lept_LIBRARY NAMES lept
     HINTS
     /usr/lib
     /usr/local/lib)
 
-  if(Tesseract_INCLUDE_DIR AND Tesseract_LIBRARY AND Lept_LIBRARY)
-    set(Tesseract_INCLUDE_DIRS ${Tesseract_INCLUDE_DIR})
-    set(Tesseract_LIBRARIES ${Tesseract_LIBRARY} ${Lept_LIBRARY})
+  if(Tesseract_INCLUDE_DIRS AND Tesseract_LIBRARIES AND Tesseract_Lept_LIBRARY)
+    set(Tesseract_INCLUDE_DIRS ${Tesseract_INCLUDE_DIRS} CACHE INTERNAL)
+    set(Tesseract_LIBRARIES ${Tesseract_LIBRARIES} ${Tesseract_Lept_LIBRARY} CACHE INTERNAL)
     set(Tesseract_FOUND 1)
   endif()
 endif()
