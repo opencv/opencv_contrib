91c8ca0f0baa1650f866453de7a87798da580e28
Rostislav Vasilikhin <rostislav.vasilikhin@intel.com>
2019-04-27 20:29:40 +0300

    reverted

--- modules/rgbd/src/kinfu_frame.cpp
+++ modules/rgbd/src/kinfu_frame.cpp
@@ -616,9 +616,6 @@ void makeFrameFromDepth(InputArray _depth,
     // looks like OpenCV's bilateral filter works the same as KinFu's
     Depth smooth;
 
-    //TODO: remove it after tests
-    patchNaNs(depth);
-
     bilateralFilter(depth, smooth, kernelSize, sigmaDepth*depthFactor, sigmaSpatial);
 
     // depth truncation is not used by default
