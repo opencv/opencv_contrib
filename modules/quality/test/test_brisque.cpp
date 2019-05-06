388be146cdda3ff745de4d988779838828bd66a0
clunietp@gmail.com <clunietp@gmail.com>
2019-05-06 14:43:42 -0400

    Test updates

--- modules/quality/test/test_brisque.cpp
+++ modules/quality/test/test_brisque.cpp
@@ -21,15 +21,35 @@ const cv::Scalar
 static const char* MODEL_FNAME = "brisque_model_live.yml";
 static const char* RANGE_FNAME = "brisque_range_live.yml";
 
+// calls cvtest::findDataFile, catches exceptions that we don't want raised and throws a skip test exception
+//  cvtest::findDataFile sometimes throws even if setting required=false
+inline cv::String findDataFile(const cv::String& relative_path, bool required = true)
+{
+    try
+    {
+        return cvtest::findDataFile(relative_path, required);
+    }
+    catch (const SkipTestException&)
+    {
+        throw;//rethrow
+    }
+    catch (...)
+    {
+        if (required)
+            throw;
+        throw SkipTestException("File not found: " + relative_path);
+    }
+    return cv::String();
+}
+
 // instantiates a brisque object for testing
 inline cv::Ptr<quality::QualityBRISQUE> create_brisque()
 {
     // location of BRISQUE model and range file
     //  place these files in ${OPENCV_TEST_DATA_PATH}/quality/, or the tests will be skipped
-    const auto model = cvtest::findDataFile(MODEL_FNAME, false);
-    const auto range = cvtest::findDataFile(RANGE_FNAME, false);
+    const auto model = findDataFile(MODEL_FNAME, false);
+    const auto range = findDataFile(RANGE_FNAME, false);
     return quality::QualityBRISQUE::create(model, range);
-
 }
 
 // static method
@@ -38,8 +58,8 @@ TEST(TEST_CASE_NAME, static_ )
     quality_expect_near(
         quality::QualityBRISQUE::compute(
             get_testfile_1a()
-            , cvtest::findDataFile(MODEL_FNAME, false)
-            , cvtest::findDataFile(RANGE_FNAME, false)
+            , findDataFile(MODEL_FNAME, false)
+            , findDataFile(RANGE_FNAME, false)
         )
         , BRISQUE_EXPECTED_1
     );
