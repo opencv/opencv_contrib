300aa15c1aa79280aed25ad93f12dab2d9e5cf93
Ahmed Ashour <asashour@yahoo.com>
2019-06-05 11:13:15 +0200

    docs: add missing parameter description

--- modules/tracking/samples/tracker.cpp
+++ modules/tracking/samples/tracker.cpp
@@ -50,14 +50,14 @@ int main( int argc, char** argv ){
       for(size_t npos=0,pos=0,ctr=0;ctr<4;ctr++){
         npos=initBoundingBox.find_first_of(',',pos);
         if(npos==string::npos && ctr<3){
-           printf("bounding box should be given in format \"x1,y1,x2,y2\",where x's and y's are integer cordinates of opposed corners of bdd box\n");
+           printf("bounding box should be given in format \"x1,y1,x2,y2\",where x's and y's are integer coordinates of opposed corners of bdd box\n");
            printf("got: %s\n",initBoundingBox.substr(pos,string::npos).c_str());
            printf("manual selection of bounding box will be employed\n");
            break;
         }
         int num=atoi(initBoundingBox.substr(pos,(ctr==3)?(string::npos):(npos-pos)).c_str());
         if(num<=0){
-           printf("bounding box should be given in format \"x1,y1,x2,y2\",where x's and y's are integer cordinates of opposed corners of bdd box\n");
+           printf("bounding box should be given in format \"x1,y1,x2,y2\",where x's and y's are integer coordinates of opposed corners of bdd box\n");
            printf("got: %s\n",initBoundingBox.substr(pos,npos-pos).c_str());
            printf("manual selection of bounding box will be employed\n");
            break;
