9397a658980da4336a870b1f5ad6aab793c33b87
Apoorv Goel <apoorvgoel_bt2k16@dtu.ac.in>
2019-06-06 17:25:38 +0300

    Regularisation Heirarchy

--- modules/rgbd/include/opencv2/rgbd/warpfield.hpp
+++ modules/rgbd/include/opencv2/rgbd/warpfield.hpp
@@ -13,23 +13,35 @@ struct CV_EXPORTS_W WarpNode
 {
     Point3f pos;
     float radius;
+    std::vector<Ptr<WarpNode> > children;
 };
+
+typedef std::vector<Ptr<WarpNode> > NodesLevelType;
+typedef std::vector<std::vector<int> > EdgesLevelType;
 
 class CV_EXPORTS_W WarpField
 {
 public:
-    WarpField();
-    void updateNodesFromPoints(InputArray _points, float resolution);
+    WarpField(int _maxNeighbours=1000000, int K=4, int levels=4, float baseResolution=0.025, float resolutionGrowth=2);
+    void updateNodesFromPoints(InputArray _points);
 
     std::vector<Ptr<WarpNode> > getNodes();
+    std::vector<NodesLevelType> getGraphNodes();
 
 private:
-    std::vector<Ptr<WarpNode> > nodes;
     void removeSupported(flann::GenericIndex<flann::L2_Simple<float> >& ind, std::vector<bool>& supInd);
-    void subsampleIndex(Mat& pmat, flann::GenericIndex<flann::L2_Simple<float> >& ind, std::vector<bool>& supInd, float res);
-    void appendNodeFromCluster(float res, Point3f p);
+    std::vector<Ptr<WarpNode> > subsampleIndex(Mat& pmat, flann::GenericIndex<flann::L2_Simple<float> >& ind, std::vector<bool>& supInd, float res);
+    void constructRegGraph();
 
+    std::vector<Ptr<WarpNode> > nodes; //heirarchy level 0
     int maxNeighbours;
+
+    int k; //k-nearest neighbours will be used
+    int n_levels; // number of levels in the heirarchy
+    float baseRes;
+    float resGrowthRate;
+    
+    std::vector<NodesLevelType> regGraphNodes; // heirarchy levels 1 to L
 };
 
 } // namepsace dynafu
