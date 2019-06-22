1901739450ffd3fdfba830066ea975be83e5d09d
Fady Essam <fadyesam1996@gmail.com>
2019-06-22 19:56:26 +0200

    added constructors for GaussianBlur transformation

--- modules/augment/include/opencv2/augment/gaussianblur.hpp
+++ modules/augment/include/opencv2/augment/gaussianblur.hpp
@@ -15,16 +15,34 @@ public:
     /* @brief Constructor to initialize the gaussian blur transformation with a range of kernel sizes and range of sigma values
        @param minKernelSize the min size of gaussian blur kernel
        @param maxKernelSize the max size of gaussian blur kernel
-       @param minKernelSize the min sigma value of the gaussian blur kernel
-       @param maxKernelSize the max sigma value of the gaussian blur kernel
+       @param minSigmaX the min standard deviation in the x direction of the gaussian blur kernel
+       @param maxSigmaX the max standard deviation in the x direction of the gaussian blur kernel
+       @param minSigmaY the min standard deviation in the y direction of the gaussian blur kernel
+       @param maxSigmaY the max standard deviation in the y direction of the gaussian blur kernel
     */
-    CV_WRAP GaussianBlur(int minKernelSize, int maxKernelSize, float minSigma, float maxSigma);
+    CV_WRAP GaussianBlur(Size minKernelSize, Size maxKernelSize, double minSigmaX, double maxSigmaX, double minSigmaY = 0, double maxSigmaY = 0);
 
-    /* @brief Constructor to initialize the gaussian blur transformation with a range of kernel sizes and sigma will be decided based on kernel size
+    /* @brief Constructor to initialize the gaussian blur transformation with a range of kernel sizes and range of sigma values (sigma in both directions is the same)
        @param minKernelSize the min size of gaussian blur kernel
        @param maxKernelSize the max size of gaussian blur kernel
+       @param minSigma the min standard deviation in both directions of the gaussian blur kernel
+       @param maxSigma the max standard deviation in both directions of the gaussian blur kernel
     */
-    CV_WRAP GaussianBlur(int minKernelSize, int maxKernelSize);
+    CV_WRAP GaussianBlur(int minKernelSize, int maxKernelSize, double minSigma = 0, double maxSigma = 0);
+
+    /* @brief Constructor to initialize the gaussian blur transformation with a specific kernel size and a same standard deviation in both directions
+       @param kernelSize the size of gaussian blur kernel
+       @param sigma the standard deviation in both directions of the gaussian blur kernel
+    */
+    CV_WRAP GaussianBlur(int kernelSize, double sigma = 0);
+
+    /* @brief Constructor to initialize the gaussian blur transformation with a specific kernel size and sigmaX and sigmaY
+       @param kernelSize the size of gaussian blur kernel
+       @param sigmaX the standard deviation in x direction of the gaussian blur kernel
+       @param sigmaY the standard deviation in y direction of the gaussian blur kernel
+    */
+    CV_WRAP GaussianBlur(Size kernelSize, double sigmaX, double sigmaY = 0);
+
 
     /* @brief Apply the gaussian blue to a single image
         @param src Input image to be blurred
@@ -37,12 +55,16 @@ public:
     void virtual init(const Mat& srcImage);
 
 private :
-    int minKernelSize;
-    int maxKernelSize;
-    float minSigma;
-    float maxSigma;
-    int currentKernelSize;
-    float currentSigma;
+    Size minKernelSize;
+    Size maxKernelSize;
+    float minSigmaX;
+    float minSigmaY;
+    float maxSigmaX;
+    float maxSigmaY;
+    Size kernelSize;
+    float sigmaX;
+    float sigmaY;
+    bool sameXY;
 
 };
 
--- modules/augment/src/gaussianblur.cpp
+++ modules/augment/src/gaussianblur.cpp
@@ -5,35 +5,52 @@
 #include "precomp.hpp"
 namespace cv { namespace augment {
 
-GaussianBlur::GaussianBlur(int minKernelSize, int maxKernelSize, float minSigma, float maxSigma)
+GaussianBlur::GaussianBlur(Size minKernelSize, Size maxKernelSize, double minSigmaX, double maxSigmaX, double minSigmaY, double maxSigmaY)
 {
     this->minKernelSize = minKernelSize;
     this->maxKernelSize = maxKernelSize;
-    this->minSigma = minSigma;
-    this->maxSigma = maxSigma;
+    this->minSigmaX = minSigmaX;
+    this->minSigmaY = minSigmaY;
+    this->maxSigmaX = maxSigmaX;
+    this->maxSigmaY = maxSigmaY;
+    sameXY = false;
 }
 
-GaussianBlur::GaussianBlur(int minKernelSize, int maxKernelSize)
+GaussianBlur::GaussianBlur(int minKernelSize, int maxKernelSize, double minSigma, double maxSigma)
 {
-    this->minKernelSize = minKernelSize;
-    this->maxKernelSize = maxKernelSize;
-    minSigma = maxSigma = 0; //the sigma will be decided depending on the kernel size
+    this->minKernelSize = Size(minKernelSize, minKernelSize);
+    this->maxKernelSize = Size(maxKernelSize, maxKernelSize);
+    minSigmaX = minSigmaY = minSigma;
+    maxSigmaX = maxSigmaY = maxSigma;
+    sameXY = true;
+}
+
+GaussianBlur::GaussianBlur(int kernelSize, double sigma)
+{
+    minKernelSize = maxKernelSize = Size(kernelSize, kernelSize);
+    minSigmaX = minSigmaY = maxSigmaY = maxSigmaX = sigma;
+    sameXY = true;
+}
+
+GaussianBlur::GaussianBlur(Size kernelSize, double sigmaX, double sigmaY)
+{
+    minKernelSize = maxKernelSize = kernelSize;
+    minSigmaX = maxSigmaX = sigmaX;
+    minSigmaY = maxSigmaY = sigmaY;
+    sameXY = false;
 }
 
 void GaussianBlur::init(const Mat& image)
 {
-    currentKernelSize = Transform::rng.uniform(minKernelSize, maxKernelSize+1); //+1 because the RNG.uniform excludes the max number
-    if (currentKernelSize % 2 == 0)
-    {
-        if (currentKernelSize > minKernelSize) currentKernelSize--;
-        else if (currentKernelSize < maxKernelSize) currentKernelSize++;
-    }
-    currentSigma = Transform::rng.uniform(minSigma, maxSigma);
+    kernelSize.height = Transform::rng.uniform(minKernelSize.height / 2, maxKernelSize.height / 2 + maxKernelSize.height % 2) * 2 + 1; //generate only random odd numbers
+    kernelSize.width = sameXY? kernelSize.height : Transform::rng.uniform(minKernelSize.width / 2, maxKernelSize.width / 2 + maxKernelSize.width % 2) * 2 + 1; //generate only random odd numbers
+    sigmaX = Transform::rng.uniform(minSigmaX, maxSigmaX);
+    sigmaY = sameXY? sigmaX : Transform::rng.uniform(minSigmaY, maxSigmaY);
 }
 
 void GaussianBlur::image(InputArray src, OutputArray dst)
 {
-    cv::GaussianBlur(src, dst, Size(currentKernelSize, currentKernelSize), currentSigma);
+    cv::GaussianBlur(src, dst, kernelSize, sigmaX, sigmaY);
 }
 
 }}
