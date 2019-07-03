ed7cc2d912842a123138c233a4e5e3174fc0ec6a
Leonid Beynenson <Leonid.Beynenson@intel.com>
2019-07-03 19:23:56 +0300

    Make proper filtering by class id in tracking_by_matching
    Also make the sample build in the case when opencv_dnn module is not
    built.
    Also help is added.

--- modules/tracking/samples/tracking_by_matching.cpp
+++ modules/tracking/samples/tracking_by_matching.cpp
@@ -20,21 +20,32 @@ static const char* keys =
     "{frame_step       |5| Frame step                       }"
     "{detector_model   | | Path to detector's Caffe model   }"
     "{detector_weights | | Path to detector's Caffe weights }"
+    "{desired_class_id |-1| The desired class that should be tracked }"
 };
 
 static void help()
 {
-  cout << "\nThis example shows the functionality of \"Long-term optical tracking API\""
-       "-- pause video [p] and draw a bounding box around the target to start the tracker\n"
+  cout << "\nThis example shows the functionality of \"Tracking-by-Matching\" approach:"
+      " detector is used to detect objects on frames, \n"
+      "matching is used to find correspondences between new detections and tracked objects.\n"
+      "Detection is made by DNN detection network every `--frame_step` frame.\n"
+      "Point .prototxt file of the network as the parameter `--detector_model`, and .caffemodel file"
+      " as the parameter `--detector_weights`.\n"
+      "(As an example of such detection network is a popular MobileNet_SSD network trained on VOC dataset.)\n"
+      "If `--desired_class_id` parameter is set, the detection result is filtered by class id,"
+      " returned by the detection network.\n"
+      "(That is, if a detection net was trained on VOC dataset, then to track pedestrians point --desired_class_id=15)\n"
        "Example of <video_name> is in opencv_extra/testdata/cv/tracking/\n"
        "Call:\n"
-       "./tracker <tracker_algorithm> <video_name> <start_frame> [<bounding_frame>]\n"
-       "tracker_algorithm can be: MIL, BOOSTING, MEDIANFLOW, TLD, KCF, GOTURN, MOSSE.\n"
+       "./example_tracking_tracking_by_matching --video_name=<video_name> --detector_model=<detector_model_path> --detector_weights=<detector_weights_path> \\\n"
+       "                                       [--start_frame=<start_frame>] \\\n"
+       "                                       [--frame_step=<frame_step>] \\\n"
+       "                                       [--desired_class_id=<desired_class_id>]\n"
        << endl;
 
   cout << "\n\nHot keys: \n"
        "\tq - quit the program\n"
-       "\tp - pause video\n";
+       "\tp - pause/resume video\n";
 }
 
 #ifdef HAVE_OPENCV_DNN
@@ -79,25 +90,27 @@ public:
         for (int i = 0; i < detection_as_mat.rows; i++)
         {
             float cur_confidence = detection_as_mat.at<float>(i, 2);
-            if (cur_confidence < confidence_threshold)
-                continue;
-            int cur_idx = static_cast<int>(detection_as_mat.at<float>(i, 1));
-            if ((desired_class_id >= 0) && (cur_idx != desired_class_id))
-                continue;
-
+            int cur_class_id = static_cast<int>(detection_as_mat.at<float>(i, 1));
             int x_left = static_cast<int>(detection_as_mat.at<float>(i, 3) * frame.cols);
             int y_bottom = static_cast<int>(detection_as_mat.at<float>(i, 4) * frame.rows);
             int x_right = static_cast<int>(detection_as_mat.at<float>(i, 5) * frame.cols);
             int y_top = static_cast<int>(detection_as_mat.at<float>(i, 6) * frame.rows);
 
             Rect cur_rect(x_left, y_bottom, (x_right - x_left), (y_top - y_bottom));
+
+            //TODO cout << "detectObjectsByNet: " << cur_rect << " conf=" << cur_confidence << " class=" << cur_class_id << endl;
+            if (cur_confidence < confidence_threshold)
+                continue;
+            if ((desired_class_id >= 0) && (cur_class_id != desired_class_id))
+                continue;
+
             //clipping by frame size
             cur_rect = cur_rect & Rect(Point(), frame.size());
             if (cur_rect.empty())
                 continue;
 
             //TODO: remove it
-            cout << "detectObjectsByNet: " << cur_rect << " " << cur_confidence << endl;
+            //cout << "detectObjectsByNet:+" << cur_rect << " conf=" << cur_confidence << " class=" << cur_class_id << endl;
 
             TrackedObject cur_obj(cur_rect, cur_confidence, frame_idx, -1);
             res.push_back(cur_obj);
@@ -114,6 +127,17 @@ private:
     Size net_size;
     Scalar net_mean;
     bool net_swapRB;
+};
+#else
+/// Stub for the case when opencv_dnn module is not built
+class DnnObjectDetector
+{
+public:
+    DnnObjectDetector(const String& , const String& , ...) {}
+    TrackedObjects detect(const cv::Mat& frame, int frame_idx)
+    {
+        return TrackedObjects();
+    }
 };
 #endif
 
@@ -135,6 +159,10 @@ CreateTrackerByMatchingWithFastDescriptor() {
     return tracker;
 }
 int main( int argc, char** argv ){
+#ifndef HAVE_OPENCV_DNN
+    CV_Error(Error::StsNotImplemented, "At the moment the sample 'tracking_by_matching' can work only when opencv_dnn module is built.");
+#endif
+
     CommandLineParser parser( argc, argv, keys );
     cv::Ptr<ITrackerByMatching> tracker = CreateTrackerByMatchingWithFastDescriptor();
 
@@ -143,7 +171,7 @@ int main( int argc, char** argv ){
     int frame_step = parser.get<int>("frame_step");
     String detector_model = parser.get<String>("detector_model");
     String detector_weights = parser.get<String>("detector_weights");
-    cout << "video_name + " << video_name << endl;
+    int desired_class_id = parser.get<int>("desired_class_id");
 
     if( video_name.empty() || detector_model.empty() || detector_weights.empty() )
     {
@@ -168,7 +196,7 @@ int main( int argc, char** argv ){
 
     // If you use the popular MobileNet_SSD detector, the default parameters may be used.
     // Otherwise, set your own parameters (net_mean, net_scalefactor, etc).
-    DnnObjectDetector detector(detector_model, detector_weights);
+    DnnObjectDetector detector(detector_model, detector_weights, desired_class_id);
 
     Mat frame;
     namedWindow( "Tracking by Matching", 1 );
@@ -180,15 +208,21 @@ int main( int argc, char** argv ){
     {
         if( paused )
         {
-            char c = (char) waitKey( 2 );
-            if( c == 'p' )
+            char c = (char) waitKey(30);
+            if (c == 'p')
                 paused = !paused;
+            if (c == 'q')
+                break;
             continue;
         }
 
         cap >> frame;
         if(frame.empty()){
             break;
+        }
+        if (frame_counter < start_frame) {
+            frame_counter++;
+            continue;
         }
 
 
@@ -226,14 +260,14 @@ int main( int argc, char** argv ){
         frame_counter++;
 
         char c = (char) waitKey( 2 );
-        if( c == 'q' )
+        if (c == 'q')
             break;
-        if( c == 'p' )
+        if (c == 'p')
             paused = !paused;
-
-        double s = frame_counter / (time_total / getTickFrequency());
-        printf("FPS: %f\n", s);
     }
 
-return 0;
+    double s = frame_counter / (time_total / getTickFrequency());
+    printf("FPS: %f\n", s);
+
+    return 0;
 }
