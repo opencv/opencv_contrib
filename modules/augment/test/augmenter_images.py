17003501b3f3201a00b0bce6fe2dbee5e2d152cb
Fady Essam <fadyesam1996@gmail.com>
2019-07-11 15:00:03 +0200

    added test for Augmenter.applyImages

---
+++ modules/augment/test/augmenter_images.py
@@ -0,0 +1,34 @@
+try:
+    import cv2 as cv
+except ImportError:
+    raise ImportError('Can\'t find OpenCV Python module. If you\'ve built it from sources without installation, '
+                      'configure environment variable PYTHONPATH to "opencv_build_dir/lib" directory (with "python3" subdirectory if required)')
+try:
+    import numpy as np
+except ImportError:
+    raise ImportError('Can\'t find numpy library, please make sure it is installed')
+
+if __name__ == "__main__":
+    ## for consistency
+    np.random.seed(seed=1)
+    cv.setRNGSeed(seed=1)
+
+    numberOfImages = np.random.randint(10,100)
+    widthOfImages = np.random.randint(400,2000)
+    heightOfImages = np.random.randint(400,2000)
+
+    aug = cv.augment_Augmenter()
+    aug.add(cv.augment_FlipHorizontal(), 0.7)
+    aug.add(cv.augment_FlipVertical(), 0.7)
+    aug.add(cv.augment_GaussianBlur(5,12), .7)
+    aug.add(cv.augment_Rotate(0,180), 1)
+    aug.add(cv.augment_Resize((1200,900)),1)
+
+    imgs = []
+
+    for i in range(10):
+        img = np.random.rand(heightOfImages, widthOfImages)
+        imgs.append(img)
+
+    imgs = imgs
+    imgs2 = aug.applyImages(imgs)
