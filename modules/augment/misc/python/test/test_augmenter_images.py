89bc9ddbf616718c14c3b001fb553ca9a0e51000
Fady Essam <fadyesam1996@gmail.com>
2019-07-13 10:55:56 +0200

    regression tests refactoring

--- modules/augment/misc/python/test/test_augmenter_images.py
+++ modules/augment/misc/python/test/test_augmenter_images.py
@@ -14,9 +14,9 @@ class augmenter_test(NewOpenCVTests):
         aug = cv.augment_Augmenter()
         aug.add(t=cv.augment_FlipHorizontal(), prob=0.7)
         aug.add(t=cv.augment_FlipVertical(), prob=0.5)
-        aug.add(t=cv.augment_GaussianBlur(5, 12), prob=0.7)
-        aug.add(t=cv.augment_Rotate(0, 180), prob=0.3)
-        aug.add(t=cv.augment_Resize((1200, 900)), prob=0.4)
+        aug.add(t=cv.augment_GaussianBlur(kernelSize=5, sigma=12), prob=0.7)
+        aug.add(t=cv.augment_Rotate(minAngle=0, maxAngle=180), prob=0.3)
+        aug.add(t=cv.augment_Resize(size=(1200, 900)), prob=0.4)
 
         imgs = []
 
@@ -36,9 +36,9 @@ class augmenter_test(NewOpenCVTests):
         aug = cv.augment_Augmenter()
         aug.add(t=cv.augment_FlipHorizontal(), prob=0.7)
         aug.add(t=cv.augment_FlipVertical(), prob=0.5)
-        aug.add(t=cv.augment_GaussianBlur(5, 12), prob=0.7)
-        aug.add(t=cv.augment_Rotate(0, 180), prob=0.3)
-        aug.add(t=cv.augment_Resize((1200, 900)), prob=0.4)
+        aug.add(t=cv.augment_GaussianBlur(kernelSize=5, sigma=12), prob=0.7)
+        aug.add(t=cv.augment_Rotate(minAngle=0, maxAngle=180), prob=0.3)
+        aug.add(t=cv.augment_Resize(size=(1200, 900)), prob=0.4)
 
         imgs = []
         masks = []
@@ -60,9 +60,9 @@ class augmenter_test(NewOpenCVTests):
         aug = cv.augment_Augmenter()
         aug.add(t=cv.augment_FlipHorizontal(), prob=0.7)
         aug.add(t=cv.augment_FlipVertical(), prob=0.5)
-        aug.add(t=cv.augment_GaussianBlur(5, 12), prob=0.7)
-        aug.add(t=cv.augment_Rotate(0, 180), prob=0.3)
-        aug.add(t=cv.augment_Resize((1200, 900)), prob=0.4)
+        aug.add(t=cv.augment_GaussianBlur(kernelSize=5, sigma=12), prob=0.7)
+        aug.add(t=cv.augment_Rotate(minAngle=0, maxAngle=180), prob=0.3)
+        aug.add(t=cv.augment_Resize(size=(1200, 900)), prob=0.4)
 
         imgs = []
         pointsArr = []
@@ -85,9 +85,9 @@ class augmenter_test(NewOpenCVTests):
         aug = cv.augment_Augmenter()
         aug.add(t=cv.augment_FlipHorizontal(), prob=0.7)
         aug.add(t=cv.augment_FlipVertical(), prob=0.5)
-        aug.add(t=cv.augment_GaussianBlur(5, 12), prob=0.7)
-        aug.add(t=cv.augment_Rotate(0, 180), prob=0.3)
-        aug.add(t=cv.augment_Resize((1200, 900)), prob=0.4)
+        aug.add(t=cv.augment_GaussianBlur(kernelSize=5, sigma=12), prob=0.7)
+        aug.add(t=cv.augment_Rotate(minAngle=0, maxAngle=180), prob=0.3)
+        aug.add(t=cv.augment_Resize(size=(1200, 900)), prob=0.4)
 
         imgs = []
         rectsArr = []
