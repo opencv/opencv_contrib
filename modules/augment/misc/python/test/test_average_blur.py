07eeac0ce115b2473310631548b822a30c717d50
Fady Essam <fadyesam1996@gmail.com>
2019-07-14 19:43:08 +0200

    added testing for AverageBlur and Rotate

---
+++ modules/augment/misc/python/test/test_average_blur.py
@@ -0,0 +1,30 @@
+import cv2 as cv
+import numpy as np
+from tests_common import NewOpenCVTests
+import utils
+
+## for consistency
+np.random.seed(seed=1)
+cv.setRNGSeed(seed=1)
+
+transformations = [cv.augment_AverageBlur(minKernelSize=3, maxKernelSize=15),
+                   cv.augment_AverageBlur(minKernelSize=(5,7), maxKernelSize=(13,15)),
+                   cv.augment_AverageBlur(kernelSize=7),
+                   cv.augment_AverageBlur(kernelSize=(5,7))]
+
+class averageBlurTest(NewOpenCVTests):
+    def test_image(self):
+        utils.test_image(transformations)
+
+
+    def test_point(self):
+        utils.test_points(transformations)
+
+
+    def test_rectangle(self):
+        utils.test_rectangles(transformations)
+
+
+    def test_polygon(self):
+        utils.test_polygons(transformations)
+
---
+++ modules/augment/misc/python/test/test_rotate.py
@@ -0,0 +1,28 @@
+import cv2 as cv
+import numpy as np
+from tests_common import NewOpenCVTests
+import utils
+
+## for consistency
+np.random.seed(seed=1)
+cv.setRNGSeed(seed=1)
+
+transformations = [cv.augment_Rotate(minAngle=0, maxAngle=180),
+                   cv.augment_Rotate(angle=45)]
+
+class rotateTest(NewOpenCVTests):
+    def test_image(self):
+        utils.test_image(transformations)
+
+
+    def test_point(self):
+        utils.test_points(transformations)
+
+
+    def test_rectangle(self):
+        utils.test_rectangles(transformations)
+
+
+    def test_polygon(self):
+        utils.test_polygons(transformations)
+
