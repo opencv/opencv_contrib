6585a14bb08a4b8f6b17011f747780b203bb7340
Fady Essam <fadyesam1996@gmail.com>
2019-07-21 00:51:03 +0200

    added optimized flip rectangle

--- modules/augment/src/flip.cpp
+++ modules/augment/src/flip.cpp
@@ -19,7 +19,7 @@ Point2f FlipHorizontal::point(const Point2f& src)
 
 Rect2f FlipHorizontal::rectangle(const Rect2f& src)
 {
-    return basicRectangle(src);
+    return Rect2f(srcImageCols - 1 - (src.x + src.width), src.y, src.width, src.height);
 }
 
 
@@ -37,7 +37,7 @@ Point2f FlipVertical::point(const Point2f& src)
 
 Rect2f FlipVertical::rectangle(const Rect2f& src)
 {
-    return basicRectangle(src);
+    return Rect2f(src.x, srcImageRows - 1 - (src.y + src.height), src.width, src.height);
 }
 
 }}
