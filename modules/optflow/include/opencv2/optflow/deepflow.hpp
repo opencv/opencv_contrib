0cf18c6251fe59d1017242b11180587ec9f2b970
chacha21 <pierre@chachatelier.fr>
2019-08-27 10:21:09 +0200

    Update deepflow.hpp
    Removed get/set methods for parameters

--- modules/optflow/include/opencv2/optflow/deepflow.hpp
+++ modules/optflow/include/opencv2/optflow/deepflow.hpp
@@ -25,30 +25,6 @@ public:
     void calc( InputArray I0, InputArray I1, InputOutputArray flow ) CV_OVERRIDE;
     void collectGarbage() CV_OVERRIDE;
 public:
-    CV_WRAP void setSigma(float val) {sigma = val;}
-    CV_WRAP float getSigma() const {return sigma;}
-    CV_WRAP void setMinSize(int val) {minSize = val;}
-    CV_WRAP int getMinSize() const {return minSize;}
-    CV_WRAP void setDownscaleFactor(float val) {downscaleFactor = val;}
-    CV_WRAP float getDownscaleFactor() const {return downscaleFactor;}
-    CV_WRAP void setFixedPointIterations(int val) {fixedPointIterations = val;}
-    CV_WRAP int getFixedPointIterations() const {return fixedPointIterations;}
-    CV_WRAP void setSorIterations(int val) {sorIterations = val;}
-    CV_WRAP int getSorIterations() const {return sorIterations;}
-    CV_WRAP void setAlpha(float val) {alpha = val;}
-    CV_WRAP float getAlpha() const {return alpha;}
-    CV_WRAP void setDelta(float val) {delta = val;}
-    CV_WRAP float getDelta() const {return delta;}
-    CV_WRAP void setGamma(float val) {gamma = val;}
-    CV_WRAP float getGamma() const {return gamma;}
-    CV_WRAP void setOmega(float val) {omega = val;}
-    CV_WRAP float getOmega() const {return omega;}
-    CV_WRAP void setMaxLayers(int val) {maxLayers = val;}
-    CV_WRAP int getMaxLayers() const {return maxLayers;}
-    CV_WRAP void setInterpolationType(int val) {interpolationType = val;}
-    CV_WRAP int getInterpolationType() const {return interpolationType;}
-
-public:
     float sigma; // Gaussian smoothing parameter
     int minSize; // minimal dimension of an image in the pyramid
     float downscaleFactor; // scaling factor in the pyramid
