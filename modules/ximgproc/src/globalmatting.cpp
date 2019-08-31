65818c67dee7000544b46472ce69fdaf5f1143c0
Vedanta Jha <jhavedanta@hotmail.com>
2019-09-01 01:19:01 +0530

    Added license and removed extra header files

--- modules/ximgproc/src/globalmatting.cpp
+++ modules/ximgproc/src/globalmatting.cpp
@@ -1,6 +1,6 @@
-#include <string>
-#include <cstdlib>
-#include <opencv2/ximgproc.hpp>
+// This file is part of OpenCV project.
+// It is subject to the license terms in the LICENSE file found in the top-level directory
+// of this distribution and at http://opencv.org/license.html.
 #include "precomp.hpp"
 
 
