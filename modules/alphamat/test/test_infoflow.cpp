c5cd2388a63176b1b7a2609aaf19ebbce7be5e1c
muskaankularia <muskaankularia@gmail.com>
2019-09-03 11:49:56 +0530

    test file header updated

--- modules/alphamat/test/test_infoflow.cpp
+++ modules/alphamat/test/test_infoflow.cpp
@@ -2,8 +2,6 @@
 // It is subject to the license terms in the LICENSE file found in the top-level directory
 // of this distribution and at http://opencv.org/license.html.
 #include "test_precomp.hpp"
-#include <opencv2/ts/ts.hpp>
-
 
 namespace opencv_test { namespace {
 
