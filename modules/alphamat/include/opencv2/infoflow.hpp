18b14b2d970f52b279972afdb45088849626d501
muskaankularia <muskaankularia@gmail.com>
2019-09-03 23:17:41 +0530

    path to Eigen added

--- modules/alphamat/include/opencv2/infoflow.hpp
+++ modules/alphamat/include/opencv2/infoflow.hpp
@@ -18,7 +18,7 @@ The following four models are implemented:
 - LapSRN <https://arxiv.org/abs/1710.01992>
 
 */
-#include "Eigen/Sparse"
+#include "../../src/Eigen/Sparse"
 using namespace Eigen; 
 
 namespace cv{ namespace alphamat{
