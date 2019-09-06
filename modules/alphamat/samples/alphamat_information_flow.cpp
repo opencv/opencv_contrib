db50a052adc07ee3681928f2cf2fbd8e9c8a8147
Sunita Nayak <nayak.sunita@gmail.com>
2019-09-05 22:26:35 -0700

    edited cpp sample

--- modules/alphamat/samples/alphamat_information_flow.cpp
+++ modules/alphamat/samples/alphamat_information_flow.cpp
@@ -13,29 +13,68 @@ using namespace std;
 using namespace cv;
 using namespace cv::alphamat;
 
-int check_image(Mat& image)
+const char* keys =
 {
-    if ( !image.data )                              // Check for invalid input
-    {
-        cout <<  "Could not open or find the image" << endl;
-        return -1;
-    }
-    return 0;
-}
+    "{img || input image name}"
+    "{tri || input trimap image name}"
+    "{out || output image name}"
+};
 
 int main(int argc, char *argv[])
 {
+    bool show_help = (argc == 1);
+    show_help = show_help || (argc == 2 && string(argv[1]) == "--help");
+    show_help = show_help || (argc == 2 && string(argv[1]) == "-h");
+    
+    if (show_help)
+    {
+        printf("\nThis sample demonstrates Information Flow alpha matting\n"
+               "Call:\n"
+               "    alphamat_information_flow -img=<string> -tri=<string> [-out=<string>]\n\n");
+        return 0;
+    }
+    
+    CommandLineParser parser(argc, argv, keys);
+    if (!parser.check())
+    {
+        parser.printErrors();
+        return -1;
+    }
+    
+    string img_path = parser.get<std::string>("img");
+    string trimap_path = parser.get<std::string>("tri");
+    string result_path = parser.get<std::string>("out");
+    
     Mat image, tmap;
-    char* img_path = argv[1];
-    image = imread(img_path);   // Read the file
-    check_image(image);
+    
+    image = imread(img_path, IMREAD_COLOR);   // Read the input image file
+    if (image.empty())
+    {
+        printf("Cannot read image file: %s\n", img_path.c_str());
+        return -1;
+    }
 
-    char* tmap_path = argv[2];
-    tmap = imread(tmap_path);
-    check_image(tmap);
+    tmap = imread(trimap_path, IMREAD_GRAYSCALE);
+    if (tmap.empty())
+    {
+        printf("Cannot read trimap file: %s\n", trimap_path.c_str());
+        return -1;
+    }
 
     Mat result;
     infoFlow(image, tmap, result, false, true);
+    
+    if (result_path.empty())
+    {
+        namedWindow("result alpha matte", WINDOW_NORMAL);
+        imshow("result alpha matte", result);
+        waitKey(0);
+    }
+    else
+    {
+        imwrite(result_path, result);
+    }
+
     imshow("Result Matte", result);
     return 0;
     
