cd7d4268db2d8f622c767661de9951801118eb72
Moeed Shaik <shaikmoeed@gmail.com>
2019-09-22 21:58:56 +0530

    Fix bug: TypeError- integer argument expected, got float

--- modules/optflow/samples/motempl.py
+++ modules/optflow/samples/motempl.py
@@ -14,8 +14,8 @@ def nothing(dummy):
 def draw_motion_comp(vis, rect, angle, color):
     x, y, w, h = rect
     cv.rectangle(vis, (x, y), (x+w, y+h), (0, 255, 0))
-    r = min(w/2, h/2)
-    cx, cy = x+w/2, y+h/2
+    r = min(w//2, h//2)
+    cx, cy = x+w//2, y+h//2
     angle = angle*np.pi/180
     cv.circle(vis, (cx, cy), r, color, 3)
     cv.line(vis, (cx, cy), (int(cx+np.cos(angle)*r), int(cy+np.sin(angle)*r)), color, 3)
