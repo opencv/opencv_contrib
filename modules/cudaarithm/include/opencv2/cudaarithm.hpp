20e4292250d72dbeac095fc00dff1a7afebdd268
cudawarped <mail@jamesbowley.co.uk>
2019-12-27 18:49:13 +0200

    Added shift and hog.

--- modules/cudaarithm/include/opencv2/cudaarithm.hpp
+++ modules/cudaarithm/include/opencv2/cudaarithm.hpp
@@ -274,6 +274,10 @@ CV_EXPORTS_W void bitwise_xor(InputArray src1, InputArray src2, OutputArray dst,
  */
 CV_EXPORTS void rshift(InputArray src, Scalar_<int> val, OutputArray dst, Stream& stream = Stream::Null());
 
+CV_EXPORTS_W inline void rshift(InputArray src, Scalar val, OutputArray dst, Stream& stream = Stream::Null()) {
+    rshift(src, Scalar_<int>(val), dst, stream);
+}
+
 /** @brief Performs pixel by pixel right left of an image by a constant value.
 
 @param src Source matrix. Supports 1, 3 and 4 channels images with CV_8U , CV_16U or CV_32S
@@ -283,6 +287,10 @@ depth.
 @param stream Stream for the asynchronous version.
  */
 CV_EXPORTS void lshift(InputArray src, Scalar_<int> val, OutputArray dst, Stream& stream = Stream::Null());
+
+CV_EXPORTS_W inline void lshift(InputArray src, Scalar val, OutputArray dst, Stream& stream = Stream::Null()) {
+    lshift(src, Scalar_<int>(val), dst, stream);
+}
 
 /** @brief Computes the per-element minimum of two matrices (or a matrix and a scalar).
 
