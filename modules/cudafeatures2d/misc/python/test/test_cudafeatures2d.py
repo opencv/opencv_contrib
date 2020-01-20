c8f2d2c8ed5045182c634e78c2f17768d29c1bc6
cudawarped <mail@jamesbowley.co.uk>
2020-01-20 18:29:56 +0000

    Updated KeyPoints in cuda::ORB::Convert python wrapper to be an output argument.

--- modules/cudafeatures2d/misc/python/test/test_cudafeatures2d.py
+++ modules/cudafeatures2d/misc/python/test/test_cudafeatures2d.py
@@ -30,6 +30,9 @@ class cudafeatures2d_test(NewOpenCVTests):
         _kps1, descs1 = orb.detectAndComputeAsync(cuMat1, None)
         _kps2, descs2 = orb.detectAndComputeAsync(cuMat2, None)
 
+        self.assertTrue(len(orb.convert(_kps1)) == _kps1.size()[0])
+        self.assertTrue(len(orb.convert(_kps2)) == _kps2.size()[0])
+
         bf = cv.cuda_DescriptorMatcher.createBFMatcher(cv.NORM_HAMMING)
         matches = bf.match(descs1, descs2)
         self.assertGreater(len(matches), 0)
