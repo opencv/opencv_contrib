4ad97ee4bdb6d315f2324d6022b56299119f1661
tsenst <senst@nue.tu-berlin.de>
2020-03-21 20:05:55 +0100

    * add HAL for PLK ILL

--- modules/optflow/src/rlof/rlof_invokerbase.hpp
+++ modules/optflow/src/rlof/rlof_invokerbase.hpp
@@ -151,8 +151,274 @@ typedef uchar tMaskType;
 #define tCVMaskType CV_8UC1
 #define MaskSet 0xffffffff
 
-static
-void getLocalPatch(
+static inline void copyWinBuffers(int iw00, int iw01, int iw10, int iw11,
+    Size winSize,
+    const Mat & I, const Mat & derivI, const Mat & winMaskMat,
+    Mat & IWinBuf, Mat & derivIWinBuf,
+    Point iprevPt)
+{
+    int cn = I.channels(), cn2 = cn * 2;
+    const int W_BITS = 14, W_BITS1 = 14;
+#ifdef CV_SIMD128
+    v_int16x8 vqw0((short)(iw00), (short)(iw01), (short)(iw00), (short)(iw01), (short)(iw00), (short)(iw01), (short)(iw00), (short)(iw01));
+    v_int16x8 vqw1((short)(iw10), (short)(iw11), (short)(iw10), (short)(iw11), (short)(iw10), (short)(iw11), (short)(iw10), (short)(iw11));
+    v_int32x4 vdelta_d = v_setall_s32(1 << (W_BITS1 - 1));
+    v_int32x4 vdelta = v_setall_s32(1 << (W_BITS1 - 5 - 1));
+    v_int32x4 vmax_val_32 = v_setall_s32(std::numeric_limits<unsigned int>::max());
+    v_int32x4 vmask_border_0, vmask_border_1;
+    getVBitMask(winSize.width, vmask_border_0, vmask_border_1);
+#endif
+
+    // extract the patch from the first image, compute covariation matrix of derivatives
+    int x, y;
+    for (y = 0; y < winSize.height; y++)
+    {
+        const uchar* src = I.ptr<uchar>(y + iprevPt.y, 0) + iprevPt.x*cn;
+        const uchar* src1 = I.ptr<uchar>(y + iprevPt.y + 1, 0) + iprevPt.x*cn;
+        const short* dsrc = derivI.ptr<short>(y + iprevPt.y, 0) + iprevPt.x*cn2;
+        const short* dsrc1 = derivI.ptr<short>(y + iprevPt.y + 1, 0) + iprevPt.x*cn2;
+        short* Iptr = IWinBuf.ptr<short>(y, 0);
+        short* dIptr = derivIWinBuf.ptr<short>(y, 0);
+        const tMaskType* maskPtr = winMaskMat.ptr<tMaskType>(y, 0);
+        x = 0;
+#ifdef CV_SIMD128
+
+        for (; x <= winSize.width*cn; x += 8, dsrc += 8 * 2, dsrc1 += 8 * 2, dIptr += 8 * 2)
+        {
+            v_int32x4 vmask0 = v_reinterpret_as_s32(v_load_expand_q(maskPtr + x)) * vmax_val_32;
+            v_int32x4 vmask1 = v_reinterpret_as_s32(v_load_expand_q(maskPtr + x + 4)) * vmax_val_32;
+            if (x + 4 > winSize.width)
+            {
+                vmask0 = vmask0 & vmask_border_0;
+            }
+            if (x + 8 > winSize.width)
+            {
+                vmask1 = vmask1 & vmask_border_1;
+            }
+
+            v_int32x4 t0, t1;
+            v_int16x8 v00, v01, v10, v11, t00, t01, t10, t11;
+            v00 = v_reinterpret_as_s16(v_load_expand(src + x));
+            v01 = v_reinterpret_as_s16(v_load_expand(src + x + cn));
+            v10 = v_reinterpret_as_s16(v_load_expand(src1 + x));
+            v11 = v_reinterpret_as_s16(v_load_expand(src1 + x + cn));
+
+            v_zip(v00, v01, t00, t01);
+            v_zip(v10, v11, t10, t11);
+            t0 = v_dotprod(t00, vqw0, vdelta) + v_dotprod(t10, vqw1);
+            t1 = v_dotprod(t01, vqw0, vdelta) + v_dotprod(t11, vqw1);
+            t0 = t0 >> (W_BITS1 - 5)  & vmask0;
+            t1 = t1 >> (W_BITS1 - 5)  & vmask1;
+            v_store(Iptr + x, v_pack(t0, t1));
+
+            v00 = v_reinterpret_as_s16(v_load(dsrc));
+            v01 = v_reinterpret_as_s16(v_load(dsrc + cn2));
+            v10 = v_reinterpret_as_s16(v_load(dsrc1));
+            v11 = v_reinterpret_as_s16(v_load(dsrc1 + cn2));
+
+            v_zip(v00, v01, t00, t01);
+            v_zip(v10, v11, t10, t11);
+
+            t0 = v_dotprod(t00, vqw0, vdelta_d) + v_dotprod(t10, vqw1);
+            t1 = v_dotprod(t01, vqw0, vdelta_d) + v_dotprod(t11, vqw1);
+            t0 = t0 >> W_BITS1;
+            t1 = t1 >> W_BITS1;
+            v00 = v_pack(t0, t1); // Ix0 Iy0 Ix1 Iy1 ...
+            v00 = v00 & v_reinterpret_as_s16(vmask0);
+            v_store(dIptr, v00);
+
+            v00 = v_reinterpret_as_s16(v_load(dsrc + 4 * 2));
+            v01 = v_reinterpret_as_s16(v_load(dsrc + 4 * 2 + cn2));
+            v10 = v_reinterpret_as_s16(v_load(dsrc1 + 4 * 2));
+            v11 = v_reinterpret_as_s16(v_load(dsrc1 + 4 * 2 + cn2));
+
+            v_zip(v00, v01, t00, t01);
+            v_zip(v10, v11, t10, t11);
+
+            t0 = v_dotprod(t00, vqw0, vdelta_d) + v_dotprod(t10, vqw1);
+            t1 = v_dotprod(t01, vqw0, vdelta_d) + v_dotprod(t11, vqw1);
+            t0 = t0 >> W_BITS1;
+            t1 = t1 >> W_BITS1;
+            v00 = v_pack(t0, t1); // Ix0 Iy0 Ix1 Iy1 ...
+            v00 = v00 & v_reinterpret_as_s16(vmask1);
+            v_store(dIptr + 4 * 2, v00);
+        }
+#else
+        for (; x < winSize.width*cn; x++, dsrc += 2, dsrc1 += 2, dIptr += 2)
+        {
+            if (maskPtr[x] == 0)
+            {
+                dIptr[0] = 0;
+                dIptr[1] = 0;
+                continue;
+            }
+            int ival = CV_DESCALE(src[x] * iw00 + src[x + cn] * iw01 +
+                src1[x] * iw10 + src1[x + cn] * iw11, W_BITS1 - 5);
+            int ixval = CV_DESCALE(dsrc[0] * iw00 + dsrc[cn2] * iw01 +
+                dsrc1[0] * iw10 + dsrc1[cn2] * iw11, W_BITS1);
+            int iyval = CV_DESCALE(dsrc[1] * iw00 + dsrc[cn2 + 1] * iw01 + dsrc1[1] * iw10 +
+                dsrc1[cn2 + 1] * iw11, W_BITS1);
+
+            Iptr[x] = (short)ival;
+            dIptr[0] = (short)ixval;
+            dIptr[1] = (short)iyval;
+        }
+#endif
+    }
+}
+
+static inline void copyWinBuffers(int iw00, int iw01, int iw10, int iw11,
+    Size winSize,
+    const Mat & I, const Mat & derivI, const Mat & winMaskMat,
+    Mat & IWinBuf, Mat & derivIWinBuf,
+    float & A11, float & A22, float & A12,
+    Point iprevPt)
+{
+    const float FLT_SCALE = (1.f / (1 << 20));
+    int cn = I.channels(), cn2 = cn * 2;
+    const int W_BITS = 14, W_BITS1 = 14;
+#ifdef CV_SIMD128
+    v_int16x8 vqw0((short)(iw00), (short)(iw01), (short)(iw00), (short)(iw01), (short)(iw00), (short)(iw01), (short)(iw00), (short)(iw01));
+    v_int16x8 vqw1((short)(iw10), (short)(iw11), (short)(iw10), (short)(iw11), (short)(iw10), (short)(iw11), (short)(iw10), (short)(iw11));
+    v_int32x4 vdelta_d = v_setall_s32(1 << (W_BITS1 - 1));
+    v_int32x4 vdelta = v_setall_s32(1 << (W_BITS1 - 5 - 1));
+    v_int32x4 vmax_val_32 = v_setall_s32(std::numeric_limits<unsigned int>::max());
+    v_int32x4 vmask_border0, vmask_border1;
+    v_float32x4 vA11 = v_setzero_f32(), vA22 = v_setzero_f32(), vA12 = v_setzero_f32();
+    getVBitMask(winSize.width, vmask_border0, vmask_border1);
+#endif
+
+    // extract the patch from the first image, compute covariation matrix of derivatives
+    for (int y = 0; y < winSize.height; y++)
+    {
+        const uchar* src = I.ptr<uchar>(y + iprevPt.y, 0) + iprevPt.x*cn;
+        const uchar* src1 = I.ptr<uchar>(y + iprevPt.y + 1, 0) + iprevPt.x*cn;
+        const short* dsrc = derivI.ptr<short>(y + iprevPt.y, 0) + iprevPt.x*cn2;
+        const short* dsrc1 = derivI.ptr<short>(y + iprevPt.y + 1, 0) + iprevPt.x*cn2;
+        short* Iptr = IWinBuf.ptr<short>(y, 0);
+        short* dIptr = derivIWinBuf.ptr<short>(y, 0);
+        const tMaskType* maskPtr = winMaskMat.ptr<tMaskType>(y, 0);
+#ifdef CV_SIMD128
+        for (int x = 0; x <= winSize.width*cn; x += 8, dsrc += 8 * 2, dsrc1 += 8 * 2, dIptr += 8 * 2)
+        {
+            v_int16x8 vmask_16 = v_reinterpret_as_s16(v_load_expand(maskPtr + x));
+            v_int32x4 vmask0 = v_reinterpret_as_s32(v_load_expand_q(maskPtr + x)) * vmax_val_32;
+            v_int32x4 vmask1 = v_reinterpret_as_s32(v_load_expand_q(maskPtr + x + 4)) * vmax_val_32;
+            if (x + 4 > winSize.width)
+            {
+                vmask0 = vmask0 & vmask_border0;
+            }
+            if (x + 8 > winSize.width)
+            {
+                vmask1 = vmask1 & vmask_border1;
+            }
+
+            v_int32x4 t0, t1;
+            v_int16x8 v00, v01, v10, v11, t00, t01, t10, t11;
+            v00 = v_reinterpret_as_s16(v_load_expand(src + x));
+            v01 = v_reinterpret_as_s16(v_load_expand(src + x + cn));
+            v10 = v_reinterpret_as_s16(v_load_expand(src1 + x));
+            v11 = v_reinterpret_as_s16(v_load_expand(src1 + x + cn));
+
+            v_zip(v00, v01, t00, t01);
+            v_zip(v10, v11, t10, t11);
+            t0 = v_dotprod(t00, vqw0, vdelta) + v_dotprod(t10, vqw1);
+            t1 = v_dotprod(t01, vqw0, vdelta) + v_dotprod(t11, vqw1);
+            t0 = t0 >> (W_BITS1 - 5);
+            t1 = t1 >> (W_BITS1 - 5);
+            t0 = t0 & vmask0;
+            t1 = t1 & vmask1;
+            v_store(Iptr + x, v_pack(t0, t1));
+
+            v00 = v_reinterpret_as_s16(v_load(dsrc));
+            v01 = v_reinterpret_as_s16(v_load(dsrc + cn2));
+            v10 = v_reinterpret_as_s16(v_load(dsrc1));
+            v11 = v_reinterpret_as_s16(v_load(dsrc1 + cn2));
+
+            v_zip(v00, v01, t00, t01);
+            v_zip(v10, v11, t10, t11);
+
+            t0 = v_dotprod(t00, vqw0, vdelta_d) + v_dotprod(t10, vqw1);
+            t1 = v_dotprod(t01, vqw0, vdelta_d) + v_dotprod(t11, vqw1);
+            t0 = t0 >> W_BITS1;
+            t1 = t1 >> W_BITS1;
+            v00 = v_pack(t0, t1); // Ix0 Iy0 Ix1 Iy1 ...
+            v00 = v00 & v_reinterpret_as_s16(vmask0);
+            v_store(dIptr, v00);
+
+            v00 = v_reinterpret_as_s16(v_interleave_pairs(v_reinterpret_as_s32(v_interleave_pairs(v00))));
+            v_expand(v00, t1, t0);
+
+            v_float32x4 fy = v_cvt_f32(t0);
+            v_float32x4 fx = v_cvt_f32(t1);
+
+            vA22 = v_muladd(fy, fy, vA22);
+            vA12 = v_muladd(fx, fy, vA12);
+            vA11 = v_muladd(fx, fx, vA11);
+
+            v00 = v_reinterpret_as_s16(v_load(dsrc + 4 * 2));
+            v01 = v_reinterpret_as_s16(v_load(dsrc + 4 * 2 + cn2));
+            v10 = v_reinterpret_as_s16(v_load(dsrc1 + 4 * 2));
+            v11 = v_reinterpret_as_s16(v_load(dsrc1 + 4 * 2 + cn2));
+
+            v_zip(v00, v01, t00, t01);
+            v_zip(v10, v11, t10, t11);
+
+            t0 = v_dotprod(t00, vqw0, vdelta_d) + v_dotprod(t10, vqw1);
+            t1 = v_dotprod(t01, vqw0, vdelta_d) + v_dotprod(t11, vqw1);
+            t0 = t0 >> W_BITS1;
+            t1 = t1 >> W_BITS1;
+            v00 = v_pack(t0, t1); // Ix0 Iy0 Ix1 Iy1 ...
+            v00 = v00 & v_reinterpret_as_s16(vmask1);
+            v_store(dIptr + 4 * 2, v00);
+
+            v00 = v_reinterpret_as_s16(v_interleave_pairs(v_reinterpret_as_s32(v_interleave_pairs(v00))));
+            v_expand(v00, t1, t0);
+
+            fy = v_cvt_f32(t0);
+            fx = v_cvt_f32(t1);
+
+            vA22 = v_muladd(fy, fy, vA22);
+            vA12 = v_muladd(fx, fy, vA12);
+            vA11 = v_muladd(fx, fx, vA11);
+        }
+#else
+        for (; x < winSize.width*cn; x++, dsrc += 2, dsrc1 += 2, dIptr += 2)
+        {
+            if (maskPtr[x] == 0)
+            {
+                dIptr[0] = 0;
+                dIptr[1] = 0;
+                continue;
+            }
+            int ival = CV_DESCALE(src[x] * iw00 + src[x + cn] * iw01 +
+                src1[x] * iw10 + src1[x + cn] * iw11, W_BITS1 - 5);
+            int ixval = CV_DESCALE(dsrc[0] * iw00 + dsrc[cn2] * iw01 +
+                dsrc1[0] * iw10 + dsrc1[cn2] * iw11, W_BITS1);
+            int iyval = CV_DESCALE(dsrc[1] * iw00 + dsrc[cn2 + 1] * iw01 + dsrc1[1] * iw10 +
+                dsrc1[cn2 + 1] * iw11, W_BITS1);
+
+            Iptr[x] = (short)ival;
+            dIptr[0] = (short)ixval;
+            dIptr[1] = (short)iyval;
+            A11 += (float)(ixval*ixval);
+            A12 += (float)(ixval*iyval);
+            A22 += (float)(iyval*iyval);
+        }
+#endif
+    }
+#ifdef CV_SIMD128
+    A11 += v_reduce_sum(vA11);
+    A12 += v_reduce_sum(vA12);
+    A22 += v_reduce_sum(vA22);
+#endif
+
+    A11 *= FLT_SCALE;
+    A12 *= FLT_SCALE;
+    A22 *= FLT_SCALE;
+}
+
+static void getLocalPatch(
         const cv::Mat & src,
         const cv::Point2i & prevPoint, // feature points
         cv::Mat & winPointMask,
