67ebb52a0cd9b88026cc32e5c3c07cff8e4bf598
ilbalance <ilbalance476@gmail.com>
2020-05-21 22:15:05 +0300

    Fixes for comments, added new comments in code, code style fixes, averaging problem not solved

--- modules/tracking/samples/lasot_benchmark.py
+++ modules/tracking/samples/lasot_benchmark.py
@@ -2,53 +2,45 @@ import numpy as np
 import cv2 as cv
 import glob
 import argparse
+import os
 
+# Method used for initializing of trackers by creating it 
+# via cv.TrackerX_create()
 def init_tracker(tracker_name):
-    if tracker_name == "Boosting":
-        tracker = cv.TrackerBoosting_create()
-        frames_before_reinit = 500
-    elif tracker_name == "MIL":
-        tracker = cv.TrackerMIL_create()
-        frames_before_reinit = 1000
-    elif tracker_name == "KCF":
-        tracker = cv.TrackerKCF_create()
-        frames_before_reinit = 110
-    elif tracker_name == "MedianFlow":
-        tracker = cv.TrackerMedianFlow_create()
-        frames_before_reinit = 20
-    elif tracker_name == "GOTURN":
-        tracker = cv.TrackerGOTURN_create()
-        frames_before_reinit = 20
-    elif tracker_name == "MOSSE":
-        tracker = cv.TrackerMOSSE_create()
-        frames_before_reinit = 20
-    elif tracker_name == "CSRT":
-        tracker = cv.TrackerCSRT_create()
-        frames_before_reinit = 250
-    return [tracker, frames_before_reinit]
+    config = {"Boosting": (cv.TrackerBoosting_create(), 500),
+    "MIL": (cv.TrackerMIL_create(), 1000),
+    "KCF": (cv.TrackerKCF_create(), 110),
+    "MedianFlow": (cv.TrackerMedianFlow_create(), 20),
+    "GOTURN": (cv.TrackerGOTURN_create(), 20),
+    "MOSSE": (cv.TrackerMOSSE_create(), 20),
+    "CSRT": (cv.TrackerCSRT_create(), 250)}
+    return config[tracker_name]
 
 def main():
-    parser = argparse.ArgumentParser(description="Run LaSOT-based benchmark for visual object trackers")
+    parser = argparse.ArgumentParser(
+        description="Run LaSOT-based benchmark for visual object trackers")
+    # As a default argument used name of 
+    # original dataset folder
     parser.add_argument("--path_to_dataset", type=str,
-                        default="Test part of LaSOT dataset", help="Full path to LaSOT folder")
+                        default="LaSOTTesting", help="Full path to LaSOT")
     args = parser.parse_args()
 
+    # Creating list with names of videos via reading names from txt file
     list_of_videos = [None for _ in range(280)]
-    file_with_video_names = open(args.path_to_dataset + "/testing_set.txt", "r")
+    video_names = os.path.join(args.path_to_dataset, "/testing_set.txt")
+    with open(video_names, 'rt') as f:
+        list_of_videos = f.read().rstrip('\n').split('\n')
 
-    for i in range(len(list_of_videos)):
-        list_of_videos[i] = file_with_video_names.readline()
+    trackers = [
+        'Boosting', 'MIL', 'KCF', 'MedianFlow', 'GOTURN', 'MOSSE', 'CSRT']
 
-    trackers = {'Boosting': 500, 'MIL': 1000, 'KCF': 110,
-                'MedianFlow': 20, 'GOTURN': 20, 'MOSSE': 20, 'CSRT': 250}
+    # Loop for every tracker
+    for tracker_id in range(len(trackers)):
 
-    #For every tracker
-    for tracker_id in range(len(list(trackers.keys()))):
-
-        tracker_name = list(trackers.keys())[tracker_id]
+        tracker_name = trackers[tracker_id]
         print(tracker_name)
 
-        #For every video
+        # Loop for every video
         for video_name in list_of_videos:
 
             tracker, frames_before_reinit = init_tracker(tracker_name)
@@ -56,103 +48,132 @@ def main():
 
             print("Video: " + str(video_name))
 
+            # Open specific video and read ground truth for it
             video_name = video_name.replace("\n", "")
             gt_file = open(
                 args.path_to_dataset + video_name + "/groundtruth.txt", "r")
             gt_bb = gt_file.readline().replace("\n", "").split(",")
             init_bb = gt_bb
-
-            for _ in range(len(init_bb)):
-                init_bb[_] = float(init_bb[_])
-
-            init_bb = tuple(init_bb)
+            init_bb = tuple([float(b) for b in init_bb])
 
             print("Initial bounding box: ", init_bb)
 
-            video_sequence = sorted(
-                glob.glob(str(args.path_to_dataset + str(video_name) + "/img/*.jpg")))
+            # Creating blob from image sequence
+            video_sequence = sorted(glob.glob(str(args.path_to_dataset + str(
+                video_name) + "/img/*.jpg")))
 
             print("Number of frames in video: " + str(len(video_sequence)))
 
+            # Variables for saving sum of every metric for every frame and
+            # every video respectively
             sum_iou = 0
             sum_pr = 0
             sum_norm_pr = 0
+            iou_values = 0
+            pr_values = 0
+            norm_pr_values = 0
             frame_counter = len(video_sequence)
 
-            #For every frame in video
+            # For every frame in video
             for number_of_the_frame, image in enumerate(video_sequence):
 
-                for _ in range(len(gt_bb)):
-                    gt_bb[_] = float(gt_bb[_])
+                for i in range(len(gt_bb)):
+                    gt_bb[i] = float(gt_bb[i])
                 gt_bb = tuple(gt_bb)
                 frame = cv.imread(image)
 
-                #Condition of tracker re-initialization
+                # Condition of tracker re-initialization
                 if ((number_of_the_frame + 1) % frames_before_reinit == 0) and (
                         number_of_the_frame != 0):
 
                     tracker, frames_before_reinit = init_tracker(tracker_name)
                     init_once = False
                     init_bb = gt_bb
-                    first_frame = cv.imread(image)
 
                 if not init_once:
-                    temp = tracker.init(frame, init_bb)
+                    init_state = tracker.init(frame, init_bb)
                     init_once = True
-                temp, new_bb = tracker.update(frame)
+                init_state, new_bb = tracker.update(frame)
 
-                #Evaluation of coordinates of corners and centers from [x, y, w, h]
-                new_bb_xmin = new_bb[0]
-                new_bb_xmax = new_bb[0] + new_bb[2] - 1.0
-                new_bb_ymin = new_bb[1]
-                new_bb_ymax = new_bb[1] + new_bb[3] - 1.0
-                gt_xmin = gt_bb[0]
-                gt_xmax = gt_bb[0] + gt_bb[2] - 1.0
-                gt_ymin = gt_bb[1]
-                gt_ymax = gt_bb[1] + gt_bb[3] - 1.0
-                new_cx = new_bb[0] + (new_bb[2] + 1.0) / 2
-                new_cy = new_bb[1] + (new_bb[3] + 1.0) / 2
-                gt_cx = gt_bb[0] + (gt_bb[2] + 1.0) / 2
-                gt_cy = gt_bb[1] + (gt_bb[3] + 1.0) / 2
+                # Check for presence of object on the frame
+                # If no object on frame, we reduce number of
+                # accounted for evaluation frames
+                if gt_bb != (0, 0, 0, 0):
+                    # Evaluation of coordinates of corners and centers 
+                    # from [x, y, w, h] bounding boxes
+                    new_bb_xmin = new_bb[0]
+                    new_bb_xmax = new_bb[0] + new_bb[2] - 1.0
+                    new_bb_ymin = new_bb[1]
+                    new_bb_ymax = new_bb[1] + new_bb[3] - 1.0
+                    gt_xmin = gt_bb[0]
+                    gt_xmax = gt_bb[0] + gt_bb[2] - 1.0
+                    gt_ymin = gt_bb[1]
+                    gt_ymax = gt_bb[1] + gt_bb[3] - 1.0
+                    new_cx = new_bb[0] + (new_bb[2] + 1.0) / 2
+                    new_cy = new_bb[1] + (new_bb[3] + 1.0) / 2
+                    gt_cx = gt_bb[0] + (gt_bb[2] + 1.0) / 2
+                    gt_cy = gt_bb[1] + (gt_bb[3] + 1.0) / 2
 
-                # Metrics: Intersection over Union, Precision, Normalized Precision
-                # Width and height of overlap
-                dx = max(0, min(new_bb_xmax, gt_xmax) - max(new_bb_xmin, gt_xmin))
-                dy = max(0, min(new_bb_ymax, gt_ymax) - max(new_bb_ymin, gt_ymin))
-                area_of_overlap = dx * dy
-                area_of_union = (new_bb_xmax - new_bb_xmin) * (new_bb_ymax - new_bb_ymin) + (
-                    gt_xmax - gt_xmin) * (gt_ymax - gt_ymin) - area_of_overlap
-                if (area_of_union != 0):
+                    # Metrics: Intersection over Union, Precision, 
+                    # Normalized Precision
+
+                    # Width and height of overlap
+                    dx = max(0, min(new_bb_xmax, gt_xmax) - max(
+                        new_bb_xmin, gt_xmin))
+                    dy = max(0, min(new_bb_ymax, gt_ymax) - max(
+                        new_bb_ymin, gt_ymin))
+
                     # Intersection over Union
-                    intersection_over_union = area_of_overlap / area_of_union
-                    sum_iou += intersection_over_union
+                    area_of_overlap = dx * dy
+                    area_of_union = (new_bb_xmax - new_bb_xmin) * (
+                        new_bb_ymax - new_bb_ymin) + (gt_xmax - gt_xmin) * (
+                            gt_ymax - gt_ymin) - area_of_overlap
+                    # Zero division check
+                    if area_of_union != 0:
+                        iou = area_of_overlap / area_of_union
+                        sum_iou += iou
 
-                precision = np.sqrt((new_cx - gt_cx) * (new_cx - gt_cx) + (new_cy - gt_cy) * (new_cy - gt_cy))
-                if precision > 20.0:
-                    precision_value = 0.0
+                    # Precision
+                    precision = np.sqrt((new_cx - gt_cx) * (new_cx - gt_cx) + (
+                        new_cy - gt_cy) * (new_cy - gt_cy))
+                    if precision > 20.0:
+                        precision_value = 0.0
+                    else:
+                        precision_value = 1.0
+                    sum_pr += precision_value
+
+                    # Normalized precision
+                    # Zero division check
+                    if gt_bb[2] != 0 and gt_bb[3] != 0:
+                        normalized_precision = np.sqrt((new_cx - gt_cx) / \
+                            gt_bb[2] * (new_cx - gt_cx) / gt_bb[2] + \
+                                (new_cy - gt_cy) / gt_bb[3] * \
+                                    (new_cy - gt_cy) / gt_bb[3])
+                        if normalized_precision < 0.5:
+                            normalized_precision_value = 0.0
+                        else:
+                            normalized_precision_value = 1.0
+                        sum_norm_pr += normalized_precision_value
                 else:
-                    precision_value = 1.0
-                sum_pr += precision_value
+                    frame_counter -= 1
 
-                normalized_precision = np.sqrt((new_cx - gt_cx) / gt_bb[2] * (new_cx - gt_cx) / gt_bb[2] + (
-                    new_cy - gt_cy) / gt_bb[3] * (new_cy - gt_cy) / gt_bb[3])
-                if normalized_precision < 0.5:
-                    normalized_precision_value = 0.0
-                else:
-                    normalized_precision_value = 1.0
-                sum_norm_pr += normalized_precision_value
-
+                # Setting as ground truth bounding box from next frame
                 gt_bb = gt_file.readline().replace("\n", "").split(",")
 
+            # Evaluating mean value for specific video
             iou_values += sum_iou / frame_counter
             pr_values += sum_pr / frame_counter
             norm_pr_values += sum_norm_pr / frame_counter
 
-        mean_iou = iou_values / 280
-        mean_pr = pr_values / 280
-        mean_norm_pr = norm_pr_values / 280
+        # Evaluating mean value for dataset
+        mean_iou = iou_values / len(list_of_videos)
+        mean_pr = pr_values / len(list_of_videos)
+        mean_norm_pr = norm_pr_values / len(list_of_videos)
 
-        print(tracker_name + ":\n\tmean IoU = " + str(mean_iou) + "\n\tmean precision = " + str(mean_pr) + "\n\tmean normalized precision = " + str(mean_norm_pr) + "\n\n")
+        print(tracker_name + ":\n\tmean IoU = " + str(
+            mean_iou) + "\n\tmean precision = " + str(
+                mean_pr) + "\n\tmean normalized precision = " + str(
+                    mean_norm_pr) + "\n\n")
 
 if __name__ == '__main__':
     main()
