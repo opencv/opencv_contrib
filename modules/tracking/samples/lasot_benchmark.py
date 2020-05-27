852f9a461e505657431c19304bd1e8925d8c52dc
ilbalance <ilbalance476@gmail.com>
2020-05-27 16:21:40 +0300

    fixing comments

--- modules/tracking/samples/lasot_benchmark.py
+++ modules/tracking/samples/lasot_benchmark.py
@@ -17,6 +17,45 @@ def calc_coords(bb):
                    'cx': cx, 'cy': cy}
     return coords
 
+# During calculation of intersection over union we are checking numerical value
+# of area_of_overlap, because if it is equal to 0 - we have no intersection
+def get_iou(
+    new_xmin, new_xmax, new_ymin, new_ymax, gt_xmin, gt_xmax, gt_ymin, gt_ymax):
+    dx = max(0, min(new_xmax, gt_xmax) - max(
+        new_xmin, gt_xmin))
+    dy = max(0, min(new_ymax, gt_ymax) - max(
+        new_ymin, gt_ymin))
+    area_of_overlap = dx * dy
+    if area_of_overlap != 0:
+        area_of_union = (new_xmax - new_xmin) * (
+            new_ymax - new_ymin) + (gt_xmax - gt_xmin) * (
+                gt_ymax - gt_ymin) - area_of_overlap
+        iou = area_of_overlap / area_of_union
+    else:
+        iou = 0
+    return iou
+
+# In calculations of precision and normalized precision are used thresholds from
+# original TrackingNet paper: for metric calculation TrackingNet using lists of
+# thresholds, but here we use only one numerical value of the threshold
+# for each metric
+def get_pr(new_cx, new_cy, gt_cx, gt_cy):
+    precision = np.sqrt((new_cx - gt_cx) ** 2 + (new_cy - gt_cy) ** 2)
+    if precision > 20.0:
+        pr_value = 0.0
+    else:
+        pr_value = 1.0
+    return pr_value
+
+def get_norm_pr(new_cx, new_cy, gt_cx, gt_cy, gt_bb_w, gt_bb_h):
+    normalized_precision = np.sqrt(((new_cx - gt_cx) / gt_bb_w) ** 2 + (
+            (new_cy - gt_cy) / gt_bb_h) ** 2)
+    if normalized_precision > 0.1:
+        norm_pr_value = 0.0
+    else:
+        norm_pr_value = 1.0
+    return norm_pr_value
+
 # Method used for initializing of trackers by creating it 
 # via cv.TrackerX_create()
 def init_tracker(tracker_name):
@@ -41,19 +80,18 @@ def main():
     args = parser.parse_args()
 
     # Creating list with names of videos via reading names from txt file
-
-    video_names = os.path.join(args.path_to_dataset, "testing_set.txt")
-    with open(video_names, 'rt') as f:
-        list_of_videos = f.read().rstrip('\n').split('\n')
-
+    #video_names = os.path.join(args.path_to_dataset, "testing_set.txt")
+    #with open(video_names, 'rt') as f:
+    #    list_of_videos = f.read().rstrip('\n').split('\n')
+    list_of_videos = ['airplane-1']
+    #trackers = ['GOTURN']
     trackers = [
         'Boosting', 'MIL', 'KCF', 'MedianFlow', 'GOTURN', 'MOSSE', 'CSRT']
 
     # Loop for every tracker
-    for tracker_id in trackers:
+    for tracker_name in trackers:
 
-        tracker_name = tracker_id
-        print(tracker_name)
+        print("Tracker name: ", tracker_name)
 
         # Loop for every video
         for video_name in list_of_videos:
@@ -61,22 +99,19 @@ def main():
             tracker, frames_before_reinit = init_tracker(tracker_name)
             init_once = False
 
-            print("Video: " + str(video_name))
+            print("Video name: " + str(video_name))
 
             # Open specific video and read ground truth for it
             gt_file = open(os.path.join(args.path_to_dataset, video_name, "groundtruth.txt"), "r")
             gt_bb = gt_file.readline().replace("\n", "").split(",")
-            print(gt_bb)
             init_bb = gt_bb
             init_bb = tuple([float(b) for b in init_bb])
 
             print("Initial bounding box: ", init_bb)
 
             # Creating blob from image sequence
-            #video_sequence = sorted(os.listdir(os.path.join(args.path_to_dataset, video_name, "img")))
-            video_sequence = sorted(glob.glob(str("D:/lasot/" + str(video_name) +
-                                                  "/img/*.jpg")))
-            #print(video_sequence)
+            video_sequence = sorted(glob.glob(str("D:/lasot/" + str(video_name) +"/img/*.jpg")))
+
             print("Number of frames in video: " + str(len(video_sequence)))
 
             # Variables for saving sum of every metric for every frame and
@@ -91,11 +126,11 @@ def main():
 
             # For every frame in video
             for number_of_the_frame, image in enumerate(video_sequence):
-
+                frame = cv.imread(image)
                 for i in range(len(gt_bb)):
                     gt_bb[i] = float(gt_bb[i])
                 gt_bb = tuple(gt_bb)
-                frame = cv.imread(image)
+                #frame = cv.imread(image)
 
                 # Condition of tracker`s re-initialization
                 if ((number_of_the_frame + 1) % frames_before_reinit == 0) and (
@@ -128,45 +163,12 @@ def main():
                             int(new_xmax), int(new_ymax)), (200, 0, 0))
                         cv.imshow("Tracking", frame)
                         cv.waitKey(1)
-                    # Metrics: Intersection over Union, Precision, 
-                    # Normalized Precision
 
-                    # Width and height of overlap
-                    dx = max(0, min(new_xmax, gt_xmax) - max(
-                        new_xmin, gt_xmin))
-                    dy = max(0, min(new_ymax, gt_ymax) - max(
-                        new_ymin, gt_ymin))
-
-                    # Intersection over Union
-                    area_of_overlap = dx * dy
-                    area_of_union = (new_xmax - new_xmin) * (
-                        new_ymax - new_ymin) + (gt_xmax - gt_xmin) * (
-                            gt_ymax - gt_ymin) - area_of_overlap
-                    # Zero division check
-                    if area_of_union != 0:
-                        iou = area_of_overlap / area_of_union
-                        sum_iou += iou
-
-                    # Precision
-                    precision = np.sqrt((new_cx - gt_cx) ** 2 + (
-                        new_cy - gt_cy) ** 2)
-                    if precision > 20.0:
-                        pr_value = 0.0
-                    else:
-                        pr_value = 1.0
-                    sum_pr += pr_value
-
-                    # Normalized precision
-                    # Zero division check
-                    if gt_bb[2] != 0 and gt_bb[3] != 0:
-                        normalized_precision = np.sqrt(
-                            ((new_cx - gt_cx) / gt_bb[2]) ** 2 + (
-                                (new_cy - gt_cy) / gt_bb[3]) ** 2)
-                        if normalized_precision > 0.1:
-                            norm_pr_value = 0.0
-                        else:
-                            norm_pr_value = 1.0
-                        sum_norm_pr += norm_pr_value
+                    sum_iou += get_iou(new_xmin, new_xmax, new_ymin,
+                                       new_ymax, gt_xmin, gt_xmax, gt_ymin, gt_ymax)
+                    sum_pr += get_pr(new_cx, new_cy, gt_cx, gt_cy)
+                    sum_norm_pr += get_norm_pr(
+                        new_cx, new_cy, gt_cx, gt_cy, gt_bb[2], gt_bb[3])
                 else:
                     frame_counter -= 1
 
@@ -178,16 +180,10 @@ def main():
             pr_values += sum_pr / frame_counter
             norm_pr_values += sum_norm_pr / frame_counter
 
-        # Calculating mean arithmetic value for dataset
-        mean_iou = iou_values / len(list_of_videos)
-        mean_pr = pr_values / len(list_of_videos)
-        mean_norm_pr = norm_pr_values / len(list_of_videos)
-
-        print(tracker_name + ":")
-        print("\n\t" + str("Mean Intersection over Union = " + str(mean_iou)))
-        print("\n\t" + str("Mean Precision = ") + str(mean_pr))
-        print("\n\t" + str("Mean Normalized Precision = ") + str(mean_norm_pr))
-        print("\n\n")
+        print(tracker_name, ":")
+        print("Mean Intersection over Union = ", np.mean(iou_values))
+        print("Mean Precision = ", np.mean(pr_values))
+        print("Mean Normalized Precision = ", np.mean(norm_pr_values), "\n")
 
 if __name__ == '__main__':
     main()
