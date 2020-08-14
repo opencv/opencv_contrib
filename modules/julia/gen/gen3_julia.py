9afeff9be8dd85e5e64a871c2ab482d0726357eb
Archit Rungta <architrungta120@gmail.com>
2020-08-14 13:28:42 +0530

    Add copyright notice to .py files

--- modules/julia/gen/gen3_julia.py
+++ modules/julia/gen/gen3_julia.py
@@ -1,5 +1,11 @@
 #!/usr/bin/env python
 
+# This file is part of OpenCV project.
+# It is subject to the license terms in the LICENSE file found in the top-level directory
+# of this distribution and at http://opencv.org/license.html
+# Copyright (C) 2020 by Archit Rungta
+
+
 from __future__ import unicode_literals # Needed for python2
 
 import hdr_parser, sys, re, os
