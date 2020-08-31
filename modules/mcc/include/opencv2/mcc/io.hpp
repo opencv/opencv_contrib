7a3d082d58ae215409e035b27052faf0a68b682c
Chenqi Shan <shanchenqi@huawei.com>
2020-08-31 03:46:31 +0000

    Add utils, io helpers, the operations for linearization and distance

---
+++ modules/mcc/include/opencv2/mcc/io.hpp
@@ -0,0 +1,108 @@
+// This file is part of OpenCV project.
+// It is subject to the license terms in the LICENSE file found in the top-level directory
+// of this distribution and at http://opencv.org/license.html.
+
+/*
+ * MIT License
+ *
+ * Copyright (c) 2018 Pedro Diamel Marrero Fernández
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+#ifndef __OPENCV_MCC_IO_HPP__
+#define __OPENCV_MCC_IO_HPP__
+
+#include <string>
+#include <iostream>
+#include <map>
+#include <opencv2/core.hpp>
+
+namespace cv 
+{
+namespace ccm 
+{
+/* *\ brief Io is the meaning of illuminant and observer. See notes of ccm.hpp
+ *          for supported list for illuminant and observer*/
+class IO
+{
+public:
+   
+    std::string illuminant;
+    std::string observer;
+
+    IO() {};
+
+    IO(std::string illuminant, std::string observer) :illuminant(illuminant), observer(observer) {}; 
+
+    virtual ~IO() {};
+
+    bool operator<(const IO& other) const 
+    { 
+        return (illuminant < other.illuminant || ((illuminant == other.illuminant) && (observer < other.observer))); 
+    }
+
+    bool operator==(const IO& other) const
+    {
+        return illuminant == other.illuminant && observer == other.observer;
+    };
+};
+
+const IO A_2("A", "2"), A_10("A", "10"),
+    D50_2("D50", "2"), D50_10("D50", "10"),
+    D55_2("D55", "2"), D55_10("D55", "10"),
+    D65_2("D65", "2"), D65_10("D65", "10"),
+    D75_2("D75", "2"), D75_10("D75", "10"),
+    E_2("E", "2"), E_10("E", "10");
+
+// data from https://en.wikipedia.org/wiki/Standard_illuminant.
+const static std::map<IO, std::vector<double>> illuminants_xy = 
+{
+    {A_2, { 0.44757, 0.40745 }}, {A_10, { 0.45117, 0.40594 }},
+    {D50_2, { 0.34567, 0.35850 }}, {D50_10, { 0.34773, 0.35952 }},
+    {D55_2, { 0.33242, 0.34743 }}, {D55_10, { 0.33411, 0.34877 }},
+    {D65_2, { 0.31271, 0.32902 }}, {D65_10, { 0.31382, 0.33100 }},
+    {D75_2, { 0.29902, 0.31485 }}, {D75_10, { 0.45117, 0.40594 }},
+    {E_2, { 1 / 3, 1 / 3 }}, {E_10, { 1 / 3, 1 / 3 }},
+};
+
+std::vector<double> xyY2XYZ(const std::vector<double>& xyY) 
+{
+    double Y = xyY.size() >= 3 ? xyY[2] : 1;
+    return { Y * xyY[0] / xyY[1], Y, Y / xyY[1] * (1 - xyY[0] - xyY[1]) };
+}
+
+/* *\ brief function to get illuminants*/
+static std::map <IO, std::vector<double>> getIlluminant() 
+{
+    std::map <IO, std::vector<double>>  illuminants;
+    for (auto it = illuminants_xy.begin(); it != illuminants_xy.end(); ++it)
+    {
+        illuminants[it->first] = xyY2XYZ(it->second);
+    }
+    return illuminants;
+}
+
+const std::map<IO, std::vector<double> >  illuminants = getIlluminant(); 
+
+} // namespace ccm
+} // namespace cv
+
+
+#endif
---
+++ modules/mcc/include/opencv2/mcc/operations.hpp
@@ -0,0 +1,142 @@
+// This file is part of OpenCV project.
+// It is subject to the license terms in the LICENSE file found in the top-level directory
+// of this distribution and at http://opencv.org/license.html.
+
+/*
+ * MIT License
+ *
+ * Copyright (c) 2018 Pedro Diamel Marrero Fernández
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+#ifndef __OPENCV_MCC_OPERATIONS_HPP__
+#define __OPENCV_MCC_OPERATIONS_HPP__
+
+#include <functional>
+#include <vector>
+#include "opencv2/mcc/utils.hpp"
+
+namespace cv 
+{
+namespace ccm 
+{
+
+typedef std::function<cv::Mat(cv::Mat)> MatFunc;
+
+/* *\ brief Operation class contains some operarions used for color space
+   *        conversion containing linear transformation and non-linear transformation
+ */
+class Operation 
+{
+public:
+    bool linear;
+    cv::Mat M;
+    MatFunc f;
+
+    Operation() : linear(true), M(cv::Mat()) {};
+
+    Operation(cv::Mat M) :linear(true), M( M ) {};
+
+    Operation(MatFunc f) : linear(false), f(f) {};
+
+    virtual ~Operation() {};
+    /* *\ brief operator function will run operation*/
+    cv::Mat operator()(cv::Mat& abc) 
+    {
+        if (!linear) 
+        { 
+            return f(abc); 
+        }
+        if (M.empty()) 
+        { 
+            return abc; 
+        }
+        return multiple(abc, M);
+    };
+
+    /* *\ brief add function will conbine this operation 
+     *          with  other  linear transformation operation*/
+    void add(const Operation& other) 
+    {
+        if (M.empty()) 
+        {
+            M = other.M.clone();
+        }
+        else 
+        {
+            M = M * other.M;
+        }
+    };
+
+    void clear() 
+    {
+        M = cv::Mat();
+    };
+};
+
+const Operation IDENTITY_OP( [](cv::Mat x) {return x; } );
+
+class Operations 
+{
+public:
+    std::vector<Operation> ops;
+
+    Operations() :ops{ } {};
+
+    Operations(std::initializer_list<Operation> op) :ops{ op } {};
+
+    virtual ~Operations() {};
+
+    /* *\ brief add function will conbine this operation with  other  transformation operations*/
+    Operations& add(const Operations& other) 
+    {
+        ops.insert(ops.end(), other.ops.begin(), other.ops.end());
+        return *this;
+    };
+
+    /* *\ brief run operations to make color conversion*/
+    cv::Mat run(cv::Mat abc) 
+    {
+        Operation hd;
+        for (auto& op : ops) 
+        {
+            if (op.linear) 
+            {
+                hd.add(op);
+            }
+            else 
+            {
+                abc = hd(abc);
+                hd.clear();
+                abc = op(abc);
+            }
+        }
+        abc = hd(abc);
+        return abc;
+    };
+};
+
+const Operations IDENTITY_OPS{ IDENTITY_OP };
+
+} // namespace ccm
+} // namespace cv
+
+
+#endif
