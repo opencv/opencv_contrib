fb73bcc630ca94e997b4220774358ab8043ba1dd
steffen.ehrle <steffen.ehrle@myestro.de>
2020-10-15 18:19:39 +0200

    fix test using virtual data.

--- modules/tbmr/test/test_tbmr.cpp
+++ modules/tbmr/test/test_tbmr.cpp
@@ -49,7 +49,6 @@ direct,
 
 #include "cvconfig.h"
 #include "test_precomp.hpp"
-//#include "opencv2/ts/ocl_test.hpp"
 
 namespace opencv_test
 {
@@ -70,29 +69,44 @@ CV_TBMRTest::~CV_TBMRTest() {}
 void CV_TBMRTest::run(int)
 {
     using namespace tbmr;
-    Mat image;
-    image = imread(ts->get_data_path() +
-                       "../cv/stereomatching/datasets/tsukuba/im2.png",
-                   IMREAD_GRAYSCALE);
 
-    if (image.empty())
-    {
-        ts->printf(cvtest::TS::LOG, "Wrong input data \n");
-        ts->set_failed_test_info(cvtest::TS::FAIL_INVALID_TEST_DATA);
-        return;
-    }
+    Mat image(100, 100, CV_8UC1);
+    image.setTo(Scalar(0));
+    Point2f feature(50, 50);
+    double feature_angle = 5 * CV_PI / 180.;
+
+    ellipse(image, feature, Size2f(40, 30), feature_angle * 180. / CV_PI, 0,
+            360, Scalar(255), -1);
+    ellipse(image, Point2f(40, 50), Size2f(10, 5), 0, 0, 360, Scalar(128), -1);
+    ellipse(image, Point2f(70, 50), Size2f(10, 5), 0, 0, 360, Scalar(98), -1);
 
     Ptr<TBMR> tbmr = TBMR::create(30, 0.01);
+
     // set the corresponding parameters
     tbmr->setMinArea(30);
-    tbmr->setMaxAreaRelative(0.01);
+
+    // For normal images we want to set maxAreaRelative to around 0.01. Here we
+    // have a feature bigger than half of the image.
+    tbmr->setMaxAreaRelative(0.9);
 
     std::vector<KeyPoint> tbmrs;
     tbmr->detect(image, tbmrs);
 
-    if (tbmrs.size() != 351)
+    if (tbmrs.size() != 1)
+
     {
         ts->printf(cvtest::TS::LOG, "Invalid result \n");
+        ts->set_failed_test_info(cvtest::TS::FAIL_INVALID_OUTPUT);
+        return;
+    }
+
+    double feature_diff = cv::norm(tbmrs.at(0).pt - feature);
+    double angle_diff = (tbmrs.at(0).angle - feature_angle);
+
+    if (feature_diff > 0.1 || angle_diff > 0.01)
+
+    {
+        ts->printf(cvtest::TS::LOG, "Incorrect result \n");
         ts->set_failed_test_info(cvtest::TS::FAIL_INVALID_OUTPUT);
         return;
     }
@@ -103,18 +117,5 @@ TEST(tbmr_simple_test, accuracy)
     CV_TBMRTest test;
     test.safe_run();
 }
-#ifdef HAVE_OPENCL
-
-namespace ocl
-{
-
-// OCL_TEST_F(cv::tbmr::TBMR, ABC1)
-//{
-//    // RunTest<cv::UMat>(cv::superres::createSuperResolution_BTVL1());
-//}
-
-} // namespace ocl
-
-#endif
 
 } // namespace opencv_test
