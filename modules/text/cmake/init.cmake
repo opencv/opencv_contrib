c90c4f3dfd90ad8b905f462babaf9ace70566651
tangym <tangym@shanghaitech.edu.cn>
2020-10-15 19:00:25 +0800

    remove unnecessary code

--- modules/text/cmake/init.cmake
+++ modules/text/cmake/init.cmake
@@ -1,9 +1,9 @@
 OCV_OPTION(WITH_TESSERACT "Include Tesseract OCR library support" (NOT CMAKE_CROSSCOMPILING)
-        VERIFY HAVE_TESSERACT)
+  VERIFY HAVE_TESSERACT)
 
 if(NOT HAVE_TESSERACT
-        AND (WITH_TESSERACT OR OPENCV_FIND_TESSERACT)
-        )
+    AND (WITH_TESSERACT OR OPENCV_FIND_TESSERACT)
+)
   if(NOT Tesseract_FOUND)
     find_package(Tesseract QUIET)  # Prefer CMake's standard locations (including Tesseract_DIR)
   endif()
@@ -18,12 +18,12 @@ if(NOT HAVE_TESSERACT
     endif()
     if(NOT ENABLE_CXX11 AND NOT OPENCV_SKIP_TESSERACT_BUILD_CHECK)
       try_compile(__VALID_TESSERACT
-              "${OpenCV_BINARY_DIR}/cmake_check/tesseract"
-              "${CMAKE_CURRENT_LIST_DIR}/checks/tesseract_test.cpp"
-              CMAKE_FLAGS "-DINCLUDE_DIRECTORIES:STRING=${Tesseract_INCLUDE_DIRS}"
-              LINK_LIBRARIES ${Tesseract_LIBRARIES}
-              OUTPUT_VARIABLE TRY_OUT
-              )
+        "${OpenCV_BINARY_DIR}/cmake_check/tesseract"
+        "${CMAKE_CURRENT_LIST_DIR}/checks/tesseract_test.cpp"
+        CMAKE_FLAGS "-DINCLUDE_DIRECTORIES:STRING=${Tesseract_INCLUDE_DIRS}"
+        LINK_LIBRARIES ${Tesseract_LIBRARIES}
+        OUTPUT_VARIABLE TRY_OUT
+        )
       if(NOT __VALID_TESSERACT)
         if(OPENCV_DEBUG_TESSERACT_BUILD)
           message(STATUS "${TRY_OUT}")
@@ -41,14 +41,14 @@ if(NOT HAVE_TESSERACT
     set(link "${Tesseract_LIBRARIES}")
     set(def "")
     if(BUILD_SHARED_LIBS)
-      set(imp IMPORTED ../../rgbd/include/opencv2/rgbd/pcseg.hpp ../../rgbd/src/pcseg.cpp)
+      set(imp IMPORTED)
     endif()
-    add_library(ocv.3rdparty.${name} INTERFACE)
+    add_library(ocv.3rdparty.${name} INTERFACE ${imp})
     set_target_properties(ocv.3rdparty.${name} PROPERTIES
-            INTERFACE_INCLUDE_DIRECTORIES "${inc}"
-            INTERFACE_SYSTEM_INCLUDE_DIRECTORIES "${inc}"
-            INTERFACE_LINK_LIBRARIES "${link}"
-            INTERFACE_COMPILE_DEFINITIONS "${def}")
+      INTERFACE_INCLUDE_DIRECTORIES "${inc}"
+      INTERFACE_SYSTEM_INCLUDE_DIRECTORIES "${inc}"
+      INTERFACE_LINK_LIBRARIES "${link}"
+      INTERFACE_COMPILE_DEFINITIONS "${def}")
     if(NOT BUILD_SHARED_LIBS)
       install(TARGETS ocv.3rdparty.${name} EXPORT OpenCVModules)
     endif()
