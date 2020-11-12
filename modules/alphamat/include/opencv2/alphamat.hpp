9123701b6e8c50f47a5c4b4d5ab0fbaa50a52dbc
sunitanyk <nayak.sunita@gmail.com>
2020-11-12 14:46:09 -0800

    Included @cite

--- modules/alphamat/include/opencv2/alphamat.hpp
+++ modules/alphamat/include/opencv2/alphamat.hpp
@@ -13,7 +13,7 @@
  *
  * This module is dedicated to computing alpha matte of objects in images from a given input image and a greyscale trimap image that contains information about the foreground, background and unknown pixels. The unknown pixels are assumed to be a combination of foreground and background pixels. The algorithm uses a combination of multiple carefully defined pixels affinities to estimate the opacity of the foreground pixels in the unkown region.
  *
- * The implementation is based on "Designing Effective Inter-Pixel Information Flow for Natural Image Matting" by Yağız Aksoy, Tunç Ozan Aydın and Marc Pollefeys, CVPR 2017.
+ * The implementation is based on @cite aksoy2017designing.
  *
  * This module was developed by Muskaan Kularia and Sunita Nayak as a project
  * for Google Summer of Code 2019 (GSoC 19).
