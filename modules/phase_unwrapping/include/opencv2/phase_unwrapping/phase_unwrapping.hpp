1d84f2820243564bb376f22c6a1ff8640027a0fa
unknown <admin@.(none)>
2020-11-17 10:58:58 +0100

    phase_unwrapping: add asserts, documentation for input types

--- modules/phase_unwrapping/include/opencv2/phase_unwrapping/phase_unwrapping.hpp
+++ modules/phase_unwrapping/include/opencv2/phase_unwrapping/phase_unwrapping.hpp
@@ -58,9 +58,9 @@ public:
     /**
      * @brief Unwraps a 2D phase map.
 
-     * @param wrappedPhaseMap The wrapped phase map that needs to be unwrapped.
+     * @param wrappedPhaseMap The wrapped phase map of type CV_32FC1 that needs to be unwrapped.
      * @param unwrappedPhaseMap The unwrapped phase map.
-     * @param shadowMask Optional parameter used when some pixels do not hold any phase information in the wrapped phase map.
+     * @param shadowMask Optional CV_8UC1 mask image used when some pixels do not hold any phase information in the wrapped phase map.
      */
     CV_WRAP
     virtual void unwrapPhaseMap( InputArray wrappedPhaseMap, OutputArray unwrappedPhaseMap,
