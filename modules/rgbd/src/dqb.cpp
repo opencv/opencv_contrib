c867bf44df4bce0bda3650f84e83b89da8049557
Rostislav Vasilikhin <rostislav.vasilikhin@intel.com>
2020-12-08 01:16:07 +0300

    to/from rot matrix refactoring

--- modules/rgbd/src/dqb.cpp
+++ modules/rgbd/src/dqb.cpp
@@ -5,67 +5,52 @@ namespace dynafu {
 
 Vec4f rotMat2quat(Matx33f m)
 {
-    // Compute trace of matrix
     float tr = trace(m);
-
-    float S, X, Y, Z, W;
-
+    float w, x, y, z;
     const float TRACE_THRESHOLD = 0.00000001f;
-    if (tr > TRACE_THRESHOLD) // to avoid large distortions!
+    if (tr > TRACE_THRESHOLD)
     {
-        S = sqrt(tr) * 2.f;
-        X = (m(1, 2) - m(2, 1)) / S;
-        Y = (m(2, 0) - m(0, 2)) / S;
-        Z = (m(0, 1) - m(1, 0)) / S;
-        W = 0.25f * S;
+        float s = sqrt(tr + 1.0f) * 2.f;
+        w = 0.25f * s;
+        x = (m(2, 1) - m(1, 2)) / s;
+        y = (m(0, 2) - m(2, 0)) / s;
+        z = (m(1, 0) - m(0, 1)) / s;
+    }
+    else if ((m(0, 0) > m(1, 1)) && (m(0, 0) > m(2, 2)))
+    {
+        float s = sqrt(1.0f + m(0, 0) - m(1, 1) - m(2, 2)) * 2.f;
+        w = (m(2, 1) - m(1, 2)) / s;
+        x = 0.25f * s;
+        y = (m(0, 1) + m(1, 0)) / s;
+        z = (m(0, 2) + m(2, 0)) / s;
+    }
+    else if (m(1, 1) > m(2, 2))
+    {
+        float s = sqrt(1.0f + m(1, 1) - m(0, 0) - m(2, 2)) * 2.f;
+        w = (m(0, 2) - m(2, 0)) / s;
+        x = (m(0, 1) + m(1, 0)) / s;
+        y = 0.25f * s;
+        z = (m(1, 2) + m(2, 1)) / s;
     }
     else
     {
-        if (m(0, 0) > m(1, 1) && m(0, 0) > m(2, 2))
-        {
-            // Column 0 :
-            S = sqrt(1.0f + m(0, 0) - m(1, 1) - m(2, 2)) * 2.f;
-            X = 0.25f * S;
-            Y = (m(1, 0) + m(0, 1)) / S;
-            Z = (m(0, 2) + m(2, 0)) / S;
-            W = (m(2, 1) - m(1, 2)) / S;
-        }
-        else if (m(1, 1) > m(2, 2))
-        {
-            // Column 1 :
-            S = sqrt(1.0f + m(1, 1) - m(0, 0) - m(2, 2)) * 2.f;
-            X = (m(1, 0) + m(0, 1)) / S;
-            Y = 0.25f * S;
-            Z = (m(2, 1) + m(1, 2)) / S;
-            W = (m(0, 2) - m(2, 0)) / S;
-        }
-        else
-        {   // Column 2 :
-            S = sqrt(1.0f + m(2, 2) - m(0, 0) - m(1, 1)) * 2.f;
-            X = (m(0, 2) + m(2, 0)) / S;
-            Y = (m(2, 1) + m(1, 2)) / S;
-            Z = 0.25f * S;
-            W = (m(1, 0) - m(0, 1)) / S;
-        }
+        float s = sqrt(1.0f + m(2, 2) - m(0, 0) - m(1, 1)) * 2.f;
+        w = (m(1, 0) - m(0, 1)) / s;
+        x = (m(0, 2) + m(2, 0)) / s;
+        y = (m(1, 2) + m(2, 1)) / s;
+        z = 0.25f * s;
     }
 
-    return Vec4f(W, -X, -Y, -Z);
+    return { w, x, y, z };
 }
 
 
 // convert unit quaternion to rotation matrix
 Matx33f quat2rotMat(Vec4f q)
 {
-    float w = q[0], x = -q[1], y = -q[2], z = -q[3];
-    float xx = x * x, xy = x * y, xz = x * z, xw = x * w;
-    float yy = y * y, yz = y * z, yw = y * w, zz = z * z;
-    float zw = z * w;
-
-    // rot = (ww-(ww+xx+yy+zz))*I_3 + 2*(x, y, z)*(x, y, z)^T + 2*w*skewsym(x, y, z)
-
-    cv::Matx33f rot = cv::Matx33f::eye() + 2.f * cv::Matx33f(-yy - zz,  xy + zw,  xz - yw,
-                                                              xy - zw, -xx - zz,  yz + xw,
-                                                              xz + yw,  yz - xw, -xx - yy);
+    float w = q[0], x = q[1], y = q[2], z = q[3];
+    Vec3f rv(x, y, z);
+    cv::Matx33f rot = 2.f * ((w * w - 0.5f) * I3 + rv * rv.t() + w * skew(rv));
 
     return rot;
 }
