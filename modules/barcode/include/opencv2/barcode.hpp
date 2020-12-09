ad603e0fe5c557380e8a36d1ae2f24f365150f88
Certseeds <51754303+Certseeds@users.noreply.github.com>
2020-12-09 22:26:09 +0800

    Branch: Barcode-Support,replace vector<RotateRect> to vector<vector<Point2f>>, barcodeDirectly now just output one string, this commit is still only contain interface, barcode module will not be compile.
    Signed-off-by: Killer_Quinn <51754303+Certseeds@users.noreply.github.com>

--- modules/barcode/include/opencv2/barcode.hpp
+++ modules/barcode/include/opencv2/barcode.hpp
@@ -29,41 +29,45 @@ public:
     ~BarcodeDetector();
 
     /** @brief Detects Barcode in image and returns the rectangle(s) containing the code.
-
-    @param img grayscale or color (BGR) image containing (or not) Barcode.
-    @param rects Output vector of rotated rectangle(s) containing the barcode.
+     *
+     * @param img grayscale or color (BGR) image containing (or not) Barcode.
+     * @param rects Output vector of vector of vertices of the minimum-area rotated rectangle containing the codes.
+     * For N detected barcodes, the dimensions of this array should be [N][4].
+     * Order of four points in vector< Point2f> is bottomLeft, topLeft, topRight, bottomRight.
      */
-    CV_WRAP bool detect(InputArray img, CV_OUT std::vector <RotatedRect> &rects) const;
+    CV_WRAP bool detect(InputArray img, OutputArrayOfArrays rects) const;
 
     /** @brief Decodes barcode in image once it's found by the detect() method.
-
-    Returns UTF8-encoded output string(s) or empty string if the code cannot be decoded.
-    @param img grayscale or color (BGR) image containing barcode.
-    @param rects vector of rotated rectangle(s) found by detect() method (or some other algorithm).
-    @param decoded_info UTF8-encoded output vector of string(s) or empty vector of string if the codes cannot be decoded.
+     * Returns UTF8-encoded output string or empty string if the code cannot be decoded.
+     *
+     * @param img grayscale or color (BGR) image containing bar code.
+     * @param rects vector of rotated rectangle vertices found by detect() method (or some other algorithm).
+     * For N detected barcodes, the dimensions of this array should be [N][4].
+     * Order of four points in vector<Point2f> is bottomLeft, topLeft, topRight, bottomRight.
+     * @param decoded_info UTF8-encoded output vector of string or empty vector of string if the codes cannot be decoded.
      */
-    CV_WRAP bool decode(InputArray img, const std::vector <RotatedRect> &rects,
-                        CV_OUT std::vector <std::string> &decoded_info) const;
+    CV_WRAP bool decode(InputArray img, InputArrayOfArrays rects, CV_OUT std::vector <std::string> &decoded_info) const;
 
     /** @brief Both detects and decodes barcode
-
-    @param img grayscale or color (BGR) image containing QR code.
-    @param rects optional output vector of the found barcode rectangle(s). Will be empty if not found.
-    @param decoded_info UTF8-encoded output vector of string(s) or empty vector of string if the codes cannot be decoded.
+     *
+     * @param img grayscale or color (BGR) image containing barcode.
+     * @param decoded_info UTF8-encoded output vector of string(s) or empty vector of string if the codes cannot be decoded.
+     * @param rects optional output vector of vertices of the found  barcode rectangle. Will be empty if not found.
      */
     CV_WRAP bool detectAndDecode(InputArray img, CV_OUT std::vector <std::string> &decoded_info,
-                                 CV_OUT std::vector <RotatedRect> &rects) const;
+                                 OutputArrayOfArrays rects = noArray()) const;
 
     /** @brief Decode without detects
-
-    @param img grayscale or color (BGR) image containing barcode.
-    @param decoded_info UTF8-encoded output vector of string(s) or empty vector of string if the codes do not contain barcode.
+     *
+     * @param img grayscale or color (BGR) image containing barcode.
+     * @param decoded_info UTF8-encoded output of string or empty string if the codes do not contain barcode.
     */
-    CV_WRAP bool docodeDirectly(InputArray img, CV_OUT std::vector <std::string> &decoded_info) const;
+    CV_WRAP bool decodeDirectly(InputArray img, CV_OUT std::string &decoded_info) const;
 
 protected:
     struct Impl;
-    Ptr<Impl> p;
+    Ptr <Impl> p;
 };
-}} // cv::barcode::
+}
+} // cv::barcode::
 #endif //__OPENCV_BARCODE_HPP__
