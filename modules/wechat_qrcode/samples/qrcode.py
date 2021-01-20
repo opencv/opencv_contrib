06f690fc829630a491a544b0bff476289343b0df
dddzg <zhigangdai@hotmail.com>
2021-01-20 20:50:53 +0800

    remove licenses in samples

--- modules/wechat_qrcode/samples/qrcode.py
+++ modules/wechat_qrcode/samples/qrcode.py
@@ -1,10 +1,3 @@
-# This file is part of OpenCV project.
-# It is subject to the license terms in the LICENSE file found in the top-level directory
-# of this distribution and at http://opencv.org/license.html.
-#
-# Tencent is pleased to support the open source community by making WeChat QRCode available.
-# Copyright (C) 2020 THL A29 Limited, a Tencent company. All rights reserved.
-
 import cv2
 import sys
 
