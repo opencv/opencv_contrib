9cdb85331615cacf0830a701ff714d76f0f7a8e9
LaurentBerger <laurent.berger@univ-lemans.fr>
2021-01-21 10:10:48 +0100

    review

--- modules/stereo/samples/sample_quasi_dense.py
+++ modules/stereo/samples/sample_quasi_dense.py
@@ -1,20 +1,20 @@
 import numpy as np
 import cv2 as cv
 
-left_img = cv.imread("G:/Lib/opencv/samples/data/aloeL.jpg", cv.IMREAD_COLOR)
-right_img = cv.imread("G:/Lib/opencv/samples/data/aloeR.jpg", cv.IMREAD_COLOR)
+left_img = cv.imread(cv.samples.findFile("aloeL.jpg"), cv.IMREAD_COLOR)
+right_img = cv.imread(cv.samples.findFile("aloeR.jpg"), cv.IMREAD_COLOR)
 
 frame_size = leftImg.shape[0:2];
 
 stereo = cv.stereo.QuasiDenseStereo_create(frame_size[::-1])
-stereo.process(left_img,right_img)
+stereo.process(left_img, right_img)
 disp = stereo.getDisparity(80)
 cv.imshow("disparity", disp)
 cv.waitKey()
 dense_matches = stereo.getDenseMatches()
 try:
-    f = open("dense.txt", "wt")
-    with f:
+    with open("dense.txt", "wt") as f:
+        # if you want all matches use for idx in len(dense_matches): It can be a big file
         for idx in range(0, min(10, len(dense_matches))):
             nb = f.write(str(dense_matches[idx].p0) + "\t" + str(dense_matches[idx].p1) + "\t" + str(dense_matches[idx].corr) + "\n")
 except:
