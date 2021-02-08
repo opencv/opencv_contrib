5f8f4b5b86566872f130544169126ae4e514e8ca
Pavel Rojtberg <rojtberg@gmail.com>
2021-02-08 18:57:40 +0100

    ovis: do not leak camera when !SCENE_SEPARATE

--- modules/ovis/src/ovis.cpp
+++ modules/ovis/src/ovis.cpp
@@ -414,6 +414,11 @@ public:
             RTShader::ShaderGenerator::getSingleton().removeSceneManager(sceneMgr);
             root->destroySceneManager(sceneMgr);
         }
+        else
+        {
+            // we share everything, but the camera
+            sceneMgr->destroyCamera(title);
+        }
 
         if(_app->mainWin == this && (flags & SCENE_SEPARATE))
         {
