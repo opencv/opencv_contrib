14b43012da7f6e5cd98c3d6b56476445ee7a5f3f
Suleyman TURKMEN <sturkmen@hotmail.com>
2021-04-07 22:15:39 +0300

    update fast_line_detector.hpp

--- modules/ximgproc/include/opencv2/ximgproc/fast_line_detector.hpp
+++ modules/ximgproc/include/opencv2/ximgproc/fast_line_detector.hpp
@@ -33,48 +33,45 @@ public:
 
       ![image](pics/corridor_fld.jpg)
 
-      @param _image A grayscale (CV_8UC1) input image. If only a roi needs to be
+      @param image A grayscale (CV_8UC1) input image. If only a roi needs to be
       selected, use: `fld_ptr-\>detect(image(roi), lines, ...);
       lines += Scalar(roi.x, roi.y, roi.x, roi.y);`
-      @param _lines A vector of Vec4f elements specifying the beginning
+      @param lines A vector of Vec4f elements specifying the beginning
       and ending point of a line.  Where Vec4f is (x1, y1, x2, y2), point
       1 is the start, point 2 - end. Returned lines are directed so that the
       brighter side is on their left.
       */
-    CV_WRAP virtual void detect(InputArray _image, OutputArray _lines) = 0;
+    CV_WRAP virtual void detect(InputArray image, OutputArray lines) = 0;
 
     /** @brief Draws the line segments on a given image.
-      @param _image The image, where the lines will be drawn. Should be bigger
+      @param image The image, where the lines will be drawn. Should be bigger
       or equal to the image, where the lines were found.
       @param lines A vector of the lines that needed to be drawn.
       @param draw_arrow If true, arrow heads will be drawn.
-    */
-    CV_WRAP virtual void drawSegments(InputOutputArray _image, InputArray lines,
-            bool draw_arrow = false) = 0;
+      @param linecolor Line color.
+      @param linethickness Line thickness.
+      */
+    CV_WRAP virtual void drawSegments(InputOutputArray image, InputArray lines,
+            bool draw_arrow = false, Scalar linecolor = Scalar(0, 0, 255), int linethickness = 1) = 0;
 
     virtual ~FastLineDetector() { }
 };
 
 /** @brief Creates a smart pointer to a FastLineDetector object and initializes it
 
-@param _length_threshold    10         - Segment shorter than this will be discarded
-@param _distance_threshold  1.41421356 - A point placed from a hypothesis line
-                                         segment farther than this will be
-                                         regarded as an outlier
-@param _canny_th1           50         - First threshold for
-                                         hysteresis procedure in Canny()
-@param _canny_th2           50         - Second threshold for
-                                         hysteresis procedure in Canny()
-@param _canny_aperture_size 3          - Aperturesize for the sobel operator in Canny().
-                                         If zero, Canny() is not applied and the input
-                                         image is taken as an edge image.
-@param _do_merge            false      - If true, incremental merging of segments
-                                         will be performed
+@param length_threshold    Segment shorter than this will be discarded
+@param distance_threshold  A point placed from a hypothesis line
+                           segment farther than this will be regarded as an outlier
+@param canny_th1           First threshold for hysteresis procedure in Canny()
+@param canny_th2           Second threshold for hysteresis procedure in Canny()
+@param canny_aperture_size Aperturesize for the sobel operator in Canny().
+                           If zero, Canny() is not applied and the input image is taken as an edge image.
+@param do_merge            If true, incremental merging of segments will be performed
 */
 CV_EXPORTS_W Ptr<FastLineDetector> createFastLineDetector(
-        int _length_threshold = 10, float _distance_threshold = 1.414213562f,
-        double _canny_th1 = 50.0, double _canny_th2 = 50.0, int _canny_aperture_size = 3,
-        bool _do_merge = false);
+        int length_threshold = 10, float distance_threshold = 1.414213562f,
+        double canny_th1 = 50.0, double canny_th2 = 50.0, int canny_aperture_size = 3,
+        bool do_merge = false);
 
 //! @} ximgproc_fast_line_detector
 }
