2b9ecebdfd7e0edeccc2986306e8e35cced6a6bc
Rostislav Vasilikhin <rostislav.vasilikhin@intel.com>
2021-04-28 16:14:58 +0300

    include files removed

--- modules/rgbd/include/opencv2/rgbd.hpp
+++ modules/rgbd/include/opencv2/rgbd.hpp
@@ -9,14 +9,7 @@
 #ifndef __OPENCV_RGBD_HPP__
 #define __OPENCV_RGBD_HPP__
 
-#include "opencv2/rgbd/linemod.hpp"
-#include "opencv2/rgbd/depth.hpp"
-#include "opencv2/rgbd/kinfu.hpp"
 #include "opencv2/rgbd/dynafu.hpp"
-#include "opencv2/rgbd/large_kinfu.hpp"
-#include "opencv2/rgbd/detail/pose_graph.hpp"
-#include "opencv2/rgbd/colored_kinfu.hpp"
-
 
 /** @defgroup rgbd RGB-Depth Processing
 */
