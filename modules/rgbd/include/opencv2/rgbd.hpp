fa1b230446a06512d054de57d7f43d7b0a7fa610
arsaratovtsev <arsaratovtsev@intel.com>
2021-06-03 20:52:49 +0300

    fix with f

--- modules/rgbd/include/opencv2/rgbd.hpp
+++ modules/rgbd/include/opencv2/rgbd.hpp
@@ -26,21 +26,21 @@ The main equetion, which it needs to minimize:
 
 Let's watch what is ppd(p, q, n)
 Firstly, we have two clouds of points, old (the existing points and normals in 3-D model) and new (new points and normals, what we want to integrate to the exising model)
-p - \f[ i^{th} \f] point in the new cloud of points
-q - \f[ i^{th} \f] point in the old cloud of points
-n - \f[ i^{th} \f] normal in the old cloud of normals
+p - \f$ i^{th} \f$ point in the new cloud of points
+q - \f$ i^{th} \f$ point in the old cloud of points
+n - \f$ i^{th} \f$ normal in the old cloud of normals
 
-\f[ppd(...)\f] - is the distance \rightarrow its formula is the dot product of (difference between p and q) and (n):
-\f[dot(T_{p2q}(p)-q, n) = dot((R \cdot  p + t) - q, n) = [(R \cdot  p + t)- q]^{T}  \cdot n\f]
-\f[T_{p2q}(p)\f] - rigid transform of p point, which brings it closer to the corresponding q point.
-\f[T_{p2q}(p) = (R \cdot  p + t)\f], where R - rotation, t - translation.
+\f$ ppd(...) \f$ - is the distance \f$ \rightarrow \f$ its formula is the dot product of (difference between p and q) and (n):
+\f[ dot(T_{p2q}(p)-q, n) = dot((R \cdot  p + t) - q, n) = [(R \cdot  p + t)- q]^{T}  \cdot n \f]
+\f[ T_{p2q}(p) \f] - rigid transform of p point, which brings it closer to the corresponding q point.
+\f[ T_{p2q}(p) = (R \cdot  p + t) \f], where R - rotation, t - translation.
 
 We use the Gauss-Newton method for the minimization of function.
 In the beginning, we will perform some mathematical operations:
-\f[E = \sum \left \| [(R \cdot  p + t)- q]^{T}  \cdot n \right \|_{2}\f]
+\f[ E = \sum \left \| [(R \cdot  p + t)- q]^{T}  \cdot n \right \|_{2} \f]
 
 R is rotation and its formula is complicated:
-\f[R = R_{z}(\gamma)R_{y}(\beta )R_{x}(\alpha)=
+\f[ R = R_{z}(\gamma)R_{y}(\beta )R_{x}(\alpha)=
 \begin{bmatrix}
 cos(\gamma) & -sin(\gamma) & 0 \\
 sin(\gamma) & cos(\gamma) & 0\\
@@ -55,11 +55,11 @@ cos(\beta) & 0 & sin(\beta)\\
 1 & 0 & 0\\
 0 & cos(\alpha) & -sin(\alpha)\\
 0 & sin(\alpha) & cos(\alpha)
-\end{bmatrix}\f]
+\end{bmatrix} \f]
 But we have Infinitesimal rotations, and in that case we have another formula.
-\f[R = I + Ad\theta\f], where I - unit matrix, A - member of the three-dimensional special orthogonal group SO(3)
+\f[ R = I + Ad\theta \f], where I - unit matrix, A - member of the three-dimensional special orthogonal group SO(3)
 In this way:
-\f[R = I + \begin{bmatrix}
+\f[ R = I + \begin{bmatrix}
 0  & -\gamma  & \beta \\
 \gamma & 0 & -\alpha \\
 -\beta  & \alpha  & 0
@@ -71,31 +71,31 @@ I + skew(\begin{bmatrix}
 \gamma \\
 \end{bmatrix})
 =
-I + skew(R_{shift})\f]
+I + skew(R_{shift}) \f]
 
 Returns to the mathematical operations:
-\f[E = \sum \left \| [(I + skew(R_{shift})) \cdot  p + t- q]^{T}  \cdot n \right \|_{2}\f]
-\f[E = \sum \left \| [I \cdot  p + skew(R_{shift}) \cdot  p + t- q]^{T}  \cdot n \right \|_{2}\f]
-\f[E = \sum \left \| [skew(R_{shift}) \cdot  p + t + p- q]^{T}  \cdot n \right \|_{2}\f]
+\f[ E = \sum \left \| [(I + skew(R_{shift})) \cdot  p + t- q]^{T}  \cdot n \right \|_{2} \f]
+\f[ E = \sum \left \| [I \cdot  p + skew(R_{shift}) \cdot  p + t- q]^{T}  \cdot n \right \|_{2} \f]
+\f[ E = \sum \left \| [skew(R_{shift}) \cdot  p + t + p- q]^{T}  \cdot n \right \|_{2} \f]
 let a new function:
-\f[f(x, p) = skew(R_{shift}) \cdot  p + t\f]
-\f[E = \sum \left \| [f(x, p) + p- q]^{T}  \cdot n \right \|_{2}\f]
+\f[ f(x, p) = skew(R_{shift}) \cdot  p + t \f]
+\f[ E = \sum \left \| [f(x, p) + p- q]^{T}  \cdot n \right \|_{2} \f]
 
 Let's find out differential of E:
-\f[\frac{\partial E}{\partial x_{i}} = \sum [2 \cdot (f(x, p) + p - q)^{T} \cdot n] \cdot [f{}'(x, p)^{T} \cdot n] = 0\f]
-\f[\sum [2 \cdot n^{T} \cdot (f(x, p) + p - q)] \cdot [n^{T} \cdot f{}'(x, p)] = 0\f]
-Let new variable: \f[\triangle p = p - q\f]
-\f[\sum [2 \cdot n^{T} \cdot (f(x, p) + \triangle p)] \cdot [n^{T} \cdot f{}'(x, p)] = 0\f]
-\f[\sum [(f(x, p) + \triangle p)^{T} \cdot (n \cdot n^{T})] \cdot f{}'(x, p) = 0\f]
-\f[\sum f{}'(x, p)^{T} \cdot [n \cdot n^{T}] \cdot [f(x, p) + \triangle p] = 0\f]
+\f[ \frac{\partial E}{\partial x_{i}} = \sum [2 \cdot (f(x, p) + p - q)^{T} \cdot n] \cdot [f{}'(x, p)^{T} \cdot n] = 0 \f]
+\f[ \sum [2 \cdot n^{T} \cdot (f(x, p) + p - q)] \cdot [n^{T} \cdot f{}'(x, p)] = 0 \f]
+Let new variable: \f$ \triangle p = p - q \f$
+\f[ \sum [2 \cdot n^{T} \cdot (f(x, p) + \triangle p)] \cdot [n^{T} \cdot f{}'(x, p)] = 0 \f]
+\f[ \sum [(f(x, p) + \triangle p)^{T} \cdot (n \cdot n^{T})] \cdot f{}'(x, p) = 0 \f]
+\f[ \sum f{}'(x, p)^{T} \cdot [n \cdot n^{T}] \cdot [f(x, p) + \triangle p] = 0 \f]
 
 Let's find out differential of f(x):
-\f[d(f(x)) = d(skew(R_{shift}) \cdot  p + t) = d(skew(R_{shift})) \cdot  p  + skew(R_{shift}) \cdot  d(p))+ d(t)\f]
-\f[d(f(x)) = skew(\triangle R_{shift}) \cdot  p + \triangle t\f]
-let's remember: \f[cross(a, b) = skew(a) \cdot b = skew(b)^{T} \cdot a\f]
-\f[d(f(x)) = cross(\triangle R_{shift}, p) + \triangle t\f]
-\f[d(f(x)) = skew(p)^{T} \cdot \triangle R_{shift} + \triangle t\f]
-\f[d(f(x)) =
+\f[ d(f(x)) = d(skew(R_{shift}) \cdot  p + t) = d(skew(R_{shift})) \cdot  p  + skew(R_{shift}) \cdot  d(p))+ d(t) \f]
+\f[ d(f(x)) = skew(\triangle R_{shift}) \cdot  p + \triangle t \f]
+let's remember: \f$ cross(a, b) = skew(a) \cdot b = skew(b)^{T} \cdot a \f$
+\f[ d(f(x)) = cross(\triangle R_{shift}, p) + \triangle t \f]
+\f[ d(f(x)) = skew(p)^{T} \cdot \triangle R_{shift} + \triangle t \f]
+\f[ d(f(x)) =
 \begin{bmatrix}
  skew(p)^{T} & | & I
 \end{bmatrix}
@@ -104,23 +104,23 @@ let's remember: \f[cross(a, b) = skew(a) \cdot b = skew(b)^{T} \cdot a\f]
 \triangle R_{shift}\\
 \triangle t
 \end{bmatrix}
-= G(p) \cdot X\f]
+= G(p) \cdot X \f]
 We introduced G(p) function for simplification
 
-\f[\sum f{}'(x, p)^{T} \cdot [n \cdot n^{T}] \cdot [f(x, p)] = \sum f{}'(x, p)^{T} \cdot [n \cdot n^{T}] \cdot [- \triangle p]\f]
-\f[\sum G(p)^{T} \cdot [n \cdot n^{T}] \cdot [G(p) \cdot X] = \sum G(p)^{T} \cdot [n \cdot n^{T}] \cdot [- \triangle p]\f]
+\f[ \sum f{}'(x, p)^{T} \cdot [n \cdot n^{T}] \cdot [f(x, p)] = \sum f{}'(x, p)^{T} \cdot [n \cdot n^{T}] \cdot [- \triangle p] \f]
+\f[ \sum G(p)^{T} \cdot [n \cdot n^{T}] \cdot [G(p) \cdot X] = \sum G(p)^{T} \cdot [n \cdot n^{T}] \cdot [- \triangle p] \f]
 let a new value:
-\f[C = G(p)^{T} \cdot n\f]
-\f[C^{T} = (G(p)^{T} \cdot n)^{T} = n^{T} \cdot G(p)\f]
+\f[ C = G(p)^{T} \cdot n \f]
+\f[ C^{T} = (G(p)^{T} \cdot n)^{T} = n^{T} \cdot G(p) \f]
 
 Let's make a replacement:
-\f[\sum C \cdot C^{T} \cdot X = \sum C \cdot n^{T} \cdot [- \triangle p]\f]
-\f[\sum C \cdot C^{T} \cdot
+\f[ \sum C \cdot C^{T} \cdot X = \sum C \cdot n^{T} \cdot [- \triangle p] \f]
+\f[ \sum C \cdot C^{T} \cdot
 \begin{bmatrix}
 \triangle R_{shift}\\
 \triangle t
 \end{bmatrix}
-= \sum C \cdot n^{T} \cdot [- \triangle p]\f]
+= \sum C \cdot n^{T} \cdot [- \triangle p] \f]
 
 We solve this equation and as result, we have diff Rigid transform
 
