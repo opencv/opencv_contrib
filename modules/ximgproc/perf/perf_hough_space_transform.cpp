6d3a0904cf64e7c168d01f71ea57219652e96090
kuloPo <admin@kulopo.com>
2021-10-26 03:55:35 -0700

    add perf for hough_space_transform

---
+++ modules/ximgproc/perf/perf_hough_space_transform.cpp
@@ -0,0 +1,36 @@
+// This file is part of OpenCV project.
+// It is subject to the license terms in the LICENSE file found in the top-level directory
+// of this distribution and at http://opencv.org/license.html.
+
+#include "perf_precomp.hpp"
+
+namespace opencv_test {
+    namespace {
+
+        typedef tuple<Size, MatType> HoughSpaceTransformPerfTestParam;
+        typedef perf::TestBaseWithParam<HoughSpaceTransformPerfTestParam> HoughSpaceTransformPerfTest;
+
+        PERF_TEST_P(HoughSpaceTransformPerfTest, perf,
+            testing::Combine(
+                testing::Values(TYPICAL_MAT_SIZES),
+                testing::Values(CV_8U)
+            )
+        )
+        {
+            Size srcSize = get<0>(GetParam());
+            int  srcType = get<1>(GetParam());
+
+            Mat src(srcSize, srcType);
+            Mat hough;
+
+            declare.in(src, WARMUP_RNG);
+
+            TEST_CYCLE_N(3)
+            {
+                HoughSpaceTransform(src, hough);
+            }
+
+            SANITY_CHECK_NOTHING();
+        }
+    }
+} 
