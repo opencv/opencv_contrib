88789d9575fe3db85304aab9ba86432a3b276fce
クロポ <reg@kulopo.com>
2021-11-06 18:11:39 -0700

    update radon transform
    fix always cropping corner when rotate
    transpose the output - now each column is integral
    add support for float number
    add comment

--- modules/ximgproc/src/radon_transform.cpp
+++ modules/ximgproc/src/radon_transform.cpp
@@ -13,43 +13,69 @@ namespace cv {namespace ximgproc {
                              bool crop,
                              bool norm)
     {
-        CV_Assert(src.type() == CV_8U);
         CV_Assert(src.dims() == 2);
         CV_Assert(src.channels() == 1);
         CV_Assert((end_angle - start_angle) * theta > 0);
 
-        int _row_num = cvRound((end_angle - start_angle) / theta);
         Mat _srcMat = src.getMat();
+
+        int _row_num, _col_num, _out_mat_type;
+        _col_num = cvRound((end_angle - start_angle) / theta);
+        transpose(_srcMat, _srcMat);
         Mat _masked_src;
-        cv::Point _center(_srcMat.cols / 2, _srcMat.rows / 2);
+        cv::Point _center;
+
+        if (_srcMat.type() == CV_32FC1 || _srcMat.type() == CV_64FC1) {
+            _out_mat_type = CV_64FC1;
+        }
+        else {
+            _out_mat_type = CV_32SC1;
+        }
 
         if (crop) {
-            CV_Assert(src.rows() == src.cols());
-            Mat _mask(_srcMat.size(), CV_8U, Scalar(0));
+            // crop the source into square
+            _row_num = min(_srcMat.rows, _srcMat.cols);
+            cv::Rect _crop_ROI(
+                _srcMat.cols / 2 - _row_num / 2,
+                _srcMat.rows / 2 - _row_num / 2,
+                _row_num, _row_num);
+            _srcMat = _srcMat(_crop_ROI);
+            // crop the source into circle
+            Mat _mask(_srcMat.size(), CV_8UC1, Scalar(0));
+            _center = Point(_srcMat.cols / 2, _srcMat.rows / 2);
             circle(_mask, _center, _srcMat.cols / 2, Scalar(255), FILLED);
             _srcMat.copyTo(_masked_src, _mask);
         }
         else {
-            _masked_src = _srcMat;
+            // avoid cropping corner when rotating
+            _row_num = cvCeil(sqrt(_srcMat.rows * _srcMat.rows + _srcMat.cols * _srcMat.cols));
+            _masked_src = Mat(Size(_row_num, _row_num), _srcMat.type(), Scalar(0));
+            _center = Point(_masked_src.cols / 2, _masked_src.rows / 2);
+            _srcMat.copyTo(_masked_src(Rect(
+                (_row_num - _srcMat.cols) / 2,
+                (_row_num - _srcMat.rows) / 2,
+                _srcMat.cols, _srcMat.rows)));
         }
 
         double _t;
         Mat _rotated_src;
-        Mat _hough(_row_num, _masked_src.cols, CV_32SC1);
+        Mat _radon(_row_num, _col_num, _out_mat_type);
 
-        for (int _row = 0; _row < _row_num; _row++) {
-            _t = start_angle + _row * theta;
-            cv::Mat r = cv::getRotationMatrix2D(_center, _t, 1);
-            cv::warpAffine(_masked_src, _rotated_src, r, _masked_src.size());
-            Mat _row_mat = _hough.row(_row);
-            cv::reduce(_rotated_src, _row_mat, 0, REDUCE_SUM, CV_32SC1);
+        for (int _col = 0; _col < _col_num; _col++) {
+            // rotate the source by _t 
+            _t = (start_angle + _col * theta);
+            cv::Mat _r_matrix = cv::getRotationMatrix2D(_center, _t, 1);
+            cv::warpAffine(_masked_src, _rotated_src, _r_matrix, _masked_src.size());
+            Mat _col_mat = _radon.col(_col);
+            // make projection
+            cv::reduce(_rotated_src, _col_mat, 1, REDUCE_SUM, _out_mat_type);
         }
 
         if (norm) {
-            normalize(_hough, _hough, 0, 255, NORM_MINMAX, CV_8U);
+            normalize(_radon, _radon, 0, 255, NORM_MINMAX, CV_8UC1);
         }
 
-        _hough.copyTo(dst);
+        _radon.copyTo(dst);
         return;
     }
 } }
