35830e977a31b3399dcf027021bf356a979f06a3
ZhaoChuyang <zcy201420752@gmail.com>
2022-09-04 15:32:02 +0800

    add docs and aug methods for detection module

--- modules/imgaug/src/transforms_det.cpp
+++ modules/imgaug/src/transforms_det.cpp
@@ -3,19 +3,22 @@
 // of this distribution and at http://opencv.org/license.html.
 #include "precomp.hpp"
 #include <opencv2/highgui.hpp>
-
+#include <iostream>
 
 namespace cv{
     namespace imgaug{
         extern RNG rng;
 
         namespace det{
+            int clamp(int v, int lo, int hi);
+            void rotate(int* x, int* y, int cx, int cy, double angle);
+
             Compose::Compose(std::vector<Ptr<Transform> >& transforms):
                     transforms(transforms){};
 
-            void Compose::call(InputArray src, OutputArray dst, std::vector<cv::Rect>& target) const{
+            void Compose::call(InputArray src, OutputArray dst, std::vector<cv::Rect>& target, std::vector<int>& labels) const{
                 for(cv::imgaug::det::Transform* transform:transforms){
-                    transform->call(src, dst, target);
+                    transform->call(src, dst, target, labels);
                 }
             }
 
@@ -27,7 +30,7 @@ namespace cv{
                 }
             };
 
-            void RandomFlip::call(InputArray _src, OutputArray _dst, std::vector<cv::Rect>& target) const{
+            void RandomFlip::call(InputArray _src, OutputArray _dst, std::vector<cv::Rect>& target, std::vector<int>& labels) const{
 //            RNG rng = RNG(getTickCount());
                 bool flag = rng.uniform(0., 1.) < p;
 
@@ -66,7 +69,7 @@ namespace cv{
             Resize::Resize(const Size& size, int interpolation):
                     size(size), interpolation(interpolation){};
 
-            void Resize::call(InputArray _src, OutputArray dst, std::vector<cv::Rect>& target) const{
+            void Resize::call(InputArray _src, OutputArray dst, std::vector<cv::Rect>& target, std::vector<int>& labels) const{
                 Mat src = _src.getMat();
                 resize(src, dst, size, 0, 0, interpolation);
                 resizeBoundingBox(target, src.size());
@@ -84,8 +87,142 @@ namespace cv{
             Convert::Convert(int code):
                     code(code){};
 
-            void Convert::call(InputArray src, OutputArray dst, std::vector<cv::Rect>& target) const{
+
+            void Convert::call(InputArray src, OutputArray dst, std::vector<cv::Rect>& target, std::vector<int>& labels) const{
                 cvtColor(src, dst, code);
+            }
+
+            RandomTranslation::RandomTranslation(const cv::Vec2i &translations, float threshold):
+                translations(translations),
+                threshold(threshold){};
+
+
+            void RandomTranslation::call(cv::InputArray _src, cv::OutputArray _dst, std::vector<cv::Rect> &bboxes, std::vector<int>& labels) const {
+                int tx = rng.uniform(-translations[0], translations[0]);
+                int ty = rng.uniform(-translations[1], translations[1]);
+
+                Mat translation_matrix = Mat::eye(2, 3, CV_32F);
+                float* data = translation_matrix.ptr<float>();
+                data[0] = 1;
+                data[1] = 0;
+                data[2] = tx;
+                data[3] = 0;
+                data[4] = 1;
+                data[5] = ty;
+
+                cv::warpAffine(_src, _dst, translation_matrix, _src.size());
+                translateBoundingBox(bboxes, labels, _src.size(), tx, ty);
+            }
+
+
+            void RandomTranslation::translateBoundingBox(std::vector<cv::Rect> &bboxes, std::vector<int> &labels, const cv::Size &imgSize, int tx, int ty) const {
+                for(int i=0; i < bboxes.size(); i++){
+                    int x1 = clamp(bboxes[i].x + tx, 0, imgSize.width);
+                    int y1 = clamp(bboxes[i].y + ty, 0, imgSize.height);
+                    int x2 = clamp(bboxes[i].x + bboxes[i].width + tx, 0, imgSize.width);
+                    int y2 = clamp(bboxes[i].y + bboxes[i].height + ty, 0, imgSize.height);
+                    int w = x2 - x1;
+                    int h = y2 - y1;
+                    if((float)(w * h) / (bboxes[i].width * bboxes[i].height) < threshold){
+                        bboxes.erase(bboxes.begin() + i);
+                        labels.erase(labels.begin() + i);
+                    }else{
+                        bboxes[i].x = x1;
+                        bboxes[i].y = y1;
+                        bboxes[i].width = x2 - x1;
+                        bboxes[i].height = y2 - y1;
+                    }
+                }
+            }
+
+            RandomRotation::RandomRotation(const cv::Vec2d &angles, double threshold):
+                angles(angles),
+                threshold(threshold){};
+
+            void RandomRotation::call(cv::InputArray _src, cv::OutputArray _dst, std::vector<cv::Rect> &bboxes,
+                                      std::vector<int> &labels) const {
+                Mat src = _src.getMat();
+                double angle = rng.uniform(angles[0], angles[1]);
+                Mat rotation_matrix = getRotationMatrix2D(cv::Point2f(src.cols/2., src.rows/2.), angle, 1);
+                warpAffine(src, _dst, rotation_matrix, src.size());
+
+//                cv::Point pt1{bboxes[0].x, bboxes[0].y};
+//                cv::Point pt2{bboxes[0].x + bboxes[0].width, bboxes[0].y + bboxes[0].height};
+//                cv::rectangle(src, pt1, pt2, cv::Scalar(), 2);
+//                cv::imshow("src", src);
+
+                Mat dst = _dst.getMat();
+                rotateBoundingBoxes(bboxes, labels, src.size(), angle, src.cols / 2, src.rows / 2, dst);
+
+            }
+
+            void RandomRotation::rotateBoundingBoxes(std::vector<cv::Rect> &bboxes, std::vector<int> &labels,
+                                                     const cv::Size &imgSize, double angle, int cx, int cy, Mat& img) const {
+                for(int i=0; i < bboxes.size(); i++){
+                    int x1 = bboxes[i].x;
+                    int y1 = bboxes[i].y;
+                    int x2 = bboxes[i].x + bboxes[i].width;
+                    int y2 = bboxes[i].y;
+                    int x3 = bboxes[i].x;
+                    int y3 = bboxes[i].y + bboxes[i].height;
+                    int x4 = bboxes[i].x + bboxes[i].width;
+                    int y4 = bboxes[i].y + bboxes[i].height;
+
+                    // convert unit from degree to radius
+                    angle = -angle * CV_PI / 180;
+
+                    // rotate the corners
+                    rotate(&x1, &y1, cx, cy, angle);
+                    rotate(&x2, &y2, cx, cy, angle);
+                    rotate(&x3, &y3, cx, cy, angle);
+                    rotate(&x4, &y4, cx, cy, angle);
+
+//                    cv::circle(img, Point2i(x1,y1), 3, Scalar(255, 255, 255), 1);
+//                    cv::circle(img, Point2i(x2,y2), 3, Scalar(255, 255, 255), 1);
+//                    cv::circle(img, Point2i(x3,y3), 3, Scalar(255, 255, 255), 1);
+//                    cv::circle(img, Point2i(x4,y4), 3, Scalar(255, 255, 255), 1);
+//                    cv::imshow("img", img);
+//                    cv::waitKey(0);
+
+                    // shrink the rotated corners to get an enclosing box
+                    int x_min = min({x1, x2, x3, x4});
+                    int y_min = min({y1, y2, y3, y4});
+                    int x_max = max({x1, x2, x3, x4});
+                    int y_max = max({y1, y2, y3, y4});
+
+                    int w = x_max - x_min;
+                    int h = y_max - y_min;
+
+                    if((float)(w * h) / (bboxes[i].width * bboxes[i].height) < threshold){
+                        bboxes.erase(bboxes.begin() + i);
+                        labels.erase(labels.begin() + i);
+                    }else{
+                        bboxes[i].x = x_min;
+                        bboxes[i].y = y_min;
+                        bboxes[i].width = w;
+                        bboxes[i].height = h;
+                    }
+
+                }
+            }
+
+            inline int clamp(int v, int lo, int hi){
+                if(v < lo){
+                    return lo;
+                }
+                if(v > hi){
+                    return hi;
+                }
+                return v;
+            }
+
+            inline void rotate(int* x, int* y, int cx, int cy, double angle){
+//                int a = round(((*x) - cx) * cos(angle) - ((*y) - cy) * sin(angle) + cx);
+//                int b = round(((*x) - cx) * sin(angle) + ((*y) - cy) * cos(angle) + cy);
+//                *x = a;
+//                *y = b;
+                (*x) = round(((*x) - cx) * cos(angle) - ((*y) - cy) * sin(angle) + cx);
+                (*y) = round(((*x) - cx) * sin(angle) + ((*y) - cy) * cos(angle) + cy);
             }
 
         }
