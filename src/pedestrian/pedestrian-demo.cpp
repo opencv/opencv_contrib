69369795f061d4fe105b67fad6032428e0adb775
kallaballa <amir@viel-zu.org>
2022-11-29 08:26:46 +0100

    refactoring, renaming and commenting

--- src/pedestrian/pedestrian-demo.cpp
+++ src/pedestrian/pedestrian-demo.cpp
@@ -16,7 +16,7 @@ constexpr bool OFFSCREEN = false;
 constexpr const int VA_HW_DEVICE_INDEX = 0;
 constexpr const char* OUTPUT_FILENAME = "pedestrian-demo.mkv";
 
-// On every frame the foreground loses on brightness. specifies the loss in percent.
+// On every frame the foreground loses on brightness. Specifies the loss in percent.
 constexpr float FG_LOSS = 3;
 // Intensity of blur defined by kernel size. The default scales with the image diagonal.
 constexpr int BLUR_KERNEL_SIZE = std::max(int(DIAG / 200 % 2 == 0 ? DIAG / 200 + 1 : DIAG / 200), 1);
@@ -27,10 +27,8 @@ using std::vector;
 using std::string;
 
 //adapted from cv::dnn_objdetect::InferBbox
-static inline bool pair_comparator (std::pair<double, size_t> l1,
-    std::pair<double, size_t> l2)
-{
-  return l1.first > l2.first;
+static inline bool pair_comparator(std::pair<double, size_t> l1, std::pair<double, size_t> l2) {
+    return l1.first > l2.first;
 }
 
 //adapted from cv::dnn_objdetect::InferBbox
@@ -61,7 +59,7 @@ void intersection_over_union(std::vector<std::vector<double> > *boxes, std::vect
 }
 
 //adapted from cv::dnn_objdetect::InferBbox
-std::vector<bool> non_maximal_suppression(std::vector<std::vector<double> > *boxes, std::vector<double> *probs) {
+std::vector<bool> non_maximal_suppression(std::vector<std::vector<double> > *boxes, std::vector<double> *probs, const double threshold = 0.1) {
     std::vector<bool> keep(((*probs).size()));
     std::fill(keep.begin(), keep.end(), true);
     std::vector<size_t> prob_args_sorted((*probs).size());
@@ -90,7 +88,7 @@ std::vector<bool> non_maximal_suppression(std::vector<std::vector<double> > *box
         intersection_over_union(&temp_boxes, &(*boxes)[prob_args_sorted[idx]], &iou_);
         for (std::vector<double>::iterator _itr = iou_.begin(); _itr != iou_.end(); ++_itr) {
             size_t iou_idx = _itr - iou_.begin();
-            if (*_itr > 0.1) {
+            if (*_itr > threshold) {
                 keep[prob_args_sorted[idx + iou_idx + 1]] = false;
             }
         }
@@ -112,7 +110,7 @@ int main(int argc, char **argv) {
     using namespace kb;
 
     if (argc != 2) {
-        std::cerr << "Usage: pedestrian-demo <input-video-file>" << endl;
+        std::cerr << "Usage: pedestrian-demo <video-input>" << endl;
         exit(1);
     }
 
@@ -127,7 +125,7 @@ int main(int argc, char **argv) {
     va::copy();
 
     if (!cap.isOpened()) {
-        cerr << "ERROR! Unable to open camera" << endl;
+        cerr << "ERROR! Unable to open video-input" << endl;
         return -1;
     }
 
@@ -170,7 +168,7 @@ int main(int argc, char **argv) {
         cv::resize(videoFrame, videoFrameUp, cv::Size(WIDTH, HEIGHT));
         cv::resize(videoFrame, videoFrameDown, cv::Size(DOWNSIZE_WIDTH, DOWNSIZE_HEIGHT));
         cv::cvtColor(videoFrameDown, videoFrameDownGrey, cv::COLOR_RGB2GRAY);
-        hog.detectMultiScale(videoFrameDownGrey, locations, 0, cv::Size(), cv::Size(), 1.05, 2.0, false);
+        hog.detectMultiScale(videoFrameDownGrey, locations, 0, cv::Size(), cv::Size(), 1.025, 2.0, false);
         maxLocations.clear();
         if (!locations.empty()) {
             boxes.clear();
@@ -180,15 +178,13 @@ int main(int argc, char **argv) {
                 probs.push_back(1.0);
             }
 
-            vector<bool> keep = non_maximal_suppression(&boxes, &probs);
+            vector<bool> keep = non_maximal_suppression(&boxes, &probs, 0.1);
 
             for (size_t i = 0; i < keep.size(); ++i) {
                 if (keep[i])
                     maxLocations.push_back(locations[i]);
             }
         }
-        if (videoFrameUp.empty())
-            break;
 
         cv::cvtColor(videoFrameUp, background, cv::COLOR_RGB2BGRA);
 
@@ -197,15 +193,12 @@ int main(int argc, char **argv) {
         nvg::clear();
         {
             using kb::nvg::vg;
-
             nvgBeginPath(vg);
             nvgStrokeWidth(vg, std::fmax(2.0, WIDTH / 960.0));
             nvgStrokeColor(vg, nvgHSLA(0.0, 1, 0.5, 200));
-
             for (size_t i = 0; i < maxLocations.size(); i++) {
                 nvgRect(vg, maxLocations[i].x * WIDTH_FACTOR, maxLocations[i].y * HEIGHT_FACTOR, maxLocations[i].width * WIDTH_FACTOR, maxLocations[i].height * HEIGHT_FACTOR);
             }
-
             nvgStroke(vg);
         }
         nvg::end();
