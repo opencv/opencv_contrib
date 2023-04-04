38fadcf08979bdb29710d0c95e5906b98abb73ba
rishikeshydv <rishikeshadh4@gmail.com>
2023-04-04 01:04:24 -0400

    #1935 Issue : Updated reg_shift.py

--- modules/reg/samples/reg_shift.py
+++ modules/reg/samples/reg_shift.py
@@ -7,12 +7,15 @@ import sys
 img1 = cv.imread(sys.argv[1])
 img1 = img1.astype(np.float32)
 shift = np.array([5., 5.])
-mapTest = cv.reg_MapShift(shift)
+
+#substituted dot (.) with an underscore (_)
+
+mapTest = cv.reg_MapShift(shift)        #line 1
 
 img2 = mapTest.warp(img1)
 
-mapper = cv.reg_MapperGradShift()
-mappPyr = cv.reg_MapperPyramid(mapper)
+mapper = cv.reg_MapperGradShift()       #line2
+mappPyr = cv.reg_MapperPyramid(mapper)          #line 3
 
 resMap = mappPyr.calculate(img1, img2)
 mapShift = cv.reg.MapTypeCaster_toShift(resMap)
