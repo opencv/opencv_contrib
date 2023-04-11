6dc392eedde1245468e308d70394d44c013e8fcf
kallaballa <amir@viel-zu.org>
2023-04-11 10:00:15 +0200

    adapted example to use sample data

--- modules/viz2d/samples/cpp/display_image.cpp
+++ modules/viz2d/samples/cpp/display_image.cpp
@@ -6,10 +6,11 @@ using namespace cv::viz;
 constexpr int WIDTH = 1280;
 constexpr int HEIGHT = 720;
 
-int main(int argc, char** argv) {
+int main() {
+	//Creates a Viz2D object for on screen rendering
 	Ptr<Viz2D> v2d = Viz2D::make(Size(WIDTH, HEIGHT), "Show image");
 	//An image
-	Mat image = imread(argv[1]);
+	Mat image = imread(samples::findFile("lena.jpg"));
 	//Feeds the image to the video pipeline
 	v2d->feed(image);
 	//Display the framebuffer in the native window
