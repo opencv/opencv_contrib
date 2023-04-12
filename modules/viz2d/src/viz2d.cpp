e4eea31d823506d320e1962ea98da608e3f8f858
kallaballa <amir@viel-zu.org>
2023-04-12 09:42:22 +0200

    enable OpenGL forward compat

--- modules/viz2d/src/viz2d.cpp
+++ modules/viz2d/src/viz2d.cpp
@@ -133,6 +133,7 @@ bool Viz2D::initializeWindowing() {
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, major_);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, minor_);
     glfwWindowHint(GLFW_CONTEXT_CREATION_API, GLFW_EGL_CONTEXT_API);
+    glfwWindowHint (GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
     glfwWindowHint(GLFW_CLIENT_API, GLFW_OPENGL_API) ;
 #endif
     glfwWindowHint(GLFW_SAMPLES, samples_);
