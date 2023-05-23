9bb9e1941e0b5ce48fc2ba1de526dc702565160e
Maksim Shabunin <maksim.shabunin@gmail.com>
2023-05-23 15:16:12 +0300

    fixup! add barcode perf tests

--- modules/barcode/perf/perf_precomp.hpp
+++ modules/barcode/perf/perf_precomp.hpp
@@ -2,7 +2,7 @@
 // It is subject to the license terms in the LICENSE file found in the top-level directory
 // of this distribution and at http://opencv.org/license.html.
 
-#ifndef __OPENCV_PERF_PRECOMP_HPP___
+#ifndef __OPENCV_PERF_PRECOMP_HPP__
 #define __OPENCV_PERF_PRECOMP_HPP__
 
 #include "opencv2/ts.hpp"
