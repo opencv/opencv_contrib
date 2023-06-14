cbeed89f352d8f546d7e62e8296cff7219f3c119
kallaballa <amir@viel-zu.org>
2023-06-14 14:58:55 +0200

    don't clear the main framebuffer in WASM builds

--- modules/v4d/src/v4d.cpp
+++ modules/v4d/src/v4d.cpp
@@ -557,11 +557,12 @@ bool V4D::display() {
 #ifndef __EMSCRIPTEN__
                 fbCtx().makeCurrent();
                 GL_CHECK(glBindFramebuffer(GL_DRAW_FRAMEBUFFER, 0));
-#else
-                FrameBufferContext::GLScope glScope(fbCtx(), GL_FRAMEBUFFER);
-#endif
+//#else
+//                FrameBufferContext::GLScope glScope(fbCtx(), GL_FRAMEBUFFER);
+//#endif
                 GL_CHECK(glClearColor(0, 0, 0, 1));
                 GL_CHECK(glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT | GL_STENCIL_BUFFER_BIT));
+#endif
             }
 
             glfwPollEvents();
