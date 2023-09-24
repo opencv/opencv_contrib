7fc418f8c00612c15de4e67798ddd5be80a31a4c
kallaballa <amir@viel-zu.com>
2023-09-24 23:28:06 +0200

    display_image_nvg: a new tutorial about displaying images using nanovg

---
+++ modules/v4d/samples/display_image_nvg.cpp
@@ -0,0 +1,36 @@
+#include <opencv2/v4d/v4d.hpp>
+#include <opencv2/imgcodecs.hpp>
+
+using namespace cv;
+using namespace cv::v4d;
+using namespace cv::v4d::nvg;
+
+int main() {
+    cv::Ptr<V4D> window = V4D::make(960, 960, "Display Image using NanoVG", false, false, 0);
+
+#ifdef __EMSCRIPTEN__
+    string filename = "doc/lena.png";
+#else
+    string filename = samples::findFile("lena.jpg");
+#endif
+	Paint imagePaint;
+    int w, h;
+	window->nvg([&imagePaint, &w, &h, filename](const cv::Size sz) {
+	    int img = createImage(filename.c_str(), NVG_IMAGE_NEAREST);
+	    CV_Assert(img > 0);
+	    imageSize(img, &w, &h);
+	    imagePaint = imagePattern(0, 0, w, h, 0.0f/180.0f*NVG_PI, img, 1.0);
+	});
+
+	window->run([&imagePaint](Ptr<V4D> win){
+	    win->nvg([&imagePaint](const cv::Size sz) {
+	        beginPath();
+	        scale(960.0/w, 960.0/h);
+	        roundedRect(0,0, w, h, 5);
+	        fillPaint(imagePaint);
+	        fill();
+	    });
+	    //Displays the framebuffer in the window. Returns false if the windows has been closed.
+	    return win->display();
+    });
+}
