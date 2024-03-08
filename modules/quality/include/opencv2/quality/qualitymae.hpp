9f86d750995125e48f18c93b0cd310347f164bb1
julien-richard-fleuret <julien.fleuret@gmail.com>
2024-03-08 14:01:32 -0500

    pre-commit check activated

---
+++ modules/quality/include/opencv2/quality/qualitymae.hpp
@@ -0,0 +1,76 @@
+#ifndef OPENCV_QUALITY_MAE_HPP
+#define OPENCV_QUALITY_MAE_HPP
+
+#include "qualitybase.hpp"
+
+
+namespace cv
+{
+
+namespace quality
+{
+
+/** @brief Flags to choose which algorithm MAE should use.
+ */
+enum MAEStatsFlags
+{
+    MAE_Max,
+    MAE_Mean
+};
+
+class CV_EXPORTS_W QualityMAE : public QualityBase
+{
+public:
+    /** @brief Computes MAE for reference images supplied in class constructor and provided comparison images
+    @param cmpImgs Comparison image(s)
+    @returns cv::Scalar with per-channel quality values.  Values range from 0 (best) to potentially max float (worst)
+    */
+    CV_WRAP Scalar compute( InputArray cmpImgs ) CV_OVERRIDE;
+
+    /** @brief Implements Algorithm::empty()  */
+    CV_WRAP bool empty() const CV_OVERRIDE { return _ref.empty() && QualityBase::empty(); }
+
+    /** @brief Implements Algorithm::clear()  */
+    CV_WRAP void clear() CV_OVERRIDE { _ref = _mat_type(); QualityBase::clear(); }
+
+    /**
+    @brief Create an object which calculates quality
+    @param ref input image to use as the reference for comparison
+    */
+    CV_WRAP static Ptr<QualityMAE> create(InputArray ref, int statsProc = MAE_Mean);
+
+    /**
+    @brief static method for computing quality
+    @param ref reference image
+    @param cmp comparison image=
+    @param qualityMap output quality map, or cv::noArray()
+    @param statsProc which statistical method should be apply on the absolute error
+    @returns cv::Scalar with per-channel quality values.  Values range from 0 (best) to max float (worst)
+    */
+    CV_WRAP static Scalar compute( InputArray ref, InputArray cmp, OutputArray qualityMap, int statsProc = MAE_Mean );
+
+
+protected:
+
+    /** @brief Reference image, converted to internal mat type */
+    QualityBase::_mat_type _ref;
+
+    /** @brief What statistics analysis to apply on the absolute error */
+    int _flags;
+
+    /**
+    @brief Constructor
+    @param ref reference image, converted to internal type
+    */
+    QualityMAE(QualityBase::_mat_type ref, int flags)
+        : _ref(std::move(ref)),
+          _flags(flags)
+    {}
+
+};
+
+} // quality
+
+} // cv
+
+#endif // OPENCV_QUALITY_MAE_HPP
