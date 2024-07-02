b8d6f196d30e8672fc5c9939dc5eae68a1d16221
Suleyman TURKMEN <sturkmen@hotmail.com>
2024-07-02 23:26:58 +0300

    Added EDColor support to EdgeDrawing class.
    Fixed a bug where params.PFmode = true caused incorrect edge detection results.
    Updated edge_drawing.py to support the new EDColor feature.
    Created a new edge_drawing_demo.cpp for demonstrating the EDColor feature.
    Updated ximgproc.bib to include references for EDColor.
    Added tests for PFmode and EDColor in test_fld.cpp.
    Added CV_WRAP to Params::read and Params::write functions to expose them to Python bindings.
    the class NFALUT uses the code from original ED code

--- modules/ximgproc/src/edge_drawing_common.hpp
+++ modules/ximgproc/src/edge_drawing_common.hpp
@@ -13,15 +13,15 @@
 #define ANCHOR_PIXEL  254
 #define EDGE_PIXEL    255
 
-#define LEFT  1
-#define RIGHT 2
-#define UP    3
-#define DOWN  4
+#define ED_LEFT  1
+#define ED_RIGHT 2
+#define ED_UP    3
+#define ED_DOWN  4
 
-#define SOUTH_SOUTH 0
-#define SOUTH_EAST 1
-#define EAST_SOUTH 2
-#define EAST_EAST 3
+#define ED_SOUTH_SOUTH 0
+#define ED_SOUTH_EAST 1
+#define ED_EAST_SOUTH 2
+#define ED_EAST_EAST 3
 
 // Circular arc, circle thresholds
 #define VERY_SHORT_ARC_ERROR     0.40  // Used for very short arcs (>= CANDIDATE_CIRCLE_RATIO1 && < CANDIDATE_CIRCLE_RATIO2)
@@ -37,39 +37,45 @@
 // Ellipse thresholds
 #define CANDIDATE_ELLIPSE_RATIO  0.50  // 50% -- If 50% of the ellipse is detected, it may be candidate for validation
 #define ELLIPSE_ERROR            1.50  // Used for ellipses. (used to be 1.65 for what reason?)
-#define MAX_GRAD_VALUE 128*256
+#define ED_MAX_GRAD_VALUE 128*256
 
 using namespace std;
 using namespace cv;
+
+#define TABSIZE 100000
+#define MAX_LUT_SIZE 1024
+#define RELATIVE_ERROR_FACTOR 100.0
 
 class NFALUT
 {
 public:
 
-    NFALUT(int size, double _prob, int _w, int _h);
+    NFALUT(int size, double _prob, double _logNT);
     ~NFALUT();
 
     int* LUT; // look up table
     int LUTSize;
 
     double prob;
-    int w, h;
+    double logNT;
 
     bool checkValidationByNFA(int n, int k);
     static double myAtan2(double yy, double xx);
 
 private:
     double nfa(int n, int k);
-    static double Comb(double n, double k);
+    static double log_gamma_lanczos(double x);
+    static double log_gamma_windschitl(double x);
+    static double log_gamma(double x);
+    static int double_equal(double a, double b);
 };
 
-NFALUT::NFALUT(int size, double _prob, int _w, int _h)
+NFALUT::NFALUT(int size, double _prob, double _logNT)
 {
     LUTSize = size > 60 ? 60 : size;
     LUT = new int[LUTSize];
-    w = _w;
-    h = _h;
     prob = _prob;
+    logNT = _logNT;
 
     LUT[0] = 1;
     int j = 1;
@@ -77,23 +83,22 @@ NFALUT::NFALUT(int size, double _prob, int _w, int _h)
     {
         LUT[i] = LUTSize + 1;
         double ret = nfa(i, j);
-        if (ret >= 1.0)
+        if (ret < 0)
         {
             while (j < i)
             {
                 j++;
                 ret = nfa(i, j);
-                if (ret <= 1.0)
+                if (ret >= 0)
                     break;
             }
 
-            if (ret >= 1.0)
+            if (ret < 0)
                 continue;
         }
         LUT[i] = j;
     }
 }
-
 
 NFALUT::~NFALUT()
 {
@@ -103,7 +108,7 @@ NFALUT::~NFALUT()
 bool NFALUT::checkValidationByNFA(int n, int k)
 {
     if (n >= LUTSize)
-        return nfa(n, k) <= 1.0;
+        return nfa(n, k) >= 0.0;
     else
         return k >= LUT[n];
 }
@@ -118,28 +123,140 @@ double NFALUT::myAtan2(double yy, double xx)
     return angle / 180 * CV_PI;
 }
 
-double NFALUT::Comb(double n, double k)   //fast combination computation
-{
-    if (k > n)
-        return 0;
-
-    double r = 1;
-    for (double d = 1; d <= k; ++d)
-    {
-        r *= n--;
-        r /= d;
-    }
-    return r;
-}
-
 double NFALUT::nfa(int n, int k)
 {
-    double sum = 0;
-    double p = 0.125;
-    for (int i = k; i <= n; i++)
-        sum += Comb(n, i) * pow(p, i) * pow(1 - p, n - i);
+    static double inv[TABSIZE];   /* table to keep computed inverse values */
+    double tolerance = 0.1;       /* an error of 10% in the result is accepted */
+    double log1term, term, bin_term, mult_term, bin_tail, err, p_term;
+    int i;
 
-    return sum * w * w * h * h;
+    /* check parameters */
+    if (n < 0 || k<0 || k>n || prob <= 0.0 || prob >= 1.0) return -1.0;
+
+    /* trivial cases */
+    if (n == 0 || k == 0) return -logNT;
+    if (n == k) return -logNT - (double)n * log10(prob);
+
+    /* probability term */
+    p_term = prob / (1.0 - prob);
+
+    /* compute the first term of the series */
+    /*
+    binomial_tail(n,k,p) = sum_{i=k}^n bincoef(n,i) * p^i * (1-p)^{n-i}
+    where bincoef(n,i) are the binomial coefficients.
+    But
+    bincoef(n,k) = gamma(n+1) / ( gamma(k+1) * gamma(n-k+1) ).
+    We use this to compute the first term. Actually the log of it.
+    */
+    log1term = log_gamma((double)n + 1.0) - log_gamma((double)k + 1.0)
+        - log_gamma((double)(n - k) + 1.0)
+        + (double)k * log(prob) + (double)(n - k) * log(1.0 - prob);
+    term = exp(log1term);
+
+    /* in some cases no more computations are needed */
+    if (double_equal(term, 0.0)) {              /* the first term is almost zero */
+        if ((double)k > (double)n * prob)     /* at begin or end of the tail?  */
+            return -log1term / M_LN10 - logNT;  /* end: use just the first term  */
+        else
+            return -logNT;                      /* begin: the tail is roughly 1  */
+    }
+
+      /* compute more terms if needed */
+    bin_tail = term;
+    for (i = k + 1; i <= n; i++) {
+        /*
+        As
+        term_i = bincoef(n,i) * p^i * (1-p)^(n-i)
+        and
+        bincoef(n,i)/bincoef(n,i-1) = n-1+1 / i,
+        then,
+        term_i / term_i-1 = (n-i+1)/i * p/(1-p)
+        and
+        term_i = term_i-1 * (n-i+1)/i * p/(1-p).
+        1/i is stored in a table as they are computed,
+        because divisions are expensive.
+        p/(1-p) is computed only once and stored in 'p_term'.
+        */
+        bin_term = (double)(n - i + 1) * (i < TABSIZE ?
+            (inv[i] != 0.0 ? inv[i] : (inv[i] = 1.0 / (double)i)) :
+            1.0 / (double)i);
+
+        mult_term = bin_term * p_term;
+        term *= mult_term;
+        bin_tail += term;
+
+        if (bin_term < 1.0) {
+            /* When bin_term<1 then mult_term_j<mult_term_i for j>i.
+            Then, the error on the binomial tail when truncated at
+            the i term can be bounded by a geometric series of form
+            term_i * sum mult_term_i^j.                            */
+            err = term * ((1.0 - pow(mult_term, (double)(n - i + 1))) /
+                (1.0 - mult_term) - 1.0);
+
+            /* One wants an error at most of tolerance*final_result, or:
+            tolerance * abs(-log10(bin_tail)-logNT).
+            Now, the error that can be accepted on bin_tail is
+            given by tolerance*final_result divided by the derivative
+            of -log10(x) when x=bin_tail. that is:
+            tolerance * abs(-log10(bin_tail)-logNT) / (1/bin_tail)
+            Finally, we truncate the tail if the error is less than:
+            tolerance * abs(-log10(bin_tail)-logNT) * bin_tail        */
+            if (err < tolerance * fabs(-log10(bin_tail) - logNT) * bin_tail) break;
+        }
+    }
+
+    return -log10(bin_tail) - logNT;
+}
+
+double NFALUT::log_gamma_lanczos(double x)
+{
+    static double q[7] = { 75122.6331530, 80916.6278952, 36308.2951477,
+        8687.24529705, 1168.92649479, 83.8676043424,
+        2.50662827511 };
+    double a = (x + 0.5) * log(x + 5.5) - (x + 5.5);
+    double b = 0.0;
+    int n;
+
+    for (n = 0; n < 7; n++)
+    {
+        a -= log(x + (double)n);
+        b += q[n] * pow(x, (double)n);
+    }
+    return a + log(b);
+}
+
+double NFALUT::log_gamma_windschitl(double x)
+{
+    return 0.918938533204673 + (x - 0.5) * log(x) - x
+        + 0.5 * x * log(x * sinh(1 / x) + 1 / (810.0 * pow(x, 6.0)));
+}
+
+double NFALUT::log_gamma(double x)
+{
+    return x > 15 ? log_gamma_windschitl(x) : log_gamma_lanczos(x);
+}
+
+int NFALUT::double_equal(double a, double b)
+{
+    double abs_diff, aa, bb, abs_max;
+
+    /* trivial case */
+    if (a == b) return true;
+
+    abs_diff = fabs(a - b);
+    aa = fabs(a);
+    bb = fabs(b);
+    abs_max = aa > bb ? aa : bb;
+
+    /* DBL_MIN is the smallest normalized number, thus, the smallest
+    number whose relative error is bounded by DBL_EPSILON. For
+    smaller numbers, the same quantization steps as for DBL_MIN
+    are used. Then, for smaller numbers, a meaningful "relative"
+    error should be computed by dividing the difference by DBL_MIN. */
+    if (abs_max < DBL_MIN) abs_max = DBL_MIN;
+
+    /* equal if relative error <= factor x eps */
+    return (abs_diff / abs_max) <= (RELATIVE_ERROR_FACTOR * DBL_EPSILON);
 }
 
 struct StackNode
@@ -220,18 +337,19 @@ struct mEllipse {
 // Ax^2 + Bxy + Cy^2 + Dx + Ey + F = 0
 //
 struct EllipseEquation {
-	double coeff[7];  // coeff[1] = A
+    static constexpr int COEFF_SIZE = 7;
+    double coeff[COEFF_SIZE];  // coeff[1] = A
 
-	EllipseEquation() {
-		for (int i = 0; i<7; i++) coeff[i] = 0;
-	}
+    // Constructor using an initialization list
+    EllipseEquation() : coeff{} {}
 
-	double A() { return coeff[1]; }
-	double B() { return coeff[2]; }
-	double C() { return coeff[3]; }
-	double D() { return coeff[4]; }
-	double E() { return coeff[5]; }
-	double F() { return coeff[6]; }
+    // Accessor functions marked as const
+    double A() const { return coeff[1]; }
+    double B() const { return coeff[2]; }
+    double C() const { return coeff[3]; }
+    double D() const { return coeff[4]; }
+    double E() const { return coeff[5]; }
+    double F() const { return coeff[6]; }
 };
 
 // ================================ CIRCLES ================================
