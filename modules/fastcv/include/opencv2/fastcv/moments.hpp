a00b3f329660f28a4775cd870e30d73c98e01c79
adsha-quic <quic_adsha@quicinc.com>
2024-12-23 12:41:45 +0530

    Merge pull request #3845 from CodeLinaro:adsha_2ndPost
    FastCV Extension code for OpenCV 2ndpost-2 #3845
    
    ### Description:
    - Add support for cv::fastcv::calcHist
    
    Depends on: [opencv/opencv_contrib#3844](https://github.com/opencv/opencv_contrib/pull/3844)
    Depends on: [opencv/opencv#26619](https://github.com/opencv/opencv/pull/26619)
    Requires binary from: [opencv/opencv_3rdparty#90](https://github.com/opencv/opencv_3rdparty/pull/90)
    
    ### Pull Request Readiness Checklist
    
    See details at https://github.com/opencv/opencv/wiki/How_to_contribute#making-a-good-pull-request
    
    - [x] I agree to contribute to the project under Apache 2 License.
    - [x] To the best of my knowledge, the proposed patch is not based on a code under GPL or another license that is incompatible with OpenCV
    - [ ] The PR is proposed to the proper branch
    - [ ] There is a reference to the original bug report and related work
    - [ ] There is accuracy test, performance test and test data in opencv_extra repository, if applicable
          Patch to opencv_extra has the same branch name.
    - [ ] The feature is well documented and sample code can be built with the project CMake

--- modules/fastcv/include/opencv2/fastcv/moments.hpp
+++ modules/fastcv/include/opencv2/fastcv/moments.hpp
@@ -16,7 +16,8 @@ namespace fastcv {
 
 /**
  * @brief Calculates all of the moments up to the third order of the image pixels' intensities
-          The results are returned in the structure cv::Moments.
+ *         The results are returned in the structure cv::Moments. This function cv::fastcv::moments()
+ *         calculate the moments using floating point calculations whereas cv::moments() calculate moments using double.
  * @param _src      Input image with type CV_8UC1, CV_32SC1, CV_32FC1
  * @param binary    If true, assumes the image to be binary (0x00 for black, 0xff for white), otherwise assumes the image to be
  *                  grayscale.
