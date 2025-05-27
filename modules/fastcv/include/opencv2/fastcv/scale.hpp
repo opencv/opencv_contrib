32b9dc4357941ae42b1ab7b50dd11a49d7db1419
Aakash Preetam <quic_apreetam@quicinc.com>
2025-05-27 15:33:28 +0530

    Update Down-Scale Limitation

--- modules/fastcv/include/opencv2/fastcv/scale.hpp
+++ modules/fastcv/include/opencv2/fastcv/scale.hpp
@@ -17,11 +17,14 @@ namespace fastcv {
 /**
  * @brief Down-scales the image using specified scaling factors or dimensions.
  *        This function supports both single-channel (CV_8UC1) and two-channel (CV_8UC2) images.
+ * 
  * @param _src The input image data, type CV_8UC1 or CV_8UC2.
  * @param _dst The output image data, type CV_8UC1 or CV_8UC2.
  * @param dsize The desired size of the output image. If empty, it is calculated using inv_scale_x and inv_scale_y.
  * @param inv_scale_x The inverse scaling factor for the width. If dsize is provided, this parameter is ignored.
  * @param inv_scale_y The inverse scaling factor for the height. If dsize is provided, this parameter is ignored.
+ * 
+ * @note If dsize is not specified, inv_scale_x and inv_scale_y must be strictly positive.
  */
 CV_EXPORTS_W void resizeDown(cv::InputArray _src, cv::OutputArray _dst, Size dsize, double inv_scale_x, double inv_scale_y);
 
