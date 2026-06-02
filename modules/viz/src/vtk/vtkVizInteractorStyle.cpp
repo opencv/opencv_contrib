0b7fca4facda59bea31f69429f2a441b7bbe68f9
jmackay2 <1.732mackay@gmail.com>
2026-06-01 23:49:22 -0400

    fix vtk build

--- modules/viz/src/vtk/vtkVizInteractorStyle.cpp
+++ modules/viz/src/vtk/vtkVizInteractorStyle.cpp
@@ -45,6 +45,8 @@
 
 #include "../precomp.hpp"
 
+#include <iostream>
+
 namespace cv { namespace viz
 {
     vtkStandardNewMacro(vtkVizInteractorStyle)
@@ -96,7 +98,7 @@ void cv::viz::vtkVizInteractorStyle::saveScreenshot(const String &file)
     snapshot_writer->SetFileName(file.c_str());
     snapshot_writer->Write();
 
-    cout << "Screenshot successfully captured (" << file.c_str() << ")" << endl;
+    std::cout << "Screenshot successfully captured (" << file.c_str() << ")" << std::endl;
 }
 
 //////////////////////////////////////////////////////////////////////////////////////////////
@@ -118,7 +120,7 @@ void cv::viz::vtkVizInteractorStyle::exportScene(const String &file)
     exporter->SetInput(Interactor->GetRenderWindow());
     exporter->Write();
 
-    cout << "Scene successfully exported (" << file.c_str() << ")" << endl;
+    std::cout << "Scene successfully exported (" << file.c_str() << ")" << std::endl;
 }
 
 void cv::viz::vtkVizInteractorStyle::exportScene()
