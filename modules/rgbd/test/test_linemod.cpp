efebfafcb24f17187fa482e15f31f94f721dc594
Omar <omarelmaru@gmail.com>
2026-07-22 06:55:44 +0300

    fix: test file of linemod.cpp

--- modules/rgbd/test/test_linemod.cpp
+++ modules/rgbd/test/test_linemod.cpp
@@ -7,14 +7,14 @@ TEST(RGBD_Linemod, MatchUnaligned)
     cv::Mat unaligned_mat(100, 17, CV_8UC1);
     unaligned_mat.setTo(0);
 
-    cv::linemod::Detector detector;
-    
+    cv::Ptr<cv::linemod::Detector> detector = cv::linemod::getDefaultLINEMOD();
+
     std::vector<cv::Mat> sources;
     sources.push_back(unaligned_mat);
 
     std::vector<cv::linemod::Match> matches;
-    
-    EXPECT_NO_THROW(detector.match(sources, 80.0f, matches));
+
+    EXPECT_NO_THROW(detector->match(sources, 80.0f, matches));
 }
 
 }}
