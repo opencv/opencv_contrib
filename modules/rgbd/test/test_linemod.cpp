b165e6e8feac899ed758dfe9c3320c09f6f7ff9d
Omar <omarelmaru@gmail.com>
2026-07-22 07:06:20 +0300

    rgbd(linemod): fix segfault in unaligned memory access
    Fix SIGSEGV in orUnaligned8u caused by unsafe memory alignment assumptions.
    Replaced raw pointer casting with OpenCV universal intrinsics (vx_load,
    vx_store, v_or) to safely support non-16-byte aligned buffers across
    architectures. Added a unit test to verify stability with unaligned inputs.

--- modules/rgbd/test/test_linemod.cpp
+++ modules/rgbd/test/test_linemod.cpp
@@ -4,19 +4,17 @@ namespace opencv_test { namespace {
 
 TEST(RGBD_Linemod, MatchUnaligned)
 {
-    cv::Mat color(128, 128, CV_8UC3, cv::Scalar(0, 0, 0));
-    cv::Mat depth(128, 128, CV_16UC1, cv::Scalar(0));
+    cv::Mat color_full(480, 641, CV_8UC3, cv::Scalar(0, 0, 0));
+    cv::Mat depth_full(480, 641, CV_16UC1, cv::Scalar(0));
 
-    cv::rectangle(color, cv::Rect(32, 32, 64, 64), cv::Scalar(255, 255, 255), -1);
-    cv::rectangle(depth, cv::Rect(32, 32, 64, 64), cv::Scalar(10000), -1);
+    cv::Mat color_unaligned = color_full(cv::Rect(1, 0, 640, 480));
+    cv::Mat depth_unaligned = depth_full(cv::Rect(1, 0, 640, 480));
 
     cv::Ptr<cv::linemod::Detector> detector = cv::linemod::getDefaultLINEMOD();
 
     std::vector<cv::Mat> sources;
-    sources.push_back(color);
-    sources.push_back(depth);
-
-    detector->addTemplate(sources, "object", cv::Mat());
+    sources.push_back(color_unaligned);
+    sources.push_back(depth_unaligned);
 
     std::vector<cv::linemod::Match> matches;
 
